11a39089f4f43415a9a03b247cffb1dfb4bc13f2
Brett Bieber <brett.bieber@gmail.com>
2008-11-10 16:00:53 +0000

    Move import code into MediaYak object.

--- UNL/MediaYak.php
+++ UNL/MediaYak.php
@@ -14,4 +14,43 @@ class UNL_MediaYak
         $media->fromArray($details);
         return $media->save();
     }
+    
+    /**
+     * Add media from a Yahoo Pipe containing the serialized feed.
+     *
+     * @param string $serialized_pipe_url URL with the serialized feed.
+     * 
+     * @return bool
+     */
+    function addFromYahooPipe($serialized_pipe_url)
+    {
+        if ($feed = @file_get_contents($serialized_pipe_url)) {
+            $feed = unserialize($feed);
+            if ($feed) {
+                foreach ($feed['value']['items'] as $entry) {
+                    // Try and find an existing one with this URL.
+                    $query    = new Doctrine_Query();
+                    $query->from('UNL_MediaYak_Media m');
+                    $query->where('m.url LIKE ?', array($entry['link']));
+                    $results = $query->execute();
+                    if (count($results)) {
+                        // Already exists
+                    } else {
+                        $media = array('url'         => $entry['link'],
+                                       'title'       => $entry['title'],
+                                       'description' => $entry['description'],
+                                       'datecreated' => date('Y-m-d H:i', $entry['y:published']['utime']));
+                        if (isset($entry['itunes:author'])) {
+                            $media['author'] = $entry['itunes:author']; 
+                        }
+                        $this->addMedia($media);
+                        echo 'Added '.$entry['title'].'<br />';
+                    }
+                }
+                return true;
+            }
+        }
+        
+        return false;
+    }
 }
