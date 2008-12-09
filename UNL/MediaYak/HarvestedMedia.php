3b766f7505e31499aa78db8e05b054799806711c
Brett Bieber <brett.bieber@gmail.com>
2008-12-09 18:57:41 +0000

    Create new MediaHarvester classes for finding media to add to the system.. move the yahoo pipe harvester to a separate class, update script.

---
+++ UNL/MediaYak/HarvestedMedia.php
@@ -0,0 +1,39 @@
+<?php
+
+class UNL_MediaYak_HarvestedMedia
+{
+    protected $url;
+    protected $title;
+    protected $description;
+    protected $datePublished;
+    
+    function __construct($url, $title, $description, $datePublished)
+    {
+        $this->url           = $url;
+        $this->title         = $title;
+        $this->description   = $description;
+        $this->datePublished = $datePublished;
+    }
+    
+    function getURL()
+    {
+        return $this->url;
+    }
+    
+    function getTitle()
+    {
+        return $this->title;
+    }
+    
+    function getDescription()
+    {
+        return $this->description;
+    }
+    
+    function getDatePublished()
+    {
+        return $this->datePublished;
+    }
+}
+
+?>
---
+++ UNL/MediaYak/Harvester.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Harvesters will find media and supply their info to the mediayak system.
+ */
+abstract class UNL_MediaYak_Harvester implements Iterator
+{
+    
+}
