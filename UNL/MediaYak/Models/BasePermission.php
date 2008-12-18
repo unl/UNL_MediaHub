db521e7972ed7385286a6f2d050f4679c1095843
Brett Bieber <brett.bieber@gmail.com>
2008-12-18 22:36:16 +0000

    Work on permissions. Make permissions based on feeds.

---
+++ UNL/MediaYak/Models/BasePermission.php
@@ -0,0 +1,13 @@
+<?php
+abstract class UNL_MediaYak_Models_BasePermission extends Doctrine_Record
+{
+
+  public function setTableDefinition()
+  {
+    $this->setTableName('feeds');
+    $this->hasColumn('id',            'integer',   4,    array('unsigned' => 0, 'primary' => true, 'notnull' => true, 'autoincrement' => true));
+    $this->hasColumn('title',         'string',    null, array('primary' => false, 'notnull' => true, 'autoincrement' => false, 'unique' => true));
+    $this->hasColumn('description',   'string',    null, array('primary' => false, 'notnull' => false, 'autoincrement' => false));
+  }
+  
+}
