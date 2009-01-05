1669070257a25d74d0328e0ccda2ad577b089dfe
Brett Bieber <brett.bieber@gmail.com>
2009-01-05 16:31:33 +0000

    add order parameter

--- UNL/MediaYak/FeedList.php
+++ UNL/MediaYak/FeedList.php
@@ -3,7 +3,8 @@ class UNL_MediaYak_FeedList extends UNL_MediaYak_List
 {
     public $tables = 'UNL_MediaYak_Feed f';
     
-    public $options = array('orderby'=>'title');
+    public $options = array('orderby' => 'title',
+                            'order'   => 'ASC');
     
     function setOrderBy(Doctrine_Query &$query)
     {
