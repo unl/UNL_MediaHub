050ae961fa6cbdac454759d72fa45f0d2d43472b
Brett Bieber <brett.bieber@gmail.com>
2009-01-09 16:15:08 +0000

    Add db models for namespaced elements.

---
+++ UNL/MediaYak/Models/BaseMediaHasNSElement.php
@@ -0,0 +1,22 @@
+<?php
+
+abstract class UNL_MediaYak_Models_BaseMediaHasNSElement extends Doctrine_Record
+{
+
+  public function setTableDefinition()
+  {
+    $this->setTableName('media_has_nslement');
+    $this->hasColumn('media_id',  'integer',    4, array('unsigned' => 0, 'primary' => true, 'notnull' => true, 'autoincrement' => true));
+    $this->hasColumn('nselement', 'string',  null, array('primary' => true, 'notnull' => true, 'autoincrement' => false));
+    $this->hasColumn('value',     'string',  null, array('primary' => false, 'notnull' => true, 'autoincrement' => false));
+
+  }
+
+  public function setUp()
+  {
+      $this->hasOne('UNL_MediaYak_Media',  array('local'   => 'media_id',
+                                                 'foreign' => 'id'));
+      parent::setUp();
+  }
+
+}
