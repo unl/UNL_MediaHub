26cc9769f87160d59da08b9e2461dcabfaa2d85b
Brett Bieber <brett.bieber@gmail.com>
2009-02-11 18:01:07 +0000

    Add logout capability.

--- manager/templates/Manager.tpl.php
+++ manager/templates/Manager.tpl.php
@@ -23,8 +23,7 @@ $page->breadcrumbs = '
 
 $page->collegenavigationlist = '
 <ul>
-    <li>'.UNL_MediaYak_Manager::getUser()->uid.'</li>
-    <li>Logout</li>
+    <li><a href="?logout=true">Logout '.UNL_MediaYak_Manager::getUser()->uid.'</a></li>
 </ul>';
 $page->maincontentarea = UNL_MediaYak_OutputController::display($this->output, true);
 $page->footercontent = '&copy; '.date('Y').' University of Nebraska&ndash;Lincoln | Lincoln, NE 68588 | 402-472-7211 | <a href="http://www1.unl.edu/comments/" title="Click here to direct your comments and questions">comments?</a>';
