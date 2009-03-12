6d48e59eabd14809c1ec6f3ea51e02cee2ab2676
Brett Bieber <brett.bieber@gmail.com>
2009-03-12 21:30:47 +0000

    Use built in functions to set the content and thumbnail elements.

--- scripts/updateMediaDimensions.php
+++ scripts/updateMediaDimensions.php
@@ -5,25 +5,13 @@ require_once dirname(__FILE__).'/../config.inc.php';
 // Set up system with DSN
 $mediayak = new UNL_MediaYak($dsn);
 
-UNL_MediaYak_MediaList::$results_per_page = 100;
+UNL_MediaYak_MediaList::$results_per_page = 500;
 $list = new UNL_MediaYak_MediaList();
 if (count($list->items)) {
     foreach ($list->items as $media) {
         // @var UNL_MediaYak_Media $media
-        list($width, $height) = getimagesize(UNL_MediaYak_Controller::$thumbnail_generator.urlencode($media->url));
-        if ($element = UNL_MediaYak_Feed_Media_NamespacedElements::mediaHasElement($media->id, 'content', 'mrss')) {
-            // all good
-        } else {
-            $element = new UNL_MediaYak_Feed_Media_NamespacedElements_mrss();
-            $element->media_id = $media->id;
-            $element->element = 'content';
-        }
-        $attributes = array('width'=>$width, 'height'=>$height);
-        if (isset($element->attributes) && is_array($element->attributes)) {
-            $attributes = array_merge($element->attributes, $attributes);
-        } 
-        $element->attributes = $attributes;
-        $element->save();
+        $media->setMRSSContent();
+        $media->setMRSSThumbnail();
         echo 'saved'.PHP_EOL;
     }
 }
