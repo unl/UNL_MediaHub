c88e67989a28a6af10e7459999492fa45d200729
Brett Bieber <brett.bieber@gmail.com>
2009-09-08 14:34:31 +0000

    Set executable bit on the scripts. Add filtering options to the media dimensions script

--- scripts/updateMediaDimensions.php
+++ scripts/updateMediaDimensions.php
@@ -5,8 +5,13 @@ require_once dirname(__FILE__).'/../config.inc.php';
 // Set up system with DSN
 $mediayak = new UNL_MediaYak($dsn);
 
-UNL_MediaYak_MediaList::$results_per_page = 500;
-$list = new UNL_MediaYak_MediaList();
+$filter = null;
+if (isset($_SERVER['argv'],$_SERVER['argv'][1])) {
+    $filter = new UNL_MediaYak_MediaList_Filter_TextSearch($_SERVER['argv'][1]);
+}
+
+UNL_MediaYak_MediaList::$results_per_page = 30;
+$list = new UNL_MediaYak_MediaList($filter);
 if (count($list->items)) {
     foreach ($list->items as $media) {
         // @var UNL_MediaYak_Media $media
