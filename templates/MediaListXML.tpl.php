1fea1094d6ac6bfb7e5ef60ad6aa472f0a3daaba
Brett Bieber <brett.bieber@gmail.com>
2009-09-10 14:59:08 +0000

    Skip over errors in the media list.

--- templates/MediaListXML.tpl.php
+++ templates/MediaListXML.tpl.php
@@ -10,18 +10,22 @@ if (count($this->items)) {
       <pubDate><?php echo date('r', strtotime($media->datecreated)); ?></pubDate>
       <guid><?php echo $media->url; ?></guid>
       <?php
-        foreach (array('UNL_MediaYak_Feed_Media_NamespacedElements_itunes',
-                       'UNL_MediaYak_Feed_Media_NamespacedElements_mrss') as $ns_class) {
-            foreach ($media->$ns_class as $namespaced_element) {
-                $element = "{$namespaced_element['xmlns']}:{$namespaced_element['element']}";
-                $attribute_string = '';
-                if (!empty($namespaced_element['attributes'])) {
-                    foreach ($namespaced_element['attributes'] as $attribute=>$value) {
-                        $attribute_string .= " $attribute=\"$value\"";
+        try {
+            foreach (array('UNL_MediaYak_Feed_Media_NamespacedElements_itunes',
+                           'UNL_MediaYak_Feed_Media_NamespacedElements_mrss') as $ns_class) {
+                foreach ($media->$ns_class as $namespaced_element) {
+                    $element = "{$namespaced_element['xmlns']}:{$namespaced_element['element']}";
+                    $attribute_string = '';
+                    if (!empty($namespaced_element['attributes'])) {
+                        foreach ($namespaced_element['attributes'] as $attribute=>$value) {
+                            $attribute_string .= " $attribute=\"$value\"";
+                        }
                     }
+                    echo "<{$element}{$attribute_string}>".htmlspecialchars($namespaced_element['value'])."</$element>\n";
                 }
-                echo "<{$element}{$attribute_string}>".htmlspecialchars($namespaced_element['value'])."</$element>\n";
             }
+        } catch (Exception $e) {
+            // Error, just skip this for now.
         }
       ?>
       <enclosure url="<?php echo $media->url; ?>" length="<?php echo $media->length; ?>" type="<?php echo $media->type; ?>" />
