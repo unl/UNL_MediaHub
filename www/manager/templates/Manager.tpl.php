8100e08f271e1e9ec57faf44f47a31d828e8ac45
Seth Meranda <smeranda2@unl.edu>
2010-02-25 21:17:44 +0000

    Move the manager to the new template, update css to display navigation.

--- www/manager/templates/Manager.tpl.php
+++ www/manager/templates/Manager.tpl.php
@@ -1,5 +1,6 @@
 <?php
 UNL_MediaYak_OutputController::setOutputTemplate('UNL_MediaYak_MediaList', '../templates/MediaList');
+UNL_Templates::$options['version'] = 3;
 $page = UNL_Templates::factory('Fixed');
 if (isset($GLOBALS['UNLTEMPLATEDEPENDENTSPATH'])) {
     UNL_Templates::$options['templatedependentspath'] = $GLOBALS['UNLTEMPLATEDEPENDENTSPATH'];
@@ -7,13 +8,7 @@ if (isset($GLOBALS['UNLTEMPLATEDEPENDENTSPATH'])) {
 $page->doctitle = '<title>UNL | Media Hub | Manager</title>';
 $page->titlegraphic = '<h1>UNL MediaHub Manager</h1><h2>Lights, Camera, Action</h2>';
 $page->addStyleSheet(UNL_MediaYak_Manager::getURL().'templates/css/all.css');
-$page->addScript(UNL_MediaYak_Manager::getURL().'templates/scripts/jquery-1.3.1.min.js');
 $page->leftRandomPromo = '';
-
-$page->navlinks = '
-<ul>
-    <li><a href="'.UNL_MediaYak_Manager::getURL().'">My Channels</a></li>
-</ul>';
 
 $page->breadcrumbs = '
 <ul>
@@ -21,10 +16,18 @@ $page->breadcrumbs = '
     <li>Media Hub</li>
 </ul>';
 
-$page->collegenavigationlist = '
+$page->navlinks        = '
 <ul>
-    <li><a href="?logout">Logout '.UNL_MediaYak_Manager::getUser()->uid.'</a></li>
+    <li><a href="'.UNL_MediaYak_Controller::getURL().'">Media Hub</a>';
+
+        $page->navlinks .= '<ul><li><a href="https://login.unl.edu/cas/login?service='.urlencode(UNL_MediaYak_Controller::getURL()).'">Login</a></li></ul>';
+        $page->navlinks .='<ul><li><a href="'.UNL_MediaYak_Controller::getURL().'manager/">My Media</a></li></ul>';
+        $page->navlinks .= '<ul><li><a href="?logout">Logout</a></li></ul>';
+$page->navlinks .='
+</li>
+<li><a href="'.UNL_MediaYak_Controller::getURL().'channels/">Channels</a></li>
 </ul>';
+
 $page->maincontentarea = UNL_MediaYak_OutputController::display($this->output, true);
 $page->footercontent = '&copy; '.date('Y').' University of Nebraska&ndash;Lincoln | Lincoln, NE 68588 | 402-472-7211 | <a href="http://www1.unl.edu/comments/" title="Click here to direct your comments and questions">comments?</a>';
 echo $page;
