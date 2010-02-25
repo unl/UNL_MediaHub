f025544c23e444249c245a54bbd181fae389ce72
Brett Bieber <brett.bieber@gmail.com>
2010-02-25 01:25:33 +0000

    Set correct URI

--- config.sample.php
+++ config.sample.php
@@ -5,6 +5,6 @@ $dsn = 'mysql://mediayak:mediayak@localhost/mediayak;unix_socket=/var/mysql/mysq
 
 set_include_path(dirname(__FILE__).'/src'.PATH_SEPARATOR.dirname(__FILE__).'/lib/php'.PATH_SEPARATOR.get_include_path());
 
-UNL_MediaYak_Controller::$url = 'http://localhost/workspace/UNL_MediaYak/';
+UNL_MediaYak_Controller::$url = 'http://localhost/workspace/UNL_MediaYak/www/';
 UNL_MediaYak_Controller::$thumbnail_generator = 'http://itunes.unl.edu/thumbnails.php?url=';
 
