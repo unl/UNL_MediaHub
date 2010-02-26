d46c372ec6717d0f83a8df918a50165273460897
Brett Bieber <brett.bieber@gmail.com>
2010-02-26 03:26:27 +0000

    Add model for boxee namespaced elements on channels.

---
+++ src/UNL/MediaYak/Feed/NamespacedElements/boxee.php
@@ -0,0 +1,18 @@
+<?php
+
+class UNL_MediaYak_Feed_NamespacedElements_boxee extends UNL_MediaYak_Feed_NamespacedElements
+{
+    protected $xmlns = 'boxee';
+    
+    protected $uri = 'as http://boxee.tv/spec/rss/';
+    
+    function getChannelElements()
+    {
+        return array(
+            'expiry',           // 60
+            'background-image', // 1280x720 image, http://www.wallpapers.net/wallpapers/snow_leopard-1280x720.jpg
+            'interval',         // <boxee:interval val="60" />
+            'category',         // <boxee:category name="Video" />
+            );
+    }
+}
