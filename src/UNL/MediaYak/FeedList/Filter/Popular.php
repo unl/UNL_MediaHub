ed3835c5579a5033dfba6d7e97c3c80988126c20
Brett Bieber <brett.bieber@gmail.com>
2010-06-14 20:21:32 +0000

    Correct popular channels data path.

--- src/UNL/MediaYak/FeedList/Filter/Popular.php
+++ src/UNL/MediaYak/FeedList/Filter/Popular.php
@@ -4,16 +4,17 @@ class UNL_MediaYak_FeedList_Filter_Popular implements UNL_MediaYak_Filter
     
     function apply(Doctrine_Query &$query)
     {
-        if (file_exists(dirname(__FILE__).'/../../../../scripts/popular_feeds.txt')) {
-            $popular = file(dirname(__FILE__).'/../../../../scripts/popular_feeds.txt');
+        $file = dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))).'/scripts/popular_feeds.txt';
+        if (file_exists($file)) {
+            $popular = file($file);
         }
-        $where = 'm.id = 0 ||';
+        $where = '';
         foreach ($popular as $feed) {
             if (preg_match('/channels\/([\d]+)/', $feed, $matches)) {
-                $where .= 'f.id = '.$matches[1].' || ';
+                $where .= 'f.id = '.$matches[1].' OR ';
             }
         }
-        $where = trim($where, ' |');
+        $where = trim($where, ' OR');
         $query->where($where);
     }
     
