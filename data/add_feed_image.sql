b1047c5e658e71d1727e524aedcea4a550578423
Brett Bieber <brett.bieber@gmail.com>
2010-06-23 15:54:56 +0000

    Use LONGBLOB instead of BLOB

--- data/add_feed_image.sql
+++ data/add_feed_image.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `feeds` ADD `image_data` BLOB NULL AFTER `description` ,
+ALTER TABLE `feeds` ADD `image_data` LONGBLOB NULL AFTER `description` ,
 ADD `image_type` VARCHAR( 120 ) NULL AFTER `image_data` ,
 ADD `image_size` INT NULL AFTER `image_type` ,
 ADD `image_title` VARCHAR( 150 ) NULL AFTER `image_size` ,
