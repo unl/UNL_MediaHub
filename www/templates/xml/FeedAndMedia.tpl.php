0f37801da9a15ebcea30a284a7074999cf3ddbe3
Brett Bieber <brett.bieber@gmail.com>
2010-06-23 16:21:28 +0000

    Add image to rss feed.

--- www/templates/xml/FeedAndMedia.tpl.php
+++ www/templates/xml/FeedAndMedia.tpl.php
@@ -6,7 +6,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL;
 <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:itunesu="http://www.itunesu.com/feed" xmlns:media="http://search.yahoo.com/mrss/">
   <channel>
     <title><?php echo $context->feed->title; ?></title>
-    <link><?php echo UNL_MediaYak_Controller::getURL(); ?></link>
+    <link><?php echo UNL_MediaYak_Controller::getURL($context); ?></link>
     <description><?php echo $context->feed->description; ?></description>
     <language>en-us</language>
     <pubDate><?php echo date('r'); ?></pubDate>
@@ -16,7 +16,16 @@ echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL;
     <managingEditor>editor@example.com</managingEditor>
     <webMaster>brett.bieber@gmail.com (Brett Bieber)</webMaster>
     <ttl>5</ttl>
-    <?php
+    <?php if (isset($context->feed->image_data, $context->feed->image_type)) : ?>
+    <image>
+        <url><?php echo UNL_MediaYak_Controller::getURL($context->feed); ?>/image</url>
+        <title><?php echo $context->feed->image_title; ?></title>
+        <link><?php echo UNL_MediaYak_Controller::getURL($context->feed); ?></link>
+        <?php if (isset($context->feed->image_description)): ?>
+            <description><?php echo $context->feed->image_description; ?></description>
+        <?php endif; ?>
+    </image>
+    <?php endif;
     foreach (array('UNL_MediaYak_Feed_NamespacedElements_itunes',
                    'UNL_MediaYak_Feed_NamespacedElements_media') as $ns_class) {
         foreach ($context->feed->$ns_class as $namespaced_element) {
