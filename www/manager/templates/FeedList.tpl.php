a221f0c7164d0f97bf9801502183387f19567b30
Seth Meranda <smeranda2@unl.edu>
2010-07-06 14:43:22 +0000

    Add user help text about channels, provide a quick link to edit channel details.

--- www/manager/templates/FeedList.tpl.php
+++ www/manager/templates/FeedList.tpl.php
@@ -1,12 +1,13 @@
 <div id="feedlist">
     <h1>Your Channels</h1>
+    <h4>Channels are collections of your media. Use channels to organize specific shows (ex: Backyard Farmer).</h4>
     <p>Select a channel to add your media to, or create a new channel.</p>
     <?php
     if (count($context->items)) {
         echo 'You have '.$context->total. ' channel(s):';
         echo '<ul>';
         foreach ($context->items as $feed) {
-            echo '<li><a href="'.htmlspecialchars(UNL_MediaYak_Controller::getURL($feed, array('format'=>'xml'))).'" title="RSS feed for this channel" class="feed-icon"></a> <a href="'.htmlspecialchars(UNL_MediaYak_Manager::getURL($feed), ENT_QUOTES).'" title="Edit this channel" class="edit_channel"></a> <a href="'.htmlspecialchars(UNL_MediaYak_Manager::getURL($feed), ENT_QUOTES).'">'.$feed->title.'</a> </li>'.PHP_EOL;
+            echo '<li><a href="'.htmlspecialchars(UNL_MediaYak_Controller::getURL($feed, array('format'=>'xml'))).'" title="RSS feed for this channel" class="feed-icon"></a> <a href="'.UNL_MediaYak_Manager::getURL().'?view=feedmetadata&amp;id='.$feed->id.'" title="Edit this channel" class="edit_channel"></a> <a href="'.htmlspecialchars(UNL_MediaYak_Manager::getURL($feed), ENT_QUOTES).'">'.$feed->title.'</a> </li>'.PHP_EOL;
         }
         echo '</ul>';
     } else {
