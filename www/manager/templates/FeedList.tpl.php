3117aa0d3f9721993cc87c29c91a8e490fc8a35b
Seth Meranda <smeranda2@unl.edu>
2010-07-06 17:04:01 +0000

    restyle the channel details display. add markup notes for Brett!

--- www/manager/templates/FeedList.tpl.php
+++ www/manager/templates/FeedList.tpl.php
@@ -6,7 +6,7 @@
         echo '<h6 class="list_header">You have '.$context->total. ' channel(s):</h6>';
         echo '<ul>';
         foreach ($context->items as $feed) {
-            echo '<li><a href="'.htmlspecialchars(UNL_MediaYak_Controller::getURL($feed, array('format'=>'xml'))).'" title="RSS feed for this channel" class="feed-icon"></a> <a href="'.UNL_MediaYak_Manager::getURL().'?view=feedmetadata&amp;id='.$feed->id.'" title="Edit this channel" class="edit_channel"></a> <a href="'.htmlspecialchars(UNL_MediaYak_Manager::getURL($feed), ENT_QUOTES).'">'.$feed->title.'</a> </li>'.PHP_EOL;
+            echo '<li><a href="'.htmlspecialchars(UNL_MediaYak_Controller::getURL($feed, array('format'=>'xml'))).'" title="RSS feed for this channel" class="feed-icon"></a> <a href="'.UNL_MediaYak_Manager::getURL().'?view=feedmetadata&amp;id='.$feed->id.'" title="Edit this channel" class="edit solo"></a> <a href="'.htmlspecialchars(UNL_MediaYak_Manager::getURL($feed), ENT_QUOTES).'">'.$feed->title.'</a> </li>'.PHP_EOL;
         }
         echo '</ul>';
     } else {
