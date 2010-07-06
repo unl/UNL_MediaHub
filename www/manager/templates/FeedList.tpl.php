efcb4e52ee424a4c4ff0c7c60b6707a10a9730a3
Seth Meranda <smeranda2@unl.edu>
2010-07-06 15:18:19 +0000

    style updates to the channel list.

--- www/manager/templates/FeedList.tpl.php
+++ www/manager/templates/FeedList.tpl.php
@@ -1,10 +1,9 @@
 <div id="feedlist">
     <h1>Your Channels</h1>
     <h4>Channels are collections of your media. Use channels to organize specific shows (ex: Backyard Farmer).</h4>
-    <p>Select a channel to add your media to, or create a new channel.</p>
     <?php
     if (count($context->items)) {
-        echo 'You have '.$context->total. ' channel(s):';
+        echo '<h6 class="list_header">You have '.$context->total. ' channel(s):</h6>';
         echo '<ul>';
         foreach ($context->items as $feed) {
             echo '<li><a href="'.htmlspecialchars(UNL_MediaYak_Controller::getURL($feed, array('format'=>'xml'))).'" title="RSS feed for this channel" class="feed-icon"></a> <a href="'.UNL_MediaYak_Manager::getURL().'?view=feedmetadata&amp;id='.$feed->id.'" title="Edit this channel" class="edit_channel"></a> <a href="'.htmlspecialchars(UNL_MediaYak_Manager::getURL($feed), ENT_QUOTES).'">'.$feed->title.'</a> </li>'.PHP_EOL;
@@ -18,8 +17,6 @@
         </p>';
     }
     echo '
-        <p class="action">
-            <a class="add_feed" href="'.UNL_MediaYak_Manager::getURL().'?view=feedmetadata">Create a channel</a>
-        </p>';
+        <a class="action add_feed" href="'.UNL_MediaYak_Manager::getURL().'?view=feedmetadata">Create a channel</a>';
     ?>
 </div>
