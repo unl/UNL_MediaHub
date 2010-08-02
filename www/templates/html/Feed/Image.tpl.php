f2c31e84118a3bf56c04217e9acc0b3f343dbb77
Brett Bieber <brett.bieber@gmail.com>
2010-08-02 21:51:34 +0000

    Add feed images in all views.

--- www/templates/html/Feed/Image.tpl.php
+++ www/templates/html/Feed/Image.tpl.php
@@ -1,9 +1,10 @@
 <?php
-if (isset($context->image_data, $context->image_type)) {
+if (!empty($context->image_data) && !empty($context->image_type)) {
     header('Content-type:'.$context->image_type);
     header('Content-Length:'.$context->image_size);
     echo $context->image_data;
 } else {
-    echo "Error!';";
+    header('Content-type:image/png');
+    echo file_get_contents(dirname(__FILE__).'/../../../manager/templates/css/images/iconTV.png');
 }
 
