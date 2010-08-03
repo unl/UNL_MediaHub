88f13c360cc6d1beaad9d413fd0dc8f8b2ad4650
Brett Bieber <brett.bieber@gmail.com>
2010-08-03 16:34:37 +0000

    Combine all our autoloaders into one, update config to call the autoload registration method, and use the Mock caching interface.

--- config.sample.php
+++ config.sample.php
@@ -5,8 +5,10 @@ $dsn = 'mysql://mediayak:mediayak@localhost/mediayak';
 
 set_include_path(dirname(__FILE__).'/src'.PATH_SEPARATOR.dirname(__FILE__).'/lib/php');
 
-require_once 'UNL/Autoload.php';
+require_once 'UNL/MediaYak.php';
+UNL_MediaYak::registerAutoloaders();
 
 UNL_MediaYak_Controller::$url = 'http://localhost/workspace/UNL_MediaYak/www/';
 UNL_MediaYak_Controller::$thumbnail_generator = 'http://itunes.unl.edu/thumbnails.php?url=';
 
+UNL_MediaYak_OutputController::setCacheInterface(new UNL_MediaYak_CacheInterface_Mock());
