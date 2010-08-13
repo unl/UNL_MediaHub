5e4db05675eb6a1ef748c5f8a2463b935aefd31e
Brett Bieber <brett.bieber@gmail.com>
2010-08-13 17:04:23 +0000

    Improve speed of query.

--- src/UNL/MediaYak/FeedList/Filter/WithRelatedMedia.php
+++ src/UNL/MediaYak/FeedList/Filter/WithRelatedMedia.php
@@ -4,7 +4,7 @@ class UNL_MediaYak_FeedList_Filter_WithRelatedMedia implements UNL_MediaYak_Filt
     
 function apply(Doctrine_Query &$query)
     {
-        $query->where('UNL_MediaYak_Feed_Media.media_id = UNL_MediaYak_Feed_Media.media_id AND UNL_MediaYak_Feed_Media.feed_id = f.id');
+        $query->where('UNL_MediaYak_Feed_Media.media_id IS NOT NULL AND UNL_MediaYak_Feed_Media.feed_id = f.id');
         $query->distinct();
     }
     
