8213aa433cd4a2ac971200b431b63938c02e5201
Brett Bieber <brett.bieber@gmail.com>
2010-08-13 17:03:13 +0000

    Only show channels that actually have media!

---
+++ src/UNL/MediaYak/FeedList/Filter/WithRelatedMedia.php
@@ -0,0 +1,30 @@
+<?php
+class UNL_MediaYak_FeedList_Filter_WithRelatedMedia implements UNL_MediaYak_Filter
+{
+    
+function apply(Doctrine_Query &$query)
+    {
+        $query->where('UNL_MediaYak_Feed_Media.media_id = UNL_MediaYak_Feed_Media.media_id AND UNL_MediaYak_Feed_Media.feed_id = f.id');
+        $query->distinct();
+    }
+    
+    function getLabel()
+    {
+        return 'Available Channels';
+    }
+    
+    function getType()
+    {
+        return '';
+    }
+    
+    function getValue()
+    {
+        return '';
+    }
+    
+    function __toString()
+    {
+        return '';
+    }
+}
