6ebb0150956885d9ed4a88aadf9425800ea56d9e
Seth Meranda <smeranda2@unl.edu>
2010-08-15 03:47:28 +0000

    Style the delete button.

--- www/manager/templates/Media/DeleteForm.tpl.php
+++ www/manager/templates/Media/DeleteForm.tpl.php
@@ -2,5 +2,5 @@
     <input type="hidden" id="__unlmy_posttarget" name="__unlmy_posttarget" value="delete_media" />
     <input type="hidden" name="feed_id" value="<?php echo $parent->parent->context->options['id']; ?>" />
     <input type="hidden" name="media_id" value="<?php echo $context->id; ?>" />
-    <a href="#" onclick="if (confirm('Are you sure? This will delete this media, and remove it from any associated channels.')) document.getElementById('deletemedia_<?php echo $context->id; ?>').submit();">Delete</a>
+    <a href="#" class="action delete" onclick="if (confirm('Are you sure? This will delete this media, and remove it from any associated channels.')) document.getElementById('deletemedia_<?php echo $context->id; ?>').submit();">Delete</a>
 </form>
