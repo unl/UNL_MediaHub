5abefaba237f91a1e947b18fe0684f5f081b209c
Brett Bieber <brett.bieber@gmail.com>
2010-08-17 14:17:26 +0000

    Allow feeds to be linked by title and by ID. Prefer ID.

--- src/UNL/MediaYak/Feed/Image.php
+++ src/UNL/MediaYak/Feed/Image.php
@@ -1,6 +1,18 @@
 <?php
 class UNL_MediaYak_Feed_Image extends UNL_MediaYak_Feed
 {
+    /**
+     * Get by ID
+     *
+     * @param int $id The id of the feed to get
+     *
+     * @return UNL_MediaYak_Feed
+     */
+    static function getById($id)
+    {
+        return Doctrine::getTable(__CLASS__)->find($id);
+    }
+
     static function getByTitle($title)
     {
         return Doctrine::getTable(__CLASS__)->findOneByTitle($title);
