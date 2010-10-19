35dea18f623b1daafa48ae2c8b0b7e9e9f8313ae
Seth Meranda <smeranda2@unl.edu>
2010-10-19 20:02:02 +0000

    remove the unused variables for JS

--- www/templates/html/Controller.tpl.php
+++ www/templates/html/Controller.tpl.php
@@ -10,12 +10,7 @@ $page->titlegraphic = '<h1>UNL Media Hub</h1><h2>Lights, Camera, Action</h2>';
 $page->addStyleSheet(UNL_MediaYak_Controller::getURL().'templates/html/css/all.css');
 $page->breadcrumbs = '<ul> <li><a href="http://www.unl.edu/">UNL</a></li> <li>Media Hub</li></ul>';
 $page->addScript('/ucomm/templatedependents/templatesharedcode/scripts/components/mediaplayer/swfobject.js');
-$page->head .= '<script type="text/javascript">
-UNL_MediaYak.url  = "'.UNL_MediaYak_Controller::getURL().'";
-UNL_MediaYak.thumbnail_generator = "'.UNL_MediaYak_Controller::$thumbnail_generator.'";
-var UNL_MediaYak_thumbnail_generator = "'.UNL_MediaYak_Controller::$thumbnail_generator.'";
-</script>
-<link rel="search" href="'.UNL_MediaYak_Controller::getURL().'search/" />
+$page->head .= '<link rel="search" href="'.UNL_MediaYak_Controller::getURL().'search/" />
 ';
 $page->addScript(UNL_MediaYak_Controller::getURL().'templates/html/audio-player/audio-player-noswfobject.js');
 if (!$context->output instanceof UNL_MediaYak_FeedAndMedia) {
