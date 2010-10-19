c92317d143fff3d20ed49c6c1b3d69c85d95d31c
Seth Meranda <smeranda2@unl.edu>
2010-10-19 19:59:48 +0000

    change text when rendering new image

--- www/manager/templates/Feed/Media/Form.tpl.php
+++ www/manager/templates/Feed/Media/Form.tpl.php
@@ -34,7 +34,7 @@ UNL_MediaYak_Manager::setReplacementData('head', $js);
         </ol>
         <h5 class="sec_header">Your Image</h5>
         <div id="imageOverlay">
-            <p>We're updating your image, but longer videos take more time. Now is a good time to make sure the information below is up to snuff!</p>
+            <p>We're updating your image, this may take a few minutes depending on video length. <strong>Now is a good time to make sure the information below is up to snuff!</strong></p>
         </div>
         <img src="<?php echo $thumbnail; ?>" id="thumbnail" alt="Thumbnail preview" />
         <a class="action" id="setImage" href="#">Set Image</a>
