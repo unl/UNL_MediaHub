9dffa4985b3e76939c5f77f581da60fa804b1643
Brett Bieber <brett.bieber@gmail.com>
2010-12-13 21:17:04 +0000

    Readability fixes. Fail early.

--- www/manager/index.php
+++ www/manager/index.php
@@ -4,13 +4,13 @@ require_once dirname(__FILE__).'/../../config.inc.php';
 
 $manager = new UNL_MediaYak_Manager($_GET, $dsn);
 
-if ($manager->isLoggedIn()) {
-    $outputcontroller = new UNL_MediaYak_OutputController();
-    $outputcontroller->setCacheInterface(new UNL_MediaYak_CacheInterface_Mock());
-    $outputcontroller->setTemplatePath(dirname(__FILE__).'/templates');
-
-    echo $outputcontroller->render($manager);
-
-} else {
+if (!$manager->isLoggedIn()) {
     throw new Exception('Not logged in!');
 }
+
+$outputcontroller = new UNL_MediaYak_OutputController();
+$outputcontroller->setCacheInterface(new UNL_MediaYak_CacheInterface_Mock());
+$outputcontroller->setTemplatePath(dirname(__FILE__).'/templates');
+
+echo $outputcontroller->render($manager);
+
