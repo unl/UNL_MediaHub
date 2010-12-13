117d816f39efa94ec7bcc9795552f33e795c5f31
Brett Bieber <brett.bieber@gmail.com>
2010-12-13 16:11:58 +0000

    Support the new URL syntax which contains the feed title.

--- src/UNL/MediaYak/FeedList/Filter/Popular.php
+++ src/UNL/MediaYak/FeedList/Filter/Popular.php
@@ -4,18 +4,25 @@ class UNL_MediaYak_FeedList_Filter_Popular implements UNL_MediaYak_Filter
     
     function apply(Doctrine_Query &$query)
     {
-        $file = dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))).'/scripts/popular_feeds.txt';
+        $file = dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))).'/scripts/popular_channels.txt';
         if (file_exists($file)) {
             $popular = file($file);
         }
         $where = '';
+        $params = array();
         foreach ($popular as $feed) {
-            if (preg_match('/channels\/([\d]+)/', $feed, $matches)) {
-                $where .= 'f.id = '.$matches[1].' OR ';
+            if (preg_match('/channels\/(.*)$/', $feed, $matches)) {
+                if (preg_match('/^[\d]+$/', $matches[1])) {
+                    $where .= 'f.id = ? OR ';
+                    $params[] = $matches[1];
+                } else {
+                    $where .= 'f.title = ? OR ';
+                    $params = urldecode($matches[1]);
+                }
             }
         }
         $where = trim($where, ' OR');
-        $query->where($where);
+        $query->where($where, $params);
     }
     
     function getLabel()
