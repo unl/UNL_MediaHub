b05047ab14a378b93a17b2b53690a642c767c219
Brett Bieber <brett.bieber@gmail.com>
2010-12-13 21:16:25 +0000

    Correct call to setCacheInterface.

--- www/manager/index.php
+++ www/manager/index.php
@@ -3,9 +3,10 @@
 require_once dirname(__FILE__).'/../../config.inc.php';
 
 $manager = new UNL_MediaYak_Manager($_GET, $dsn);
-UNL_MediaYak_OutputController::setCacheInterface(new UNL_MediaYak_CacheInterface_Mock());
+
 if ($manager->isLoggedIn()) {
     $outputcontroller = new UNL_MediaYak_OutputController();
+    $outputcontroller->setCacheInterface(new UNL_MediaYak_CacheInterface_Mock());
     $outputcontroller->setTemplatePath(dirname(__FILE__).'/templates');
 
     echo $outputcontroller->render($manager);
