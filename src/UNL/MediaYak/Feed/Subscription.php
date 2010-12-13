5821b885e1c6ac05a276a284c72aae02a05dc711
Brett Bieber <brett.bieber@gmail.com>
2010-12-13 22:41:12 +0000

    Add models for subscription and feed/subscription

---
+++ src/UNL/MediaYak/Feed/Subscription.php
@@ -0,0 +1,5 @@
+<?php
+class UNL_MediaYak_Feed_Subscription extends UNL_MediaYak_Models_BaseFeedHasSubscription
+{
+    
+}
---
+++ src/UNL/MediaYak/Subscription.php
@@ -0,0 +1,5 @@
+<?php
+class UNL_MediaYak_Subscription extends UNL_MediaYak_Models_BaseSubscription
+{
+    
+}
