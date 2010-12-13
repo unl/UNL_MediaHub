28fae705df7aa88be42618c0507e4d76578e0427
Brett Bieber <brett.bieber@gmail.com>
2010-12-13 21:39:31 +0000

    Correct limit value so there is NO limit.

--- src/UNL/MediaYak/UserList.php
+++ src/UNL/MediaYak/UserList.php
@@ -3,7 +3,7 @@ class UNL_MediaYak_UserList extends UNL_MediaYak_List
 {
     public $tables = 'UNL_MediaYak_User u';
     
-    public $options = array('orderby'=>'uid', 'order'=>'ASC', 'page'=>0, 'limit'=>-1);
+    public $options = array('orderby'=>'uid', 'order'=>'ASC', 'page'=>0, 'limit'=>null);
     
     function setOrderBy(Doctrine_Query &$query)
     {
