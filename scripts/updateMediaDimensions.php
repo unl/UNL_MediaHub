6a30081973fc134a97adc99aa829235d7e410a71
Brett Bieber <brett.bieber@gmail.com>
2010-12-14 19:21:27 +0000

    Add limit parameter and acommodate for the cached nature of the list.

--- scripts/updateMediaDimensions.php
+++ scripts/updateMediaDimensions.php
@@ -10,8 +10,10 @@ if (isset($_SERVER['argv'],$_SERVER['argv'][1])) {
     $filter = new UNL_MediaYak_MediaList_Filter_TextSearch($_SERVER['argv'][1]);
 }
 
-//UNL_MediaYak_MediaList::$results_per_page = 30;
-$list = new UNL_MediaYak_MediaList($filter);
+$options = array('filter' => $filter,
+                 'limit'  => 100);
+$list = new UNL_MediaYak_MediaList($options);
+$list->run();
 if (count($list->items)) {
     foreach ($list->items as $media) {
         // @var UNL_MediaYak_Media $media
