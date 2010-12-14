ca1a496d32a405969b67d5880b5b70e441787580
Brett Bieber <brett.bieber@gmail.com>
2010-12-14 19:20:58 +0000

    Add limit parameter to the list options.

--- scripts/updateMediaContentTypes.php
+++ scripts/updateMediaContentTypes.php
@@ -4,8 +4,9 @@ require_once dirname(__FILE__).'/../config.inc.php';
 
 $mediayak = new UNL_MediaYak($dsn);
 
-//UNL_MediaYak_MediaList::$results_per_page = 100;
-$list = new UNL_MediaYak_MediaList(array('filter'=>new UNL_MediaYak_MediaList_Filter_NoContentType()));
+$options = array('filter' => new UNL_MediaYak_MediaList_Filter_NoContentType(),
+                 'limit'  => 100);
+$list = new UNL_MediaYak_MediaList($options);
 $list->run();
 if (count($list->items)) {
     echo count($list->items).' media missing content types found.'.PHP_EOL;
