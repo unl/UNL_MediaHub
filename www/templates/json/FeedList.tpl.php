445b21e4e50a4a0e44e37ccab909afd4c781426a
Brett Bieber <brett.bieber@gmail.com>
2010-12-22 14:56:50 +0000

    add image and description to the json output

--- www/templates/json/FeedList.tpl.php
+++ www/templates/json/FeedList.tpl.php
@@ -4,7 +4,10 @@ $feeds = array();
 {
 <?php 
 foreach ($context->items as $feed) {
-    $feeds[] = '"'.UNL_MediaYak_Controller::getURL($feed).'":"'.$feed->title.'"';
+    $feeds[] = '"'.UNL_MediaYak_Controller::getURL($feed).'":{
+        title:"'.$feed->title.'",
+        description:"'.$feed->description.'",
+        image:"'.UNL_MediaYak_Controller::getURL($feed).'/image"}';
 }
 $ws = array("\r\n", "\n", "\r", "    ");
 echo implode(",\n", str_replace($ws, '', $feeds));
