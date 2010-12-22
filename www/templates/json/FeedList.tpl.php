24f8fe9a0a441abf255dd13b11ab3d8dfadef1cb
Brett Bieber <brett.bieber@gmail.com>
2010-12-22 15:52:25 +0000

    Begin work on a json feed for media lists.

--- www/templates/json/FeedList.tpl.php
+++ www/templates/json/FeedList.tpl.php
@@ -4,10 +4,7 @@ $feeds = array();
 {
 <?php 
 foreach ($context->items as $feed) {
-    $feeds[] = '"'.UNL_MediaYak_Controller::getURL($feed).'":{
-        title:"'.$feed->title.'",
-        description:"'.$feed->description.'",
-        image:"'.UNL_MediaYak_Controller::getURL($feed).'/image"}';
+    $feeds[] = '"'.UNL_MediaYak_Controller::getURL($feed).'":'.$savvy->render($feed);
 }
 $ws = array("\r\n", "\n", "\r", "    ");
 echo implode(",\n", str_replace($ws, '', $feeds));
