f40f7f3d2294af32a312b792a8b49ff109e8dca2
Brett Bieber <brett.bieber@gmail.com>
2011-01-03 14:53:46 +0000

    Add upgrade script for upgrading mediahub installs.

---
+++ upgrade.php
@@ -0,0 +1,38 @@
+<?php
+if (file_exists(dirname(__FILE__).'/config.inc.php')) {
+    require_once dirname(__FILE__).'/config.inc.php';
+} else {
+    require dirname(__FILE__).'/config.sample.php';
+}
+
+echo 'Connecting to the database&hellip;';
+$mediahub = new UNL_MediaYak($dsn);
+$db = $mediahub->getDB();
+echo 'connected successfully!<br />';
+/**
+ * 
+ * Enter description here ...
+ * @param Doctrine_Connection $db
+ * @param $sql
+ * @param $message
+ * @param $fail_ok
+ */
+function exec_sql($db, $sql, $message, $fail_ok = false)
+{
+	echo $message.'&hellip;';
+	try {
+	   $result = $db->execute($sql);
+	} catch (Exception $e) {
+		if (!$fail_ok) {
+			echo 'The query failed:'.$result->errorInfo();
+			exit();
+		}
+	}
+	echo 'finished.<br />';
+}
+exec_sql($db, file_get_contents(dirname(__FILE__).'/data/mediayak.sql'), 'Initializing database structure');
+exec_sql($db, file_get_contents(dirname(__FILE__).'/data/add_dateupdated.sql'), 'Add date updated', true);
+exec_sql($db, file_get_contents(dirname(__FILE__).'/data/add_feed_image.sql'), 'Adding feed image support', true);
+exec_sql($db, file_get_contents(dirname(__FILE__).'/data/add_featured_feed_fields.sql'), 'Adding featured feeds support', true);
+
+echo 'Upgrade complete!';
