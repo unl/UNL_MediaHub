d776ee065ad9f0b6c47801c2f2192dc5daa879d1
Seth Meranda <smeranda2@unl.edu>
2011-01-04 17:13:28 +0000

    include helper text for the form elements.

--- www/manager/templates/Feed/Form.tpl.php
+++ www/manager/templates/Feed/Form.tpl.php
@@ -23,19 +23,19 @@ WDN.jQuery(document).ready(function() {
 
     <fieldset id="feed_header">
 
-        <legend><?php echo (isset($context->feed))?'Edit':'Create'; ?> Channel</legend>
+        <legend><?php echo (isset($context->feed))?'Edit':'Create'; ?> a Channel</legend>
         <ol>
             <li>
-                <label for="title"><span class="required">*</span>Title</label>
+                <label for="title"><span class="required">*</span>Title <span class="helper">Channel name displayed everywhere.</span></label>
                 <input id="title" name="title" type="text" value="<?php echo (isset($context->feed))? htmlentities($context->feed->title, ENT_QUOTES):''; ?>" size="55" />
             </li>
             <li>
-                <label for="description"><span class="required">*</span>Description</label>
+                <label for="description"><span class="required">*</span>Description<span class="helper">Explain what this channel is all about. Use a few sentences, but keep it to 1 paragraph.</span></label>
                 <textarea id="description" name="description" rows="5" cols="50"><?php echo (isset($context->feed))?htmlentities($context->feed->description):''; ?></textarea>
             </li>
             <li>
                 <fieldset>
-                    <legend>Consider Channel for:</legend>
+                    <legend>Consider this channel for: <span class="helper">Delivery options, coming soon!</span></legend>
                     <ol id="extensions">
                         <li>
                             <input type="checkbox" value="itunes" id="iTunesConsideration" name="iTunesConsideration" />
@@ -61,11 +61,11 @@ WDN.jQuery(document).ready(function() {
                 <input id="image_file" name="image_file" type="file" />
             </li>
             <li>
-                <label for="image_title" class="element">Image Title</label>
+                <label for="image_title" class="element">Image Title<span class="helper">Give the image a title, used in RSS feeds.</span></label>
                 <input id="image_title" name="image_title" type="text" value="<?php echo (isset($context->feed))? htmlentities($context->feed->image_title, ENT_QUOTES):''; ?>" size="55" />
             </li>
             <li>
-                <label for="image_description" class="element">Image Description</label>
+                <label for="image_description" class="element">Image Description<span class="helper">Describe your image, used in RSS feeds.</span></label>
                 <textarea id="image_description" name="image_description" rows="5" cols="50"><?php echo (isset($context->feed))?htmlentities($context->feed->image_description):''; ?></textarea>
             </li>
         </ol>
@@ -97,16 +97,16 @@ WDN.jQuery(document).ready(function() {
         <legend>iTunes U Options</legend>
 
         <ol>
-            <li><label for='itunes_author' class='element'>Author</label><div class='element'>
+            <li><label for='itunes_author' class='element'>Author<span class="helper">Used in the Artist column of iTunes</span></label><div class='element'>
                 <input name='UNL_MediaYak_Feed_NamespacedElements_itunes[0][element]' type='hidden' value='author' />
                 <input id='itunes_author' name='UNL_MediaYak_Feed_NamespacedElements_itunes[0][value]' type='text' value='<?php echo getFieldValue($context, 'itunes', 'author'); ?>' size='55' />
                 </div></li>
-            <li><label for='itunes_block' class='element'>Block</label><div class='element'>
+            <li><label for='itunes_block' class='element'>Block<span class="helper">Keep this channel from appearing in iTunes</span></label><div class='element'>
                 <input name='UNL_MediaYak_Feed_NamespacedElements_itunes[1][element]' type='hidden' value='block' />
                 <input id='itunes_block' name='UNL_MediaYak_Feed_NamespacedElements_itunes[1][value]' type='text' value='<?php echo getFieldValue($context, 'itunes', 'block'); ?>' size='55' />
                 </div></li>
             <li>
-                <label for='itunes_category' class='element'>Category</label>
+                <label for='itunes_category' class='element'>Category <span class="helper">Select an appropriate category listing.</span></label>
                 <div class='element'>
                     <input name='UNL_MediaYak_Feed_NamespacedElements_itunes[2][element]' type='hidden' value='category' />
                     <?php
@@ -206,7 +206,6 @@ WDN.jQuery(document).ready(function() {
                         </optgroup>
                         <option <?php if (in_array('TV & Film', $categories)) echo 'selected="selected"'; ?> value="TV &amp; Film">TV &amp; Film</option>
                     </select>
-                    <dl class="caption"><dd>Choose a category for use within iTunes and iTunes U</dd></dl>
                 </div>
             </li>
             <!--<li><label for='itunes_image' class='element'>Image</label><div class='element'>
@@ -214,7 +213,7 @@ WDN.jQuery(document).ready(function() {
                 <input id='itunes_image' name='UNL_MediaYak_Feed_NamespacedElements_itunes[3][value]' type='text' value='<?php echo getFieldValue($context, 'itunes', 'image'); ?>' size='55' />
                 </div></li>-->
                 
-                <li><label for='itunes_explicit' class='element'>Explicit</label>
+                <li><label for='itunes_explicit' class='element'>Explicit<span class="helper">Set to 'yes' if this feed contains explicit content</span></label>
                 <div class='element'>
                     <input name="UNL_MediaYak_Feed_NamespacedElements_itunes[4][element]" type="hidden" value="explicit"/>
                     <select id="itunes_explicit" name="UNL_MediaYak_Feed_NamespacedElements_itunes[4][value]">
@@ -226,27 +225,30 @@ WDN.jQuery(document).ready(function() {
                         }
                         ?>
                     </select>
-                    <dl class="caption"><dd>Set to 'yes' if this feed contains explicit content</dd></dl>
                 </div></li>
                 
-                <li><label for='itunes_keywords' class='element'>Keywords</label><div class='element'>
+                <li><label for='itunes_keywords' class='element'>Keywords<span class="helper">This tag allows users to search on a maximum of 12 text keywords. Use commas to separate keywords.</span></label><div class='element'>
                 <input name='UNL_MediaYak_Feed_NamespacedElements_itunes[5][element]' type='hidden' value='keywords' />
                 <input id='itunes_keywords' name='UNL_MediaYak_Feed_NamespacedElements_itunes[5][value]' type='text' value='<?php echo getFieldValue($context, 'itunes', 'keywords'); ?>' size='55' />
-                </div></li><li><label for='itunes_new-feed-url' class='element'>New-feed-url</label><div class='element'>
+                </div></li><li><label for='itunes_new-feed-url' class='element'>New-feed-url <span class="helper">This tag allows you to change the URL where the podcast feed is located.</span></label><div class='element'>
                 <input name='UNL_MediaYak_Feed_NamespacedElements_itunes[6][element]' type='hidden' value='new-feed-url' />
                 <input id='itunes_new-feed-url' name='UNL_MediaYak_Feed_NamespacedElements_itunes[6][value]' type='text' value='<?php echo getFieldValue($context, 'itunes', 'new-feed-url'); ?>' size='55' />
-                </div></li><li><label for='itunes_owner' class='element'>Owner</label><div class='element'>
+                </div></li><li><label for='itunes_owner' class='element'>Owner <span class="helper">Information that will be used to contact the owner of the podcast. Not publicly displayed.</span></label><div class='element'>
 
                 <input name='UNL_MediaYak_Feed_NamespacedElements_itunes[7][element]' type='hidden' value='owner' />
                 <input id='itunes_owner' name='UNL_MediaYak_Feed_NamespacedElements_itunes[7][value]' type='text' value='<?php echo getFieldValue($context, 'itunes', 'owner'); ?>' size='55' />
-                </div></li><li><label for='itunes_subtitle' class='element'>Subtitle</label><div class='element'>
-                <input name='UNL_MediaYak_Feed_NamespacedElements_itunes[8][element]' type='hidden' value='subtitle' />
-                <input id='itunes_subtitle' name='UNL_MediaYak_Feed_NamespacedElements_itunes[8][value]' type='text' value='<?php echo getFieldValue($context, 'itunes', 'subtitle'); ?>' size='55' />
-                </div></li><li><label for='itunes_summary' class='element'>Summary</label><div class='element'>
-                <input name='UNL_MediaYak_Feed_NamespacedElements_itunes[9][element]' type='hidden' value='summary' />
-                <input id='itunes_summary' name='UNL_MediaYak_Feed_NamespacedElements_itunes[9][value]' type='text' value='<?php echo getFieldValue($context, 'itunes', 'summary'); ?>' size='55' />
-
-                </div></li>            <li><label for="itunes_submit" class="element">&nbsp;</label><div class="element"><input id="itunes_submit" name="submit" value="Save" type="submit" /></div></li>
+                </div></li>
+                <li>
+	                <label for='itunes_subtitle' class='element'>Subtitle <span class="helper">The contents of this tag are shown in the Description column in iTunes. The subtitle displays best if it is only a few words long.</span></label>
+	                <input name='UNL_MediaYak_Feed_NamespacedElements_itunes[8][element]' type='hidden' value='subtitle' />
+	                <input id='itunes_subtitle' name='UNL_MediaYak_Feed_NamespacedElements_itunes[8][value]' type='text' value='<?php echo getFieldValue($context, 'itunes', 'subtitle'); ?>' size='55' />
+                </li>
+                <li>
+	                <label for='itunes_summary' class='element'>Summary <span class="helper">The contents of this tag are shown in a separate window that appears when the "circled i" in the Description column is clicked. It also appears on the iTunes page for your podcast.</span></label>
+	                <input name='UNL_MediaYak_Feed_NamespacedElements_itunes[9][element]' type='hidden' value='summary' />
+                    <textarea id="itunes_summary" name="UNL_MediaYak_Feed_NamespacedElements_itunes[9][value]" rows="5" cols="50"><?php echo getFieldValue($context, 'itunes', 'summary'); ?></textarea>
+				</li>
+				<li><label for="itunes_submit" class="element">&nbsp;</label><div class="element"><input id="itunes_submit" name="submit" value="Save" type="submit" /></div></li>
         </ol>
     </fieldset>
     <fieldset id="boxee_header" style="display:none">
@@ -277,7 +279,7 @@ WDN.jQuery(document).ready(function() {
 
         <legend>Media RSS Options</legend>
         <ol>
-            <li><label for='media_rating' class='element'>Rating</label><div class='element'>
+            <li><label for='media_rating' class='element'>Rating <span class="helper">Appropriate audience: adult or nonadult. Leave blank if no restrictions.</span></label><div class='element'>
 
                 <input name='UNL_MediaYak_Feed_NamespacedElements_media[0][element]' type='hidden' value='rating' />
                 <input id='media_rating' name='UNL_MediaYak_Feed_NamespacedElements_media[0][value]' type='text' value='<?php echo getFieldValue($context, 'media', 'rating'); ?>' size='55' />
@@ -288,30 +290,30 @@ WDN.jQuery(document).ready(function() {
                 <input name='UNL_MediaYak_Feed_NamespacedElements_media[2][element]' type='hidden' value='description' />
                 <input id='media_description' name='UNL_MediaYak_Feed_NamespacedElements_media[2][value]' type='text' value='<?php echo getFieldValue($context, 'media', 'description'); ?>' size='55' />
 
-                </div></li><li><label for='media_keywords' class='element'>Keywords</label><div class='element'>
+                </div></li><li><label for='media_keywords' class='element'>Keywords<span class="helper">Comma seperated list of highly relevant keywords/tags describing the channel.</span></label><div class='element'>
                 <input name='UNL_MediaYak_Feed_NamespacedElements_media[3][element]' type='hidden' value='keywords' />
                 <input id='media_keywords' name='UNL_MediaYak_Feed_NamespacedElements_media[3][value]' type='text' value='<?php echo getFieldValue($context, 'media', 'keywords'); ?>' size='55' />
-                </div></li><li><label for='media_thumbnail' class='element'>Thumbnail</label><div class='element'>
+                </div></li><li><label for='media_thumbnail' class='element'>Thumbnail <span class="helper">Allows particular images to be used as representative images for the media object.</span></label><div class='element'>
                 <input name='UNL_MediaYak_Feed_NamespacedElements_media[4][element]' type='hidden' value='thumbnail' />
                 <input id='media_thumbnail' name='UNL_MediaYak_Feed_NamespacedElements_media[4][value]' type='text' value='<?php echo getFieldValue($context, 'media', 'thumbnail'); ?>' size='55' />
-                </div></li><li><label for='media_category' class='element'>Category</label><div class='element'>
+                </div></li><li><label for='media_category' class='element'>Category <span class="helper">Allows a taxonomy to be set that gives an indication of the type of media content, and its particular contents</span></label><div class='element'>
 
                 <input name='UNL_MediaYak_Feed_NamespacedElements_media[5][element]' type='hidden' value='category' />
                 <input id='media_category' name='UNL_MediaYak_Feed_NamespacedElements_media[5][value]' type='text' value='<?php echo getFieldValue($context, 'media', 'category'); ?>' size='55' />
-                </div></li><li><label for='media_player' class='element'>Player</label><div class='element'>
+                </div></li><li><label for='media_player' class='element'>Player <span class="helper">Allows the media object to be accessed through a web browser media player console.</span></label><div class='element'>
                 <input name='UNL_MediaYak_Feed_NamespacedElements_media[6][element]' type='hidden' value='player' />
                 <input id='media_player' name='UNL_MediaYak_Feed_NamespacedElements_media[6][value]' type='text' value='<?php echo getFieldValue($context, 'media', 'player'); ?>' size='55' />
-                </div></li><li><label for='media_credit' class='element'>Credit</label><div class='element'>
+                </div></li><li><label for='media_credit' class='element'>Credit <span class="helper">Notable entity and the contribution to the creation of the media object.</span></label><div class='element'>
                 <input name='UNL_MediaYak_Feed_NamespacedElements_media[7][element]' type='hidden' value='credit' />
                 <input id='media_credit' name='UNL_MediaYak_Feed_NamespacedElements_media[7][value]' type='text' value='<?php echo getFieldValue($context, 'media', 'credit'); ?>' size='55' />
 
-                </div></li><li><label for='media_copyright' class='element'>Copyright</label><div class='element'>
+                </div></li><li><label for='media_copyright' class='element'>Copyright <span class="helper">Copyright information for media object.</span></label><div class='element'>
                 <input name='UNL_MediaYak_Feed_NamespacedElements_media[8][element]' type='hidden' value='copyright' />
                 <input id='media_copyright' name='UNL_MediaYak_Feed_NamespacedElements_media[8][value]' type='text' value='<?php echo getFieldValue($context, 'media', 'copyright'); ?>' size='55' />
-                </div></li><li><label for='media_text' class='element'>Text</label><div class='element'>
+                </div></li><li><label for='media_text' class='element'>Text <span class="helper">Allows the inclusion of a text transcript, closed captioning, or lyrics of the media content.</span></label><div class='element'>
                 <input name='UNL_MediaYak_Feed_NamespacedElements_media[9][element]' type='hidden' value='text' />
                 <input id='media_text' name='UNL_MediaYak_Feed_NamespacedElements_media[9][value]' type='text' value='<?php echo getFieldValue($context, 'media', 'text'); ?>' size='55' />
-                </div></li><li><label for='media_restriction' class='element'>Restriction</label><div class='element'>
+                </div></li><li><label for='media_restriction' class='element'>Restriction <span class="helper">Allows restrictions to be placed on the aggregator rendering the media in the feed.</span></label><div class='element'>
 
                 <input name='UNL_MediaYak_Feed_NamespacedElements_media[10][element]' type='hidden' value='restriction' />
                 <input id='media_restriction' name='UNL_MediaYak_Feed_NamespacedElements_media[10][value]' type='text' value='<?php echo getFieldValue($context, 'media', 'restriction'); ?>' size='55' />
