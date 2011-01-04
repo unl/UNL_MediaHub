18c58badfa1789241793175e4376ea31e592ef92
Seth Meranda <smeranda2@unl.edu>
2011-01-04 19:22:08 +0000

    update markup and style of user controls to match Enews.

--- www/manager/templates/DeleteUserForm.tpl.php
+++ www/manager/templates/DeleteUserForm.tpl.php
@@ -1,13 +1,7 @@
-<form action="" method="post" class="zenform cool" style="margin-top:-6px;">
-    <fieldset id="addhead" class="daddhead_class">
-        <ol>
-            <li><label for="submit" class="element">&nbsp;</label><div class="element"><input name="submit" value="Delete User" type="submit" /></div></li>
-        </ol>
-        <div style="display: none;">
-            <input name="uid" type="hidden" value="<?php echo $context->uid; ?>" />
-            <input name="delete" type="hidden" value="delete" />
-            <input type="hidden" id="feed_id" name="feed_id" value="<?php echo (int)$_GET['feed_id']; ?>" />
-            <input type="hidden" id="__unlmy_posttarget" name="__unlmy_posttarget" value="feed_users" />
-        </div>
-    </fieldset>
+<form action="" method="post">
+    <input name="uid" type="hidden" value="<?php echo $context->uid; ?>" />
+    <input name="delete" type="hidden" value="delete" />
+    <input type="hidden" id="feed_id" name="feed_id" value="<?php echo (int)$_GET['feed_id']; ?>" />
+    <input type="hidden" id="__unlmy_posttarget" name="__unlmy_posttarget" value="feed_users" />
+    <input type="submit" value="Remove" />
 </form>
