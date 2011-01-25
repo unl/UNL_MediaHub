dcbd7da1ef0187d9062d0118befe5efb03de9563
Brett Bieber <brett.bieber@gmail.com>
2011-01-25 16:15:15 +0000

    Move player markup into a separate template so we can re-use it in other places.

--- www/templates/html/Media.tpl.php
+++ www/templates/html/Media.tpl.php
@@ -1,16 +1,11 @@
 <?php
 $type = 'audio';
-$height = 529;
-$width = 940;
-$context->loadReference('UNL_MediaYak_Media_Comment');
 if (UNL_MediaYak_Media::isVideo($context->type)) {
     $type = 'video';
     $dimensions = UNL_MediaYak_Media::getMediaDimensions($context->id);
-    if (isset($dimensions['width'])) {
-        // Scale everything down to 450 wide
-        $height = round(($width/$dimensions['width'])*$dimensions['height']);
-    }
 }
+
+$context->loadReference('UNL_MediaYak_Media_Comment');
 UNL_MediaYak_Controller::setReplacementData('title', 'UNL | MediaHub | '.htmlspecialchars($context->title));
 UNL_MediaYak_Controller::setReplacementData('breadcrumbs', '<ul> <li><a href="http://www.unl.edu/">UNL</a></li> <li><a href="'.UNL_MediaYak_Controller::getURL().'">MediaHub</a></li> <li>'.htmlspecialchars($context->title).'</li></ul>');
 $meta = '
@@ -32,38 +27,7 @@ if ($type == 'video') {
 }
 UNL_MediaYak_Controller::setReplacementData('head', $meta);
 ?>
-<?php 
-if ($type == 'video') {
-?>
-        <video height="<?php echo $height; ?>" width="<?php echo $width; ?>" autoplay src="<?php echo $context->url?>" controls poster="<?php echo UNL_MediaYak_Controller::$thumbnail_generator.($context->url)?>">
-            <object type="application/x-shockwave-flash" style="width:<?php echo $width; ?>px;height:<?php echo $height; ?>px" data="/wdn/templates_3.0/includes/swf/player4.3.swf">
-                <param name="movie" value="/wdn/templates_3.0/includes/swf/player4.3" />
-                <param name="allowfullscreen" value="true" />
-                <param name="allowscriptaccess" value="always" />
-                <param name="wmode" value="transparent" />
-                <param name="flashvars" value="file=<?php echo urlencode($context->url)?>&amp;image=<?php echo urlencode(UNL_MediaYak_Controller::$thumbnail_generator.urlencode($context->url))?>&amp;volume=100&amp;controlbar=over&amp;autostart=true&amp;skin=/wdn/templates_3.0/includes/swf/UNLVideoSkin.swf" /> 
-            </object>
-        </video>
-<?php 
-} else if ($type == 'audio') {
-?>
-		<div class="audioplayer"> 
-			<audio preload="auto"> 
-				<source src="<?php echo $context->url?>" type="audio/mpeg"> 
-				<div class="fallback"> 
-					<div class="fallback-text"> 
-						<p>Please use a modern browser or install <a href="http://get.adobe.com/flashplayer/">Flash-Plugin</a></p> 
-						<ul> 
-							<li><a class="source" href="<?php echo $context->url?>"><?php echo $context->url?></a></li> 
-						</ul> 
-					</div> 
-				</div> 
-			</audio>
-			<span class="title"><?php echo $context->title; ?><span>
-		</div>
-<?php 
-}
-?>
+<?php echo $savvy->render($context, 'MediaPlayer.tpl.php'); ?>
 <div class="three_col left supportingContent">
     <h2><?php echo $context->title; ?></h2>
     <?php
