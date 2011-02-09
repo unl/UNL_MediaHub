83bca678ae179f729baf80db388d005faea0ccdf
Brett Bieber <brett.bieber@gmail.com>
2011-02-09 21:30:15 +0000

    Improve the find 404 script to use GET requests to obtain the headers.

--- scripts/find404Media.php
+++ scripts/find404Media.php
@@ -11,11 +11,21 @@ if (count($list->items)) {
     foreach ($list->items as $media) {
         // Try and get the media
         if (substr($media->url, 0, 5) == 'http:') {
-            $headers = get_headers($media->url);
-            if (false === $headers) {
+            $context = stream_context_create(array('http'=>array(
+                'method'     => 'GET',
+                'user_agent' => 'UNL MediaHub/mediahub.unl.edu'
+                )));
+
+            if ($result = @file_get_contents($media->url, null, $context, -1, 8)) {
+                // Assume OK, $result would === false if there was a 404
+                continue;
+            }
+
+            if (false === $http_response_header) {
                 echo 'DNS failure, did the server move?'.PHP_EOL;
             }
-            foreach ($headers as $header) {
+
+            foreach ($http_response_header as $header) {
                 if (strpos($header, 'HTTP/1.1 404 Not Found') !== false) {
                     // This file is GONE! Better remove it
                     echo 'REMOVING -'.PHP_EOL.'ID:    '.$media->id.PHP_EOL.'Title: '.$media->title.PHP_EOL.'URL:   '.$media->url.PHP_EOL.PHP_EOL;
