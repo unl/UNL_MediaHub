1bbfd2713dd4d4149a29dc15b65223929c9744cb
Brett Bieber <brett.bieber@gmail.com>
2011-02-10 22:42:37 +0000

    When processing a subscription, process it for every feed that has this subscription.

--- src/UNL/MediaYak/Subscription.php
+++ src/UNL/MediaYak/Subscription.php
@@ -5,22 +5,33 @@ class UNL_MediaYak_Subscription extends UNL_MediaYak_Models_BaseSubscription
      * Processes this subscription and adds media not currently
      * added to the feed this subscription is for.
      *
-     * @return int number of media added to the feed
+     * @return int number of media added to feeds
      */
-    public function process(UNL_MediaYak_Feed $feed)
+    public function process()
     {
         $added      = 0;
         $media_list = $this->getMediaList();
+        $media_list->run();
+
+        $feed_list  = $this->getFeedList();
+        $feed_list->run();
 
         foreach ($media_list as $media) {
-            if (!$feed->hasMedia($media)) {
-                if ($feed->addMedia($media)) {
-                    $added++;
+            foreach ($feed_list->items as $feed) {
+                if (!$feed->hasMedia($media)) {
+                    if ($feed->addMedia($media)) {
+                        $added++;
+                    }
                 }
             }
         }
 
         return $added;
+    }
+
+    function getFeedList()
+    {
+        return new UNL_MediaYak_FeedList(array('filter'=>new UNL_MediaYak_FeedList_Filter_BySubscription($this)));
     }
 
     /**
