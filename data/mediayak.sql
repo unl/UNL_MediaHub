6ed9f26b71418f0b9c85436669a03537c2b2d866
Brett Bieber <brett.bieber@gmail.com>
2011-02-10 21:51:57 +0000

    Add subscription and feed_has_subscription tables.

--- data/mediayak.sql
+++ data/mediayak.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 2.11.6
+-- version 3.3.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 25, 2009 at 08:46 AM
--- Server version: 5.0.67
--- PHP Version: 5.2.6
+-- Generation Time: Feb 10, 2011 at 03:25 PM
+-- Server version: 5.1.48
+-- PHP Version: 5.3.3
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -26,13 +26,13 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(10) unsigned NOT NULL auto_increment,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `media_id` int(10) unsigned NOT NULL,
-  `uid` varchar(50) NOT NULL,
-  `comment` mediumtext NOT NULL,
-  `datecreated` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+  `uid` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `comment` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  `datecreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -41,12 +41,19 @@ CREATE TABLE IF NOT EXISTS `comments` (
 --
 
 CREATE TABLE IF NOT EXISTS `feeds` (
-  `id` int(10) unsigned NOT NULL auto_increment,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `description` mediumtext,
+  `image_data` longblob,
+  `image_type` varchar(120) DEFAULT NULL,
+  `image_size` int(11) DEFAULT NULL,
+  `image_title` varchar(150) DEFAULT NULL,
+  `image_description` mediumtext,
+  `itunes_uri` varchar(255) DEFAULT NULL COMMENT 'URI to this feed within iTunes or iTunes U',
+  `boxee` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Flag indicating whether feed is included in Boxee',
   `uidcreated` varchar(50) NOT NULL,
   `datecreated` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -58,7 +65,7 @@ CREATE TABLE IF NOT EXISTS `feeds` (
 CREATE TABLE IF NOT EXISTS `feed_has_media` (
   `feed_id` int(10) unsigned NOT NULL,
   `media_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY  (`feed_id`,`media_id`)
+  PRIMARY KEY (`feed_id`,`media_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -71,9 +78,21 @@ CREATE TABLE IF NOT EXISTS `feed_has_nselement` (
   `feed_id` int(10) unsigned NOT NULL,
   `xmlns` varchar(10) NOT NULL,
   `element` varchar(100) NOT NULL,
-  `attributes` varchar(255) default NULL,
+  `attributes` longtext,
   `value` mediumtext NOT NULL,
-  PRIMARY KEY  (`feed_id`,`xmlns`,`element`)
+  PRIMARY KEY (`feed_id`,`xmlns`,`element`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feed_has_subscription`
+--
+
+CREATE TABLE IF NOT EXISTS `feed_has_subscription` (
+  `feed_id` int(10) unsigned NOT NULL,
+  `subscription_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`feed_id`,`subscription_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -83,16 +102,17 @@ CREATE TABLE IF NOT EXISTS `feed_has_nselement` (
 --
 
 CREATE TABLE IF NOT EXISTS `media` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `url` varchar(255) NOT NULL,
-  `length` bigint(20) unsigned default '0',
-  `type` varchar(50) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `description` mediumtext NOT NULL,
-  `author` varchar(255) default NULL,
-  `datecreated` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `length` bigint(20) unsigned DEFAULT '0',
+  `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  `author` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `datecreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateupdated` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -104,9 +124,24 @@ CREATE TABLE IF NOT EXISTS `media_has_nselement` (
   `media_id` int(10) unsigned NOT NULL,
   `xmlns` varchar(10) NOT NULL,
   `element` varchar(100) NOT NULL,
-  `attributes` varchar(255) NOT NULL,
+  `attributes` longtext NOT NULL,
   `value` mediumtext NOT NULL,
-  PRIMARY KEY  (`media_id`,`xmlns`,`element`)
+  PRIMARY KEY (`media_id`,`xmlns`,`element`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscriptions`
+--
+
+CREATE TABLE IF NOT EXISTS `subscriptions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `filter_class` int(255) NOT NULL,
+  `filter_option` varchar(255) NOT NULL,
+  `datecreated` datetime NOT NULL,
+  `uidcreated` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -118,7 +153,7 @@ CREATE TABLE IF NOT EXISTS `media_has_nselement` (
 CREATE TABLE IF NOT EXISTS `users` (
   `uid` varchar(50) NOT NULL,
   `datecreated` datetime NOT NULL,
-  PRIMARY KEY  (`uid`)
+  PRIMARY KEY (`uid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -131,5 +166,5 @@ CREATE TABLE IF NOT EXISTS `user_has_permission` (
   `user_uid` varchar(50) NOT NULL,
   `permission_id` int(10) unsigned NOT NULL,
   `feed_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY  (`user_uid`,`permission_id`,`feed_id`)
+  PRIMARY KEY (`user_uid`,`permission_id`,`feed_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
