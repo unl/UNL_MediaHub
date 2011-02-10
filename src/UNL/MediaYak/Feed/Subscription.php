711434b61d868ef596ff95ac11f71f256ec2359d
Brett Bieber <brett.bieber@gmail.com>
2011-02-10 22:11:59 +0000

    Add basic logic for processing subscriptions. Find media, then adding to a feed.

--- src/UNL/MediaYak/Feed/Subscription.php
+++ src/UNL/MediaYak/Feed/Subscription.php
@@ -1,5 +1,11 @@
 <?php
 class UNL_MediaYak_Feed_Subscription extends UNL_MediaYak_Models_BaseFeedHasSubscription
 {
-    
+    function process()
+    {
+        $feed         = UNL_MediaYak_Feed::getById($this->feed_id);
+        $subscription = UNL_MediaYak_Subscription::getById($this->subscription_id);
+
+        return $subscription->process($feed);
+    }
 }
--- src/UNL/MediaYak/Subscription.php
+++ src/UNL/MediaYak/Subscription.php
@@ -1,5 +1,37 @@
 <?php
 class UNL_MediaYak_Subscription extends UNL_MediaYak_Models_BaseSubscription
 {
-    
+    /**
+     * Processes this subscription and adds media not currently
+     * added to the feed this subscription is for.
+     *
+     * @return int number of media added to the feed
+     */
+    public function process(UNL_MediaYak_Feed $feed)
+    {
+        $added      = 0;
+        $filter     = $this->getFilter();
+        $media_list = $this->getMediaList($filter);
+
+        foreach ($media_list as $media) {
+            if (!$feed->hasMedia($media)) {
+                if ($feed->addMedia($media)) {
+                    $added++;
+                }
+            }
+        }
+
+        return $added;
+    }
+
+    protected function getFilter()
+    {
+        $class  = $this->filter_class;
+        return new $class($this->filter_option);
+    }
+
+    protected function getMediaList(UNL_MediaYak_MediaList_Filter $filter)
+    {
+        return new UNL_MediaYak_MediaList(array('filter'=>$filter));
+    }
 }
