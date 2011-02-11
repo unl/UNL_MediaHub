c3104f25093ed55701f7de1a29a4b784f4779ae0
Brett Bieber <brett.bieber@gmail.com>
2011-02-11 17:53:18 +0000

    Add a bit of documentation.

--- scripts/processSubscriptions.php
+++ scripts/processSubscriptions.php
@@ -5,13 +5,20 @@ $mediayak = new UNL_MediaYak($dsn);
 
 $subscriptions = new UNL_MediaYak_SubscriptionList();
 $subscriptions->run();
+
 if (count($subscriptions->items)) {    
     do {
+        // Set a counter so we know if any media was added
         $media_added = 0;
         foreach ($subscriptions->items as $subscription) {
+            /**
+             * @var $subscription UNL_MediaYak_Subscription 
+             */
             echo 'Processing '.$subscription->filter_class.'('.$subscription->filter_option.'):'.PHP_EOL;
             $media_added += $subscription->process();
         }
+
+        // Continue to process subscriptions until no new media has been added to channels
     } while($media_added > 0);
 }
 
