483cb4661ee86d4943be567a07809e82adeaab39
Brett Bieber <brett.bieber@gmail.com>
2011-02-11 17:38:47 +0000

    Move the feedselection markup into a separate template so it can be re-used.

---
+++ www/manager/templates/User/FeedSelection.tpl.php
@@ -0,0 +1,14 @@
+<ol>
+    <li>
+        <fieldset>
+            <legend>Select from your channel or add to a new channel</legend>
+                <ol>
+                    <?php
+                    $list = UNL_MediaYak_Manager::getUser()->getFeeds();
+                    echo $savvy->render($list, 'Feed/Media/FeedList.tpl.php');
+                    ?>
+                    <li><label for="new_feed" class="element">New Channel</label><div class="element"><input id="new_feed" name="new_feed" type="text" /></div></li>
+                </ol>
+        </fieldset>
+    </li>
+</ol>
