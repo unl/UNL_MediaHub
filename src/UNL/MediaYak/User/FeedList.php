e4f5b0463e9f1207097dc5825af0a131ef3e3614
Brett Bieber <brett.bieber@gmail.com>
2011-02-11 15:14:30 +0000

    Default to 10 feeds for the limit, and change check for if filter is empty.

--- src/UNL/MediaYak/User/FeedList.php
+++ src/UNL/MediaYak/User/FeedList.php
@@ -4,10 +4,10 @@ class UNL_MediaYak_User_FeedList extends UNL_MediaYak_FeedList
     function __construct($options = array())
     {
         if (!isset($options['limit'])) {
-            $options['limit'] = null;
+            $options['limit'] = 10;
         }
 
-        if (!isset($options['filter'])) {
+        if (empty($options['filter'])) {
             $user = UNL_MediaYak_Manager::getUser();
             $options['filter'] = new UNL_MediaYak_FeedList_Filter_ByUser($user);
         }
