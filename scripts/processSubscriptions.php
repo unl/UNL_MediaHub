17ca83d45d8b63629121b984c6eded0dd1ebfa24
Brett Bieber <brett.bieber@gmail.com>
2011-02-11 14:32:47 +0000

    Keep processing subscriptions until all have been satisfied and there have been no new media added to channels.

--- scripts/processSubscriptions.php
+++ scripts/processSubscriptions.php
@@ -5,11 +5,14 @@ $mediayak = new UNL_MediaYak($dsn);
 
 $subscriptions = new UNL_MediaYak_SubscriptionList();
 $subscriptions->run();
-if (count($subscriptions->items)) {
-    foreach ($subscriptions->items as $subscription) {
-        echo 'Processing '.$subscription->filter_class.'('.$subscription->filter_option.'):'.PHP_EOL;
-        var_dump($subscription->process());
-    }
+if (count($subscriptions->items)) {    
+    do {
+        $media_added = 0;
+        foreach ($subscriptions->items as $subscription) {
+            echo 'Processing '.$subscription->filter_class.'('.$subscription->filter_option.'):'.PHP_EOL;
+            $media_added = $subscription->process();
+        }
+    } while($media_added > 0);
 }
 
 echo 'Done'.PHP_EOL;
