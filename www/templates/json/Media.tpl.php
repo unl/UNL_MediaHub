da1e9971a3988d1c6463fa93312e7232de10fb32
Brett Bieber <brett.bieber@gmail.com>
2011-02-22 19:53:09 +0000

    Trim unnecessary whitespace off the media description when outputting json

--- www/templates/json/Media.tpl.php
+++ www/templates/json/Media.tpl.php
@@ -1,5 +1,5 @@
 <?php
 echo '{
         title:"'.$context->title.'",
-        description:"'.strip_tags($context->description).'",
+        description:"'.trim(strip_tags($context->description)).'",
         image:"'.UNL_MediaYak_Controller::getURL($context).'/image"}';
