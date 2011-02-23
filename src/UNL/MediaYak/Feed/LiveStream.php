768cef10a7ef3890de082073e85b6f808fc1aea1
Brett Bieber <brett.bieber@gmail.com>
2011-02-23 20:02:36 +0000

    If a feed parameter is passed through the URL/options, populate the feed property.

--- src/UNL/MediaYak/Feed/LiveStream.php
+++ src/UNL/MediaYak/Feed/LiveStream.php
@@ -2,10 +2,16 @@
 class UNL_MediaYak_Feed_LiveStream
 {
 
-    public $channel;
+    public $feed;
     
     function __construct($options = array())
     {
-        
+        if (!empty($options['feed'])) {
+            $this->feed = $options['feed'];
+        } elseif (!empty($options['feed_id'])) {
+            $this->feed = UNL_MediaYak_Feed::getById($options['feed_id']);
+        } elseif (!empty($options['title'])) {
+            $this->feed = UNL_MediaYak_Feed::getByTitle($this->options['title']);
+        }
     }
 }
