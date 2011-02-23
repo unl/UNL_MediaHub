4e48a333bce934b66329c4faf993680ba0a01194
Seth Meranda <smeranda2@unl.edu>
2011-02-23 19:29:30 +0000

    get live feed working with test live feed.

--- www/templates/html/Feed/LiveStream.tpl.php
+++ www/templates/html/Feed/LiveStream.tpl.php
@@ -1,14 +1,10 @@
+
+<div id="preview"></div>
 <script type='text/javascript' src='http://real.unl.edu/FlashFiles/swfobject1.js'></script>
 <script type='text/javascript'>
-    var s1 = new SWFObject('http://real.unl.edu/FlashFiles/player1.swf','player','585','360','10');
+    var s1 = new SWFObject('/wdn/templates_3.0/includes/swf/player5.4.swf','player','585','360','10');
     s1.addParam('allowfullscreen','true');
     s1.addParam('allowscriptaccess','always');
-    s1.addParam('flashvars','file=live_3.sdp&amp;streamer=rtmp://real.unl.edu/live_3/&amp;image=http://real.unl.edu/graphics/nblack.jpg&amp;skin=http://real.unl.edu/FlashFiles/snel.swf');
+    s1.addParam('flashvars','file=myStream.sdp&streamer=rtmp://real.unl.edu/live/&skin=/workspace/UNL_VideoPlaya/includes/skin/unl_3.0.zip');
     s1.write('preview');
 </script>
-
-<!-- OR, SEND THE HTML5 VIDEO TAG? -->
-
-<video src="http://real.unl.edu:1935/live_3/live_3.sdp/playlist.m3u8" controls="yes" autoplay="yes">
-Your browser is not able to use the html5 video tag. Try a different browser.
-</video>  
