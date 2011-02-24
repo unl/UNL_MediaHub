116d935ad82d7fb72da359195cbbf47b245ed8b8
Michael Fairchild <mfairchild365@gmail.com>
2011-02-24 19:13:45 +0000

    correctly format and add properties to the media JSON output.

--- www/templates/json/Media.tpl.php
+++ www/templates/json/Media.tpl.php
@@ -1,5 +1,11 @@
 <?php
-echo '{
-        title:"'.$context->title.'",
-        description:"'.trim(strip_tags($context->description)).'",
-        image:"'.UNL_MediaYak_Controller::getURL($context).'/image"}';
+echo '{ "id":'.$context->id.',
+        "url":"'.$context->url.'",
+        "title":"'.$context->title.'",
+        "description":"'.trim(strip_tags($context->description)).'",
+        "length":'.$context->length.',
+        "image":"'.UNL_MediaYak_Controller::getURL($context).'/image",
+        "type":"'.$context->type.'",
+        "author":"'.$context->author.'",
+        "datecreated":"'.$context->datecreated.'",
+        "dateupdated":"'.$context->dateupdated.'"}';
