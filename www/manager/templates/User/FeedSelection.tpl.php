7486c025f50876d1cc15f2dcd0970d3076cbbf9d
Brett Bieber <brett.bieber@gmail.com>
2011-02-24 16:55:25 +0000

    Increase limit for feeds returned when selecting feeds

--- www/manager/templates/User/FeedSelection.tpl.php
+++ www/manager/templates/User/FeedSelection.tpl.php
@@ -4,7 +4,7 @@
             <legend>Select from your channel or add to a new channel</legend>
                 <ol>
                     <?php
-                    $list = UNL_MediaYak_Manager::getUser()->getFeeds();
+                    $list = UNL_MediaYak_Manager::getUser()->getFeeds(array('limit'=>300));
                     echo $savvy->render($list, 'Feed/Media/FeedList.tpl.php');
                     ?>
                     <li><label for="new_feed" class="element">New Channel</label><div class="element"><input id="new_feed" name="new_feed" type="text" /></div></li>
