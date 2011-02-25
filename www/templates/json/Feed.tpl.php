072bacd088e411dfc6cdde859b95a1fd529a82c3
Michael Fairchild <mfairchild365@gmail.com>
2011-02-25 20:21:03 +0000

    add more properties to the JSON Feed template.

--- www/templates/json/Feed.tpl.php
+++ www/templates/json/Feed.tpl.php
@@ -1,5 +1,9 @@
 <?php
 echo '{
+        "id":'.$context->id.',
         "title":"'.$context->title.'",
         "description":"'.$context->description.'",
-        "image":"'.UNL_MediaYak_Controller::getURL($context).'/image"}';
+        "image":"'.UNL_MediaYak_Controller::getURL($context).'/image",
+        "link":"'.UNL_MediaYak_Controller::getURL($context).'",
+        "pubDate":"'.$context->datecreated.'",
+        "uidcreated":"'.$context->uidcreated.'"}';
