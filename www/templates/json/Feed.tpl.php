449bafeee70f817273a92b565bbf01da14382314
Brett Bieber <brett.bieber@gmail.com>
2011-08-11 15:34:52 -0500

    Add count of media to the JSON output. Closes #52

--- www/templates/json/Feed.tpl.php
+++ www/templates/json/Feed.tpl.php
@@ -6,4 +6,5 @@ echo '{
         "image":"'.UNL_MediaHub_Controller::getURL($context).'/image",
         "link":"'.UNL_MediaHub_Controller::getURL($context).'",
         "pubDate":"'.$context->datecreated.'",
-        "uidcreated":"'.$context->uidcreated.'"}';
+        "uidcreated":"'.$context->uidcreated.'",
+        "media_count":'.$context->UNL_MediaHub_Media->count().'}';
