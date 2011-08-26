d89dd3d9c49bfce04f0380385605e1a9b5161593
Brett Bieber <brett.bieber@gmail.com>
2011-08-26 15:17:13 -0500

    Add video text track output to the media elements.

--- www/templates/html/MediaPlayer.tpl.php
+++ www/templates/html/MediaPlayer.tpl.php
@@ -26,7 +26,9 @@ if ($inForm) {
 	
 	if ($type == 'video') { ?>
 <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<video height="<?php echo $height; ?>" width="<?php echo $width; ?>" autoplay src="<?php echo $context->url?>" controls poster="<?php echo $context->getThumbnailURL(); ?>"></video>
+<video height="<?php echo $height; ?>" width="<?php echo $width; ?>" autoplay src="<?php echo $context->url?>" controls poster="<?php echo $context->getThumbnailURL(); ?>">
+	<track src="<?php echo $context->getVideoTextTrackURL(); ?>" kind="captions" type="text/vtt" srclang="en" />
+</video>
 <?php } else if ($type == 'audio') { ?>
 <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <audio preload="auto" src="<?php echo $context->url?>"></audio>
@@ -35,6 +37,6 @@ if ($inForm) {
 	if (typeof(WDN) === "undefined") {
 		if (typeof(jQuery) === "undefined"){var j=document.createElement("script"); j.setAttribute("type","text/javascript"); j.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"); document.getElementsByTagName("head")[0].appendChild(j);}
 	} else {jQuery = WDN.jQuery;}
-	var c=document.createElement("link"); c.setAttribute("type", "text/css"); c.setAttribute("rel", "stylesheet"); c.setAttribute("href", "http://www.unl.edu/wdn/templates_3.0/css/content/mediaelement.css"); document.getElementsByTagName("head")[0].appendChild(c);
-	window.onload=function(){jQuery.getScript('http://www.unl.edu/wdn/templates_3.0/scripts/mediaelement.js', function(){jQuery('video,audio').mediaelementplayer();});};
+	var c=document.createElement("link"); c.setAttribute("type", "text/css"); c.setAttribute("rel", "stylesheet"); c.setAttribute("href", "http://ucommbieber.unl.edu/wdn/templates_3.0/css/content/mediaelement.css"); document.getElementsByTagName("head")[0].appendChild(c);
+	window.onload=function(){jQuery.getScript('http://ucommbieber.unl.edu/wdn/templates_3.0/scripts/mediaelement.js', function(){jQuery('video,audio').mediaelementplayer();});};
 </script><?php }
