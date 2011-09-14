a07c00bb5a17e67319dc0c54eba82a09ff04f968
Brett Bieber <brett.bieber@gmail.com>
2011-09-14 14:09:51 -0500

    If an ID is passed, get the upload progress info for the ID requested.

--- src/UNL/MediaHub/Feed/Media/FileUpload/Progress.php
+++ src/UNL/MediaHub/Feed/Media/FileUpload/Progress.php
@@ -3,8 +3,19 @@ class UNL_MediaHub_Feed_Media_FileUpload_Progress
 {
     protected $options = array();
 
+    /**
+     * information about the file upload
+     *
+     * @var array
+     */
+    public $info = false;
+
     function __construct($options = array())
     {
         $this->options = $options + $this->options;
+        if (!empty($options['id'])
+            && function_exists('uploadprogress_get_info')) {
+            $this->info = uploadprogress_get_info($options['id']);
+        }
     }
 }
