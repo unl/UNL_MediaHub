7deae403782169069b5039a2ff1713e7e31d800b
Brett Bieber <brett.bieber@gmail.com>
2011-09-29 16:58:54 -0500

    Split media preview players into separate templates and only render if we have media present. We can retrieve a preview player separately.

---
+++ www/manager/templates/Media/Preview/Audio.tpl.php
@@ -0,0 +1,6 @@
+<div id="audioPreview" class="two_col left">
+    <h1 style="padding: 15px 0 0 20px;">Preview your Audio</h1>
+</div>
+<div id="audioDisplay" class="two_col right">
+    <?php echo $savvy->render($context, 'MediaPlayer.tpl.php'); ?>
+</div>
