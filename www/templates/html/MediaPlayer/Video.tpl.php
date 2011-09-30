918ba4976f6f97a300a5d68c026d27d6e8f40256
Brett Bieber <brett.bieber@gmail.com>
2011-09-30 15:12:28 -0500

    Do not autoplay if we're generating the media preview

--- www/templates/html/MediaPlayer/Video.tpl.php
+++ www/templates/html/MediaPlayer/Video.tpl.php
@@ -8,7 +8,13 @@ if (isset($dimensions[0])) {
     // Scale everything down to 450 wide
     $height = round(($width/$dimensions[0])*$dimensions[1]);
 }
+
+$autoplay = 'autoplay';
+if ($parent->context instanceof UNL_MediaHub_Media_Preview) {
+    $autoplay = '';
+}
+
 ?>
-<video height="<?php echo $height; ?>" width="<?php echo $width; ?>" autoplay src="<?php echo $context->url?>" controls poster="<?php echo $context->getThumbnailURL(); ?>">
+<video height="<?php echo $height; ?>" width="<?php echo $width; ?>" <?php echo $autoplay; ?> src="<?php echo $context->url?>" controls poster="<?php echo $context->getThumbnailURL(); ?>">
 	<track src="<?php echo $context->getVideoTextTrackURL(); ?>" kind="subtitles" type="text/vtt" srclang="en" />
 </video>
