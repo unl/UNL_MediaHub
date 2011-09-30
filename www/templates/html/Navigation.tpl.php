12d57ca713894507e9de1a92f5835b08d4545340
Brett Bieber <brett.bieber@gmail.com>
2011-09-30 13:38:59 -0500

    Move "Your Media" and "Add Media" to a primary navigation element

--- www/templates/html/Navigation.tpl.php
+++ www/templates/html/Navigation.tpl.php
@@ -5,13 +5,17 @@
         if (!$controller->isLoggedIn()) {
             echo '
             <li><a href="https://login.unl.edu/cas/login?service='.urlencode(UNL_MediaHub_Controller::getURL()).'">Login</a></li>';
-        } else {
-            echo '
-            <li><a href="'.UNL_MediaHub_Controller::getURL().'manager/">Your Media</a></li>
-            <li><a href="?logout">Logout</a></li>';
         }
         ?>
         </ul>
     </li>
     <li><a href="<?php echo UNL_MediaHub_Controller::getURL(); ?>channels/">Channels</a></li>
+    <?php if ($controller->isLoggedIn()) { ?>
+    <li><a href="<?php echo UNL_MediaHub_Controller::getURL(); ?>manager/">Your Media</a>
+        <ul>
+            <li><a href="<?php echo UNL_MediaHub_Controller::getURL(); ?>manager/">Add media</a></li>
+            <li><a href="?logout">Logout</a></li>
+        </ul>
+    </li>
+    <?php } ?>
 </ul>
