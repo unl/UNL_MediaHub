66b20b975fcf30d7c5f550076075c585098132c4
Brett Bieber <brett.bieber@gmail.com>
2011-10-03 13:37:27 -0500

    Add a CSS upload progress meter, and use this instead of the textual progress bar

--- www/manager/templates/Feed/Media/FileUpload.tpl.php
+++ www/manager/templates/Feed/Media/FileUpload.tpl.php
@@ -55,6 +55,8 @@ var upload = function() {
            uploadprogress = document.getElementById("uploadprogress");
            startTime = new Date();
            infoUpdated = 0;
+           // Show progress bar
+           WDN.jQuery('.meter').show();
            this.requestInfo();
         },
         stop: function(url) {
@@ -62,6 +64,8 @@ var upload = function() {
                 var secs = (new Date() - startTime)/1000;
                 var statusText = "Upload succeeded, it took " + secs + " seconds. <br/> ";
                 if (infoUpdated > 0) {
+                    // Upload succeeded and we had intermediate status updates
+                    WDN.jQuery('.meter').hide();
                     writeStatus(statusText + "You had " + infoUpdated + " updates from the progress meter, looks like it's working fine",1);
                 } else {
                     statusText += "BUT there were no progress meter updates<br/> ";
@@ -80,23 +84,137 @@ var upload = function() {
         requestInfo: function() {
             uploadprogress.src="?view=uploadprogress&format=barebones&id=<?php echo $upload_id; ?>&"+new Date();
         },
-        
-        updateInfo: function(uploaded, total, startTime) {
+
+        updateInfo: function(uploaded, total, startTime, percentage) {
             if (uploaded) {
                 infoUpdated++;
                 if (total > upload_max_filesize) {
-                    writeStatus("The file is too large and won't be available for PHP after the upload<br/> Your file size is " + total + " bytes. Allowed is " + upload_max_filesize + " bytes. That's " + Math.round (total / upload_max_filesize * 100) + "% too large<br/> Upload started since " + (new Date() - startTime)/1000 + " seconds. " + Math.floor(uploaded / total * 100) + "% done, " + estimatedSeconds + "  seconds to go",2);
+                    writeStatus("The file is too large and won't be available for PHP after the upload<br/> Your file size is " + total + " bytes. Allowed is " + upload_max_filesize + " bytes.", 2);
                 } else {
-                    writeStatus(Math.floor(uploaded / total * 100) + "% done");
+
+                    WDN.jQuery(".meter > span").animate({
+                      width: percentage+'%'
+                    }, 1000);
+
                 }
             } else {
                 writeStatus("Upload started since " + (new Date() - startTime)/1000 + " seconds. No progress info yet");
             }
-            window.setTimeout("upload.requestInfo()", 500);
+            window.setTimeout("upload.requestInfo()", 1000);
         }
     }
 }()
 </script>
+<style type="text/css">
+.meter {
+    display:none; /* Hide by default */
+    height: 20px;  /* Can be anything */
+    position: relative;
+    margin: 60px 0 20px 0; /* Just for demo spacing */
+    background: #555;
+    -moz-border-radius: 25px;
+    -webkit-border-radius: 25px;
+    border-radius: 25px;
+    padding: 10px;
+    -webkit-box-shadow: inset 0 -1px 1px rgba(255,255,255,0.3);
+    -moz-box-shadow   : inset 0 -1px 1px rgba(255,255,255,0.3);
+    box-shadow        : inset 0 -1px 1px rgba(255,255,255,0.3);
+}
+.meter > span {
+    display: block;
+    height: 100%;
+       -webkit-border-top-right-radius: 8px;
+    -webkit-border-bottom-right-radius: 8px;
+           -moz-border-radius-topright: 8px;
+        -moz-border-radius-bottomright: 8px;
+               border-top-right-radius: 8px;
+            border-bottom-right-radius: 8px;
+        -webkit-border-top-left-radius: 20px;
+     -webkit-border-bottom-left-radius: 20px;
+            -moz-border-radius-topleft: 20px;
+         -moz-border-radius-bottomleft: 20px;
+                border-top-left-radius: 20px;
+             border-bottom-left-radius: 20px;
+    background-color: rgb(43,194,83);
+    background-image: -webkit-gradient(
+      linear,
+      left bottom,
+      left top,
+      color-stop(0, rgb(43,194,83)),
+      color-stop(1, rgb(84,240,84))
+     );
+    background-image: -moz-linear-gradient(
+      center bottom,
+      rgb(43,194,83) 37%,
+      rgb(84,240,84) 69%
+     );
+    -webkit-box-shadow: 
+      inset 0 2px 9px  rgba(255,255,255,0.3),
+      inset 0 -2px 6px rgba(0,0,0,0.4);
+    -moz-box-shadow: 
+      inset 0 2px 9px  rgba(255,255,255,0.3),
+      inset 0 -2px 6px rgba(0,0,0,0.4);
+    box-shadow: 
+      inset 0 2px 9px  rgba(255,255,255,0.3),
+      inset 0 -2px 6px rgba(0,0,0,0.4);
+    position: relative;
+    overflow: hidden;
+}
+.meter > span:after, .animate > span > span {
+    content: "";
+    position: absolute;
+    top: 0; left: 0; bottom: 0; right: 0;
+    background-image: 
+       -webkit-gradient(linear, 0 0, 100% 100%, 
+          color-stop(.25, rgba(255, 255, 255, .2)), 
+          color-stop(.25, transparent), color-stop(.5, transparent), 
+          color-stop(.5, rgba(255, 255, 255, .2)), 
+          color-stop(.75, rgba(255, 255, 255, .2)), 
+          color-stop(.75, transparent), to(transparent)
+       );
+    background-image: 
+        -moz-linear-gradient(
+          -45deg, 
+          rgba(255, 255, 255, .2) 25%, 
+          transparent 25%, 
+          transparent 50%, 
+          rgba(255, 255, 255, .2) 50%, 
+          rgba(255, 255, 255, .2) 75%, 
+          transparent 75%, 
+          transparent
+       );
+    z-index: 1;
+    -webkit-background-size: 50px 50px;
+    -moz-background-size: 50px 50px;
+    -webkit-animation: move 2s linear infinite;
+       -webkit-border-top-right-radius: 8px;
+    -webkit-border-bottom-right-radius: 8px;
+           -moz-border-radius-topright: 8px;
+        -moz-border-radius-bottomright: 8px;
+               border-top-right-radius: 8px;
+            border-bottom-right-radius: 8px;
+        -webkit-border-top-left-radius: 20px;
+     -webkit-border-bottom-left-radius: 20px;
+            -moz-border-radius-topleft: 20px;
+         -moz-border-radius-bottomleft: 20px;
+                border-top-left-radius: 20px;
+             border-bottom-left-radius: 20px;
+    overflow: hidden;
+}
+
+.animate > span:after {
+    display: none;
+}
+
+@-webkit-keyframes move {
+    0% {
+       background-position: 0 0;
+    }
+    100% {
+       background-position: 50px 50px;
+    }
+}
+</style>
 <form id="fileUpload" onsubmit="upload.start()" target="uploadtarget" action="?" enctype="multipart/form-data" method="post" class="zenform cool">
     <input type="hidden" name="APC_UPLOAD_PROGRESS" value="<?php echo $upload_id;?>" />
     <input type="hidden" name="__unlmy_posttarget" value="upload_media" />
@@ -116,6 +234,9 @@ var upload = function() {
         <input type="submit" name="submit" id="mediaSubmit" value="Add Media" />
     </fieldset>
 </form>
-<div id="uploadstatus"></div>
-<iframe name="uploadprogress" width="200px" height="50px" id="uploadprogress"></iframe>
-<iframe name="uploadtarget" width="200px" height="50px" id="uploadtarget"></iframe>
+<div class="meter animate">
+<span style="width:0%;"><span></span></span>
+</div>
+<div id="uploadstatus" style="display:none;"></div>
+<iframe name="uploadprogress" width="1" height="1" id="uploadprogress"></iframe>
+<iframe name="uploadtarget" width="1" height="1" id="uploadtarget"></iframe>
