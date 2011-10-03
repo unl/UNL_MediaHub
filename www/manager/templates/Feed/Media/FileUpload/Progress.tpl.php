97d077b8e48b5a618ab52c5fde54642a901d70e3
Brett Bieber <brett.bieber@gmail.com>
2011-10-03 12:03:02 -0500

    Ensure output of upload progress is not cached.

--- www/manager/templates/Feed/Media/FileUpload/Progress.tpl.php
+++ www/manager/templates/Feed/Media/FileUpload/Progress.tpl.php
@@ -1,3 +1,8 @@
+<?php
+// Ensure output is not cached
+header('Expires: Tue, 08 Oct 1991 00:00:00 GMT');
+header('Cache-Control: no-cache, must-revalidate');
+?>
 <script type="text/javascript">
 <?php
 if (false !== $context->info) {
