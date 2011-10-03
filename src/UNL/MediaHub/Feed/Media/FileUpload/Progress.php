1f536c31b1d09ab904d5283bfde1c8e607a05f46
Brett Bieber <brett.bieber@gmail.com>
2011-10-03 11:41:57 -0500

    Switch from uploadprogress to apc for monitoring uploads.

--- src/UNL/MediaHub/Feed/Media/FileUpload/Progress.php
+++ src/UNL/MediaHub/Feed/Media/FileUpload/Progress.php
@@ -13,9 +13,40 @@ class UNL_MediaHub_Feed_Media_FileUpload_Progress
     function __construct($options = array())
     {
         $this->options = $options + $this->options;
-        if (!empty($options['id'])
-            && function_exists('uploadprogress_get_info')) {
-            $this->info = uploadprogress_get_info($options['id']);
+
+        if (empty($this->options['id'])) {
+            throw new Exception('Upload ID must be passed to retieve progress info.', 404);
         }
+
+        if (
+            function_exists('apc_fetch')
+            && ini_get('apc.enabled')
+            && ini_get('apc.rfc1867')
+            ) {
+            $this->info = apc_fetch('upload_'.$this->options['id']);
+        }
+
+    }
+
+    /**
+     * Get the percentage complete
+     *
+     * @return boolean|number False on error
+     */
+    function getPercentComplete()
+    {
+        if (!$this->info) {
+            return false;
+        }
+
+        if ($this->info['done']) {
+            return 100;
+        }
+
+        if (!$this->info['total']) {
+            return 0;
+        }
+
+        return $this->info['current'] / $this->info['total'] * 100;
     }
 }
