74bea590b3f7c4821be460ab61135da006d0399d
Michael Fairchild <mfairchild365@gmail.com>
2011-10-26 15:30:12 -0500

    Update the json format for media to output the xml elements aswell.

--- www/templates/json/Media.tpl.php
+++ www/templates/json/Media.tpl.php
@@ -1,12 +1,39 @@
 <?php
-echo '{ "id":'.$context->id.',
-        "url":"'.htmlspecialchars($context->url).'",
-        "title":"'.htmlspecialchars($context->title).'",
-        "description":"'.trim(strip_tags($context->description)).'",
-        "length":'.$context->length.',
-        "image":"'.UNL_MediaHub_Controller::getURL($context).'/image",
-        "type":"'.htmlspecialchars($context->type).'",
-        "author":"'.htmlspecialchars($context->author).'",
-        "link":"'.htmlspecialchars(UNL_MediaHub_Controller::getURL($context)).'",
-        "pubDate":"'.$context->datecreated.'",
-        "dateupdated":"'.$context->dateupdated.'"}';
+$details = array('id'          => $context->id,
+                 'url'         => $context->url,
+                 'title'       => $context->title,
+                 'description' => $context->description,
+                 'length'      => $context->length,
+                 'image'       => UNL_MediaHub_Controller::getURL($context).'/image',
+                 'type'        => $context->type,
+                 'author'      => $context->author,
+                 'link'        => UNL_MediaHub_Controller::getURL($context),
+                 'pubDate'     => $context->datecreated,
+                 'dateupdated' => $context->dateupdated);
+
+try{
+    foreach (array('UNL_MediaHub_Feed_Media_NamespacedElements_itunesu',
+               'UNL_MediaHub_Feed_Media_NamespacedElements_itunes',
+               'UNL_MediaHub_Feed_Media_NamespacedElements_media',
+               'UNL_MediaHub_Feed_Media_NamespacedElements_boxee',
+               'UNL_MediaHub_Feed_Media_NamespacedElements_geo') as $ns_class) {
+        foreach ($context->$ns_class as $namespaced_element) {
+            $element = $namespaced_element['xmlns'] . "_" . $namespaced_element['element'];
+            switch ($element) {
+                default:
+                    $attribute_string = '';
+                if (!empty($namespaced_element['attributes'])) {
+                    foreach ($namespaced_element['attributes'] as $attribute=>$value) {
+                        $attribute_string .= " $attribute=\"$value\"";
+                    }
+                }
+                $details[$element . $attribute_string] = $namespaced_element['value'];
+                break;
+            }
+        }
+    }
+} catch (Exception $e) {
+        // Error, just skip this for now.
+}
+
+echo json_encode($details);
