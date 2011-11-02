ae8f4285c22fb89c065252d698cf34d4d9ab08c5
Michael Fairchild <mfairchild365@gmail.com>
2011-11-02 11:46:12 -0500

    change the order of the water_maf and water_cfs input so that they are listed alphabetically.  If they are not in this order, it will cause issues with doctrine.  :(

--- www/manager/templates/Feed/Media/Form.tpl.php
+++ www/manager/templates/Feed/Media/Form.tpl.php
@@ -99,17 +99,17 @@ if (isset($context->media)) {
         <legend>Other Information</legend>
         <ol>
             <li>
-                <label for="water_maf" class="element">Water: MAF</label>
+                <label for="water_cfs" class="element">Water: CFS</label>
                 <div class="element">
-                    <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[0][element]" type="hidden" value="water_maf"/>
-                    <input id="water_maf" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[0][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'water_maf'); ?>"/>
+                    <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[0][element]" type="hidden" value="water_cfs"/>
+                    <input id="water_cfs" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[0][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'water_cfs'); ?>"/>
                 </div>
             </li>
             <li>
-                <label for="water_cfs" class="element">Water: CFS</label>
+                <label for="water_maf" class="element">Water: MAF</label>
                 <div class="element">
-                    <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[1][element]" type="hidden" value="water_cfs"/>
-                    <input id="water_cfs" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[1][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'water_cfs'); ?>"/>
+                    <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[1][element]" type="hidden" value="water_maf"/>
+                    <input id="water_maf" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[1][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'water_maf'); ?>"/>
                 </div>
             </li>
         </ol>
