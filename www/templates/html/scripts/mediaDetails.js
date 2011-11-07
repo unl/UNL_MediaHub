32f0f6dc49d2eacc7f5d7f72f3f5b8735198f6f6
Michael Fairchild <mfairchild365@gmail.com>
2011-11-07 16:54:14 -0600

    only hide the loading image until the new thumbnail has been loaded into the DOM

--- www/templates/html/scripts/mediaDetails.js
+++ www/templates/html/scripts/mediaDetails.js
@@ -30,10 +30,10 @@ var mediaDetails = function() {
 			newThumbnail.src = mediaDetails.imageURL + '&time='+mediaDetails.formatTime(currentTime)+'&rebuild';
 			WDN.log(newThumbnail.src);
 			newThumbnail.onload = function(){
-				WDN.jQuery('#thumbnail').attr('src', newThumbnail.src );
-				WDN.jQuery('#imageOverlay').hide();
+				WDN.jQuery('#thumbnail').attr('src', newThumbnail.src ).load(function(){
+					WDN.jQuery('#imageOverlay').hide();
+				});
 			};
-			
 		},
 		
 		currentPostion : function(video) {
