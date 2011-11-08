24b842d834f1c71b50e3d392fd5ac09e62bbc4d5
Brett Bieber <brett.bieber@gmail.com>
2011-11-08 13:37:39 -0600

    Allow mediahub namespaced item elements to be dynamically configured
    Each deployment may require custom namespaced item elements.
    Specifically, the water_af and water_cfs fields are not likely to be
    used by any other instances. This allows each deployment of MediaHub to
    set custom mediahub namespaced item elements which can be inserted into
    the add media form at runtime.
    
    Also add an example of how to set a custom namespaced item, along with
    the new templates for these item elements.
    
    Closes #61

--- src/UNL/MediaHub/Feed/Media/NamespacedElements/mediahub.php
+++ src/UNL/MediaHub/Feed/Media/NamespacedElements/mediahub.php
@@ -10,15 +10,39 @@ class UNL_MediaHub_Feed_Media_NamespacedElements_mediahub extends UNL_MediaHub_F
     public static $xmlns = 'mediahub';
     
     public static $uri = '';
-    
+
+    /**
+     * Static var which holds the custom item element names.
+     * 
+     * @var array Array of key=>description pairs
+     */
+    protected static $itemElements = array();
+
     function getItemElements()
     {
-        return array(
-            'water_maf',
-            'water_cfs',
-            );
+        return array_keys(self::$itemElements);
     }
-    
+
+    /**
+     * Get the custom MediaHub item elements
+     * 
+     * @return array Array of key=>description pairs
+     */
+    public static function getCustomElements()
+    {
+        return self::$itemElements;
+    }
+
+    /**
+     * Set the item elements placed under the mediahub namespace
+     * 
+     * @param array $elements Array of key=>description pairs
+     */
+    public static function setCustomElements($elements)
+    {
+        self::$itemElements = $elements;
+    }
+
     public static function mediaHasElement($media_id, $element)
     {
         return UNL_MediaHub_Feed_Media_NamespacedElements::mediaHasElement($media_id, $element, 'mediahub');
