06ecad608b607d2e9e9789401dc7b99a37224684
Michael Fairchild <mfairchild365@gmail.com>
2011-11-17 16:00:33 -0600

    change maf to af.  Make the lables a little more descriptive.

--- www/manager/templates/Feed/Media/NamespacedElements/mediahub/water_af.tpl.php
+++ www/manager/templates/Feed/Media/NamespacedElements/mediahub/water_af.tpl.php
@@ -4,17 +4,17 @@
 </div>
 <ol>
     <li id='water_cfs_form'>
-        <label for="water_cfs" class="element">Water: CFS</label>
+        <label for="water_cfs" class="element">Speed of water in cubic feet per second (cfs)</label>
         <div class="element">
             <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[0][element]" type="hidden" value="water_cfs"/>
             <input id="water_cfs" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[0][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'water_cfs'); ?>"/>
         </div>
     </li>
     <li id='water_maf_form'>
-        <label for="water_maf" class="element">Water: MAF</label>
+        <label for="water_maf" class="element">Volume of water in acre-feet (af)</label>
         <div class="element">
-            <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[1][element]" type="hidden" value="water_maf"/>
-            <input id="water_maf" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[1][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'water_maf'); ?>"/>
+            <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[1][element]" type="hidden" value="water_af"/>
+            <input id="water_maf" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[1][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'water_af'); ?>"/>
         </div>
     </li>
 </ol>
