220e429f5026b6718f4c42ce3580996f6591b240
Michael Fairchild <mfairchild365@gmail.com>
2012-01-04 12:50:08 -0600

    Wording changes.

--- www/manager/templates/Feed/Media/NamespacedElements/mediahub/water_af.tpl.php
+++ www/manager/templates/Feed/Media/NamespacedElements/mediahub/water_af.tpl.php
@@ -1,10 +1,10 @@
-<label for="water_flow_type" class="element">Water flow type:</label>
+<label for="water_flow_type" class="element">Water Quantity Type:</label>
 <div class="element">
-   <a href='#' id='show_maf'>Standing</a> or <a href='#' id='show_cfs'>Flowing?</a>
+   <a href='#' id='show_maf'>Volume</a> or <a href='#' id='show_cfs'>Flow?</a>
 </div>
 <ol>
     <li id='water_cfs_form'>
-        <label for="water_cfs" class="element">Speed of water in cubic feet per second (cfs)</label>
+        <label for="water_cfs" class="element">Flow rate of water in cubic feet per second (cfs)</label>
         <div class="element">
             <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[0][element]" type="hidden" value="water_cfs"/>
             <input id="water_cfs" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[0][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'water_cfs'); ?>"/>
