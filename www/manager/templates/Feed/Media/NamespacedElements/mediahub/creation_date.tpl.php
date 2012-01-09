6cc114ef02cf4ddfe176f46baa27954447c1c957
Michael Fairchild <mfairchild365@gmail.com>
2012-01-09 10:40:02 -0600

    Add a media creation date element.

---
+++ www/manager/templates/Feed/Media/NamespacedElements/mediahub/creation_date.tpl.php
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+    //<![CDATA[ 
+    WDN.jQuery(document).ready(function ($) { 
+        WDN.loadCSS('/wdn/templates_3.0/scripts/plugins/ui/jquery-ui.css'); 
+        WDN.loadCSS('/wdn/templates_3.0/scripts/plugins/ui/ui.datepicker.css'); 
+        WDN.loadJS('/wdn/templates_3.0/scripts/plugins/ui/jQuery.ui.js', function () {
+            $('#media_creation_date').datepicker({ dateFormat: 'yy-mm-dd' }); 
+        }); 
+    }); 
+    //]]>
+</script>
+
+<li>
+    <label for="media_creation_date" class="element">Media Creation Date<span class="helper">The date that the media was created.</span></label>
+    <div class="element">
+        <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[0][element]" type="hidden" value="media_creation_date"/>
+        <input id="media_creation_date" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[0][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'media_creation_date'); ?>"/>
+    </div>
+</li>
