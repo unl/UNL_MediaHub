2f56a65ba578ea93c3a1118b9d30fb0630d324d6
Michael Fairchild <mfairchild365@gmail.com>
2012-01-18 09:39:49 -0600

    Correct the index so that there isn't a collision with another element.

--- www/manager/templates/Feed/Media/NamespacedElements/mediahub/creation_date.tpl.php
+++ www/manager/templates/Feed/Media/NamespacedElements/mediahub/creation_date.tpl.php
@@ -13,7 +13,7 @@
 <li>
     <label for="media_creation_date" class="element">Media Creation Date<span class="helper">The date that the media was created.</span></label>
     <div class="element">
-        <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[0][element]" type="hidden" value="media_creation_date"/>
-        <input id="media_creation_date" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[0][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'media_creation_date'); ?>"/>
+        <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[2][element]" type="hidden" value="media_creation_date"/>
+        <input id="media_creation_date" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[2][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'media_creation_date'); ?>"/>
     </div>
 </li>
