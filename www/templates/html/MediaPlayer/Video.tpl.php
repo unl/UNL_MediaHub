4e6b5ea3f704fa172e67983606b8ebc74f88bd56
Brett Bieber <brett.bieber@gmail.com>
2013-05-03 13:00:42 -0500

    Correct reference to field s/name/title/

--- www/templates/html/MediaPlayer/Video.tpl.php
+++ www/templates/html/MediaPlayer/Video.tpl.php
@@ -15,6 +15,6 @@ if ($parent->context instanceof UNL_MediaHub_Media_Preview) {
 }
 
 ?>
-<video class="wdn_player" style="width:100%;height:100%" <?php echo $autoplay; ?> src="<?php echo $context->url; ?>" controls poster="<?php echo $context->getThumbnailURL(); ?>" title="<?php echo $context->name; ?>">
+<video class="wdn_player" style="width:100%;height:100%" <?php echo $autoplay; ?> src="<?php echo $context->url; ?>" controls poster="<?php echo $context->getThumbnailURL(); ?>" title="<?php echo $context->title; ?>">
 	<track src="<?php echo $context->getVideoTextTrackURL(); ?>" kind="subtitles" srclang="en" />
 </video>
