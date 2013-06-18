72596066b2e380075fce1864d6fb06ba7f6a2164
Brett Bieber <brett.bieber@gmail.com>
2013-06-18 11:33:39 -0500

    Add CLI script to add a user to a channel/feed

---
+++ scripts/addUser.php
@@ -0,0 +1,20 @@
+<?php
+require_once dirname(__FILE__).'/../config.inc.php';
+
+if (!isset($_SERVER['argv'],$_SERVER['argv'][1])
+    || $_SERVER['argv'][1] == '--help' || $_SERVER['argc'] != 3) {
+        echo "This program requires 2 arguments.\n";
+        echo "addMedia.php url title description\n\n";
+        echo "Example: addUser.php channel-id username\n";
+} else {
+    require_once dirname(__FILE__).'/../config.inc.php';
+
+    $my = new UNL_MediaHub($dsn);
+
+    $feed = UNL_MediaHub_Feed::getById($_SERVER['argv'][1]);
+    $user = UNL_MediaHub_User::getByUid($_SERVER['argv'][2]);
+    $feed->addUser($user);
+
+    echo "{$_SERVER['argv'][2]} has been added to {$feed->title}.\n";
+}
+exit();
