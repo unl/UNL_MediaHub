71be32d68667bf56c7e7276808d6bbd6aafed3ce
Brett Bieber <brett.bieber@gmail.com>
2013-09-30 16:34:09 -0500

    Use the Amara v2 API to retrieve subtitles for videos, Closes #87

--- www/templates/html/Media/VideoTextTrack.tpl.php
+++ www/templates/html/Media/VideoTextTrack.tpl.php
@@ -1,11 +1,20 @@
 <?php
 
-// Try grabbing the vtt from Universal Subtitles
-$vtt = @file_get_contents('http://www.universalsubtitles.org/api/1.0/subtitles/?video_url='.$context->url.'&sformat=srt');
+$vtt = false;
+
+// Check if universal subtitles has info on this media
+if ($info_json = @file_get_contents('http://www.amara.org/api2/partners/videos/?video_url='.$context->url.'&format=json')) {
+    $json = json_decode($info_json);
+
+    if ($json->meta->total_count >= 1) {
+        // Try grabbing the vtt from Universal Subtitles
+        $vtt = @file_get_contents('http://www.amara.org/api2/partners/videos/'.$json->objects[0]->id.'/languages/en/subtitles/?format=vtt');
+    }
+}
 
 if ($vtt === false
     || $vtt == '') {
     $savvy->render(new Exception('No text track found', 404));
 }
-echo 'WEBVTT'.PHP_EOL.PHP_EOL;
+
 echo $vtt;
