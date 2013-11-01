bc44121a74668c2407db13f80235cc1f00037949
Michael Fairchild <mfairchild365@gmail.com>
2013-11-01 12:37:14 -0500

    Move embed js into the embed template

--- www/templates/html/MediaPlayer.tpl.php
+++ www/templates/html/MediaPlayer.tpl.php
@@ -5,36 +5,4 @@ if ($context->isVideo()) {
     echo $savvy->render($context, 'MediaPlayer/Audio.tpl.php');
 }
 ?>
-<script type="text/javascript">
-(function() {
-var j, l, t, r = function() {
-    WDN.setPluginParam('mediaelement_wdn', 'options', {
-        success: function(m) {
-            var w = false, u = '<?php echo $controller->getURL($context) ?>';
-            m.addEventListener('play', function() {
-                if (!w) {
-                    WDN.jQuery.post(u, {action: "playcount"});
-                    w = true;
-                }
-            });
-        }
-    });
-    WDN.initializePlugin('mediaelement_wdn');
-};
-if (typeof(WDN) === 'undefined') {
-    t = '//www.unl.edu/';
-    l = function() {
-        l = function(){}; WDN.template_path = t; r();
-    };
-    j = document.createElement('script'); j.type = 'text/javascript'; j.src = t + 'wdn/templates_3.1/scripts/wdn.js';
-    j.onreadystatechange = function() {
-        if (j.readyState == 'loaded' || j.readyState == 'complete') {
-            l();
-        }
-    };
-    j.onload = l; document.getElementsByTagName("head")[0].appendChild(j);
-} else {
-    r();
-}
-})();
-</script>
+<script type="text/javascript" src="<?php echo $controller->getURL($context)?>/embed"></script>
---
+++ www/templates/js/Media/Embed.tpl.php
@@ -0,0 +1,37 @@
+(function () {
+    var j, l, t, r = function () {
+        WDN.setPluginParam('mediaelement_wdn', 'options', {
+            success: function (m) {
+                var w = false, u = '<?php echo $controller->getURL($context->media) ?>';
+                m.addEventListener('play', function () {
+                    if (!w) {
+                        WDN.jQuery.post(u, {action: "playcount"});
+                        w = true;
+                    }
+                });
+            }
+        });
+        WDN.initializePlugin('mediaelement_wdn');
+    };
+    if (typeof(WDN) === 'undefined') {
+        t = '//www.unl.edu/';
+        l = function () {
+            l = function () {
+            };
+            WDN.template_path = t;
+            r();
+        };
+        j = document.createElement('script');
+        j.type = 'text/javascript';
+        j.src = t + 'wdn/templates_3.1/scripts/wdn.js';
+        j.onreadystatechange = function () {
+            if (j.readyState == 'loaded' || j.readyState == 'complete') {
+                l();
+            }
+        };
+        j.onload = l;
+        document.getElementsByTagName("head")[0].appendChild(j);
+    } else {
+        r();
+    }
+})();
