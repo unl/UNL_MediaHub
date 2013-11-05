cca3b0681ecdd653a6dcf972aec9c1faeae3b215
Michael Fairchild <mfairchild365@gmail.com>
2013-11-05 16:21:42 -0600

    have the embed script inject both html and js
    This will allow mediahub to update embedded scripts, rather than site admin individually updating them.

--- www/templates/js/Media/Embed.tpl.php
+++ www/templates/js/Media/Embed.tpl.php
@@ -1,39 +1,14 @@
-(function () {
-    var j, l, t, r = function () {
-        <?php if ($context->media) { ?>
-        WDN.setPluginParam('mediaelement_wdn', 'options', {
-            success: function (m) {
-                var w = false, u = '<?php echo $controller->getURL($context->media) ?>';
-                m.addEventListener('play', function () {
-                    if (!w) {
-                        WDN.jQuery.post(u, {action: "playcount"});
-                        w = true;
-                    }
-                });
-            }
-        });
-        <?php } ?>
-        WDN.initializePlugin('mediaelement_wdn');
-    };
-    if (typeof(WDN) === 'undefined') {
-        t = '//www.unl.edu/';
-        l = function () {
-            l = function () {
-            };
-            WDN.template_path = t;
-            r();
-        };
-        j = document.createElement('script');
-        j.type = 'text/javascript';
-        j.src = t + 'wdn/templates_3.1/scripts/wdn.js';
-        j.onreadystatechange = function () {
-            if (j.readyState == 'loaded' || j.readyState == 'complete') {
-                l();
-            }
-        };
-        j.onload = l;
-        document.getElementsByTagName("head")[0].appendChild(j);
-    } else {
-        r();
-    }
-})();
+<?php
+$markup = $savvy->render($context->media, 'MediaPlayer.tpl.php');
+
+//Add slashes so that it is javascirpt safe
+$markup = addslashes($markup);
+
+//Replace newlines with spaces
+$markup = trim(preg_replace('/\s+/', ' ', $markup));
+?>
+var markup = "<?php echo $markup?>";
+
+WDN.loadJQuery(function() {
+    WDN.jQuery('#mediahub_embed_<?php echo $context->media->id?>').html(markup);
+});
