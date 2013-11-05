c4ffe18b1c76274c053c12b7076f3f935f7938fb
Michael Fairchild <mfairchild365@gmail.com>
2013-11-05 16:20:04 -0600

    Move js back into MediaPlayer.tpl.php
    This is is so that all content that should be embedded is in the same place.  Embed.tpl.php will contain a reference to this.

--- www/templates/html/MediaPlayer.tpl.php
+++ www/templates/html/MediaPlayer.tpl.php
@@ -4,12 +4,47 @@ if ($context->isVideo()) {
 } else {
     echo $savvy->render($context, 'MediaPlayer/Audio.tpl.php');
 }
-
-$url = '';
-if (isset($context->id)) {
-    $url = $context->id . '/';
-}
 ?>
 
-<script type="text/javascript" src="<?php echo UNL_MediaHub_Controller::$url?>media/<?php echo $url ?>embed"></script>
-
+<script type="text/javascript">
+    (function () {
+        var j, l, t, i, r = function () {
+            <?php if ($context->id) { ?>
+            i = <?php echo $context->id ?>;
+            WDN.setPluginParam('mediaelement_wdn', 'options', {
+                success: function (m) {
+                    var w = false, u = '<?php echo $controller->getURL($context) ?>';
+                    m.addEventListener('play', function () {
+                        if (!w) {
+                            WDN.jQuery.post(u, {action: "playcount"});
+                            w = true;
+                        }
+                    });
+                }
+            });
+            <?php } ?>
+            WDN.initializePlugin('mediaelement_wdn');
+        };
+        if (typeof(WDN) === 'undefined') {
+            t = '//www.unl.edu/';
+            l = function () {
+                l = function () {
+                };
+                WDN.template_path = t;
+                r();
+            };
+            j = document.createElement('script');
+            j.type = 'text/javascript';
+            j.src = t + 'wdn/templates_3.1/scripts/wdn.js';
+            j.onreadystatechange = function () {
+                if (j.readyState == 'loaded' || j.readyState == 'complete') {
+                    l();
+                }
+            };
+            j.onload = l;
+            document.getElementsByTagName("head")[0].appendChild(j);
+        } else {
+            r();
+        }
+    })();
+</script>
