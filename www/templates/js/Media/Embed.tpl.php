57a1c91550a8a3f7bd5f552e67b6e613b840d5a6
Michael Fairchild <mfairchild365@gmail.com>
2013-11-05 16:35:10 -0600

    move js to load WDN to the embed template
    the embed template will run first, and we need to reference WDN in that script.  So, load it there.

--- www/templates/js/Media/Embed.tpl.php
+++ www/templates/js/Media/Embed.tpl.php
@@ -9,6 +9,31 @@ $markup = trim(preg_replace('/\s+/', ' ', $markup));
 ?>
 var markup = "<?php echo $markup?>";
 
-WDN.loadJQuery(function() {
-    WDN.jQuery('#mediahub_embed_<?php echo $context->media->id?>').html(markup);
-});
+(function () {
+    var j, l, t, i, r = function () {
+        WDN.loadJQuery(function() {
+            WDN.jQuery('#mediahub_embed_<?php echo $context->media->id?>').html(markup);
+        });
+    };
+    if (typeof(WDN) === 'undefined') {
+        t = '//www.unl.edu/';
+        l = function () {
+            l = function () {
+            };
+            WDN.template_path = t;
+            r();
+        };
+        j = document.createElement('script');
+        j.type = 'text/javascript';
+        j.src = t + 'wdn/templates_3.1/scripts/wdn.js';
+        j.onreadystatechange = function () {
+            if (j.readyState == 'loaded' || j.readyState == 'complete') {
+                l();
+            }
+        };
+        j.onload = l;
+        document.getElementsByTagName("head")[0].appendChild(j);
+    } else {
+        r();
+    }
+})();
