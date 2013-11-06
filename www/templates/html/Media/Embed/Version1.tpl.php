4798cd10f9033381e77202d1c4841f93ddad33af
Michael Fairchild <mfairchild365@gmail.com>
2013-11-06 16:21:24 -0600

    Only render what we need to

--- www/templates/html/Media/Embed/Version1.tpl.php
+++ www/templates/html/Media/Embed/Version1.tpl.php
@@ -1,6 +1,9 @@
 <?php
-$markup = $savvy->render($context->media, 'MediaPlayer.tpl.php');
-echo trim(preg_replace('/(<script[^>]*>.*<\/script>)/is', '', $markup));
+if ($context->media->isVideo()) {
+    echo $savvy->render($context->media, 'MediaPlayer/Video.tpl.php');
+} else {
+    echo $savvy->render($context->media, 'MediaPlayer/Audio.tpl.php');
+}
 ?>
 
 <script type="text/javascript" src="<?php echo UNL_MediaHub_Controller::$url?>media/<?php echo $context->media->id ?>/embed/1"></script>
