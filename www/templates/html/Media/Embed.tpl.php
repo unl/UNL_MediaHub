a63eec5263193e91dcb4305f1edfc716de441f44
Michael Fairchild <mfairchild365@gmail.com>
2013-11-06 09:29:17 -0600

    Add versioned embed scripts
    This will allow us to push out new embed code without breaking old code (in theory)

--- www/templates/html/Media/Embed.tpl.php
+++ www/templates/html/Media/Embed.tpl.php
@@ -1,10 +1,2 @@
 <?php
-$url = '';
-if (isset($context->media)) {
-    $url = $context->media->id . '/';
-}
-?>
-<script type="text/javascript" src="<?php echo UNL_MediaHub_Controller::$url?>media/<?php echo $url ?>embed"></script>
-<div id="mediahub_embed_<?php echo $context->media->id ?>">
-    <noscript>Sorry, we are unable to embed this media because you have javascript disabled.</noscript>
-</div>
+echo $savvy->render($context, 'Media/Embed/Version' .  $context->version . '.tpl.php');
