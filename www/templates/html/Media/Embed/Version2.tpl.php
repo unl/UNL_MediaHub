25a159a4f017cc6af697bbe3d2eb364cebf76a13
Michael Fairchild <mfairchild365@gmail.com>
2013-11-07 08:39:13 -0600

    Don't auto play embedded media by default

--- www/templates/html/Media/Embed/Version2.tpl.php
+++ www/templates/html/Media/Embed/Version2.tpl.php
@@ -1,4 +1,4 @@
 <div id="mediahub_embed_<?php echo $context->media->id ?>">
     <a href="<?php echo $controller->getURL($context->media)?>"><?php echo $context->media->title ?></a>
 </div>
-<script type="text/javascript" src="<?php echo UNL_MediaHub_Controller::$url?>media/<?php echo $context->media->id  ?>/embed/2"></script>
+<script type="text/javascript" src="<?php echo UNL_MediaHub_Controller::$url?>media/<?php echo $context->media->id  ?>/embed/2?autoplay=0"></script>
