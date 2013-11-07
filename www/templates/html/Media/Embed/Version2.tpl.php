22a24229d44f8a367c2e03936e7e66af7f8216c2
Michael Fairchild <mfairchild365@gmail.com>
2013-11-07 08:36:39 -0600

    reference the v2 js

--- www/templates/html/Media/Embed/Version2.tpl.php
+++ www/templates/html/Media/Embed/Version2.tpl.php
@@ -1,4 +1,4 @@
 <div id="mediahub_embed_<?php echo $context->media->id ?>">
     <a href="<?php echo $controller->getURL($context->media)?>"><?php echo $context->media->title ?></a>
 </div>
-<script type="text/javascript" src="<?php echo UNL_MediaHub_Controller::$url?>media/<?php echo $context->media->id  ?>/embed"></script>
+<script type="text/javascript" src="<?php echo UNL_MediaHub_Controller::$url?>media/<?php echo $context->media->id  ?>/embed/2"></script>
