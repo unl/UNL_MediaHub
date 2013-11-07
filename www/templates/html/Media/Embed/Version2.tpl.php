63c449659f7a2eec5ca1b3b0c7ef89d733eefe52
Michael Fairchild <mfairchild365@gmail.com>
2013-11-07 08:49:59 -0600

    add a class to the embed

--- www/templates/html/Media/Embed/Version2.tpl.php
+++ www/templates/html/Media/Embed/Version2.tpl.php
@@ -1,4 +1,4 @@
-<div id="mediahub_embed_<?php echo $context->media->id ?>" data-mediahub-embed-version="2">
+<div id="mediahub_embed_<?php echo $context->media->id ?>" class="mediahub-embed" data-mediahub-embed-version="2">
     <a href="<?php echo $controller->getURL($context->media)?>"><?php echo $context->media->title ?></a>
 </div>
 <script type="text/javascript" src="<?php echo UNL_MediaHub_Controller::$url?>media/<?php echo $context->media->id  ?>/embed/2?autoplay=0"></script>
