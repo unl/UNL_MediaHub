77705dd545fc705fc3ac325ad546986b060371b6
Michael Fairchild <mfairchild365@gmail.com>
2013-11-07 08:23:25 -0600

    Move the script tag under the div
    This way the div exists in the DOM before the script is executed

--- www/templates/html/Media/Embed/Version2.tpl.php
+++ www/templates/html/Media/Embed/Version2.tpl.php
@@ -4,7 +4,7 @@ if (isset($context->media)) {
     $url = $context->media->id . '/';
 }
 ?>
-<script type="text/javascript" src="<?php echo UNL_MediaHub_Controller::$url?>media/<?php echo $url ?>embed"></script>
 <div id="mediahub_embed_<?php echo $context->media->id ?>">
     <a href="<?php echo $controller->getURL($context->media)?>"><?php echo $context->media->title ?></a>
 </div>
+<script type="text/javascript" src="<?php echo UNL_MediaHub_Controller::$url?>media/<?php echo $url ?>embed"></script>
