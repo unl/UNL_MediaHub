ab464825929ead85c5238bf633421e59eb0039ee
Michael Fairchild <mfairchild365@gmail.com>
2013-11-08 10:47:32 -0600

    Still show the image, just hide the button

--- www/manager/templates/Media/Preview/Video.tpl.php
+++ www/manager/templates/Media/Preview/Video.tpl.php
@@ -5,12 +5,13 @@
         <li>Click the "Set Image" button to save this as your image representation.</li>
         <li>Continue with the form below.</li>
     </ol>
+    
+    <h5 class="sec_header">Your Image</h5>
+    <div id="imageOverlay">
+        <p>We're updating your image, this may take a few minutes depending on video length. <strong>Now is a good time to make sure the information below is up to snuff!</strong></p>
+    </div>
+    <img src="<?php echo $context->getThumbnailURL(); ?>" id="thumbnail" alt="Thumbnail preview" />
     <div id="poster_picker">
-        <h5 class="sec_header">Your Image</h5>
-        <div id="imageOverlay">
-            <p>We're updating your image, this may take a few minutes depending on video length. <strong>Now is a good time to make sure the information below is up to snuff!</strong></p>
-        </div>
-        <img src="<?php echo $context->getThumbnailURL(); ?>" id="thumbnail" alt="Thumbnail preview" />
         <a class="action" id="setImage" href="#">Set Image</a>
     </div>
     <div id="poster_picker_disabled">
