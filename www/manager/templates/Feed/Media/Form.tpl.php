00d36dc9c1f2b7c3497b26da7c73a544be8a9877
Michael Fairchild <mfairchild365@gmail.com>
2013-11-08 09:39:13 -0600

    Make help text medium agnostic
    Could be for audio or video

--- www/manager/templates/Feed/Media/Form.tpl.php
+++ www/manager/templates/Feed/Media/Form.tpl.php
@@ -69,7 +69,7 @@ if (isset($context->media)) {
                     ?>
                 </select>
             </li>
-            <li><label>URL of custom poster image<span class="helper">If filled in, this image will override the image chosen above.</span></label>
+            <li><label>URL of custom poster image<span class="helper">If filled in, this image will be displayed as the thumbnail for the media.</span></label>
                 <input id="media_poster" name="poster" type="text" class="validate-url" value="<?php echo htmlentities(@$context->media->poster, ENT_QUOTES); ?>" />
             </li>
             <li style="display:none;"><label for="submit_existing" class="element">&nbsp;</label><div class="element"><input id="submit_existing" name="submit_existing" value="Save" type="submit" /></div></li>
