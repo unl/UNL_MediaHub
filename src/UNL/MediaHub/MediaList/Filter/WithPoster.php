6668cb7f177192fa43abd61b2c244bfa9529967d
Michael Fairchild <mfairchild365@gmail.com>
2013-11-11 16:08:25 -0600

    Add a filter for selecting media with posters

---
+++ src/UNL/MediaHub/MediaList/Filter/WithPoster.php
@@ -0,0 +1,38 @@
+<?php
+class UNL_MediaHub_MediaList_Filter_WithPoster implements UNL_MediaHub_Filter
+{
+    function __construct()
+    {
+        
+    }
+
+    function apply(Doctrine_Query &$query)
+    {
+        $query->where('m.poster IS NOT NULL AND m.poster != ""');
+    }
+
+    function getLabel()
+    {
+        return 'Media with posters';
+    }
+
+    function getType()
+    {
+        return '';
+    }
+
+    function getValue()
+    {
+        return '';
+    }
+
+    function __toString()
+    {
+        return '';
+    }
+
+    public static function getDescription()
+    {
+        return 'Find media with custom poster images defined';
+    }
+}
