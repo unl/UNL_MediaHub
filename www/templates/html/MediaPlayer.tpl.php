79e0fc817cdaf99c84f9e5209a1b5200098a083b
Michael Fairchild <mfairchild365@gmail.com>
2013-11-11 10:02:17 -0600

    Add social sharing

--- www/templates/html/MediaPlayer.tpl.php
+++ www/templates/html/MediaPlayer.tpl.php
@@ -12,7 +12,8 @@ if ($context->isVideo()) {
             <?php if (isset($context->id) && $context->id) { ?>
             i = <?php echo $context->id ?>;
             WDN.setPluginParam('mediaelement_wdn', 'options', {
-                success: function (m) {
+                success: function (m, v) {
+                    //Playcount
                     var w = false, u = '<?php echo $controller->getURL($context) ?>';
                     m.addEventListener('play', function () {
                         if (!w) {
@@ -20,6 +21,106 @@ if ($context->isVideo()) {
                             w = true;
                         }
                     });
+
+                    //Social Sharing
+                    var initSharing = function(m, v) {
+                        if( v.tagName != 'VIDEO' ) {
+                            return;
+                        }
+                        
+                        //Load the CSS
+                        WDN.loadCSS('<?php echo $controller->getURL(); ?>templates/html/css/share.css');
+
+                        var $ = WDN.jQuery.noConflict( );
+                        var $inner      = false;
+                        var $video      = $(v);
+                        var $title      = $video.attr('title');
+                        var share_url   = $video.attr('data-url');
+                        
+                        if (!share_url) {
+                            return;
+                        }
+                        
+                        if ($inner = $(v).parents('.mejs-video')) {
+                            // share urls
+                            var sharelinks = {
+                                tw:     'http://twitter.com/share?text=Video: ' + $title + '&url=' + share_url, // twitter
+                                fb:     'https://www.facebook.com/sharer/sharer.php?url=' + share_url,	// facebook
+                                su:     'http://www.stumbleupon.com/submit?title=' + $title + '&url=' + share_url, //stumble upon
+                                gp:     'https://plus.google.com/share?url=' + share_url, //google plus
+                                em:     'http://api.addthis.com/oexchange/0.8/forward/email/offer?url=' + share_url //email
+                            }
+
+                            //create share links
+                            var links = '';
+                            for (var key in sharelinks) {
+                                links += '<a href="#" rel="nofollow" class="'+key+'">';
+                            }
+
+                            $inner.prepend('<div class="media-content-title">' + $title + '</div>');
+                            $inner.prepend('<a her="#" rel="nofollow" class="share-video-link">' + 'Share' + '</a>');
+
+                            var html   = '<div class="share-video-form">';
+                            html += '<em class="share-video-close">x</em><h4>' + 'share video' + '</h4>';
+                            html += '<em>'+ 'link' +'</em><input type="text" class="share-video-lnk share-data" value="' + share_url + '" />' ;
+
+                            html += '<div class="video-social-share">' + links + '</div>' ;
+                            $inner.prepend(html + '</div>');
+
+                            // start listeners
+                            $sharelink  = $inner.find('.share-video-link');
+                            $sharefrom  = $inner.find('.share-video-form');
+                            $closelink  = $inner.find('.share-video-close');
+                            $videotitle = $inner.find('.media-content-title');
+
+                            // hide form when video is playing
+                            m.addEventListener('play', function(e) {
+                                $sharelink.hide(); $sharefrom.hide(); $videotitle.hide();
+                            }, false );
+
+                            // show form when video is paused
+                            m.addEventListener('pause', function(e) {
+                                $sharelink.removeClass('video-active');
+                                $inner.find('.mejs-overlay-button').show( );
+                                $videotitle.show();
+                                $sharelink.show();
+                            }, false );
+
+                            // close video form
+                            var video_close_share_form = function(){
+                                $sharefrom.hide();
+                                $sharelink.removeClass('video-active');
+
+                                $inner.find('.mejs-overlay-play')
+                                    .removeClass('share-overlay');
+                            };
+
+                            // show / hide video form
+                            $closelink.bind('click', video_close_share_form );
+                            $sharelink.bind('click', function(){
+
+                                if( $sharefrom.is(':hidden')) {
+                                    $sharefrom.show( );
+                                    $sharelink.addClass('video-active');
+
+                                    $inner.find('.mejs-overlay-play')
+                                        .addClass('share-overlay').show();
+                                } else {
+                                    video_close_share_form();
+                                }
+                            });
+
+                            // add share links listener
+                            $inner.find('.video-social-share a').click( function(){
+                                key = $(this).attr('class');
+                                if(sharelinks[key]) {
+                                    window.open(sharelinks[key]);
+                                }
+                            });
+                        }
+                    }
+
+                    initSharing(m, v);
                 }
             });
             <?php } ?>
--- www/templates/html/MediaPlayer/Video.tpl.php
+++ www/templates/html/MediaPlayer/Video.tpl.php
@@ -19,6 +19,6 @@ if (isset($controller->options['autoplay']) && !$controller->options['autoplay']
 }
 
 ?>
-<video class="wdn_player" style="width:100%;height:100%" <?php echo $autoplay; ?> src="<?php echo $context->url; ?>" controls poster="<?php echo $context->getThumbnailURL(); ?>" title="<?php echo $context->title; ?>" crossorigin="anonymous">
+<video class="wdn_player" style="width:100%;height:100%" <?php echo $autoplay; ?> src="<?php echo $context->url; ?>" controls data-url="<?php echo $controller->getURL($context); ?>" poster="<?php echo $context->getThumbnailURL(); ?>" title="<?php echo $context->title; ?>" crossorigin="anonymous">
 	<track src="<?php echo $context->getVideoTextTrackURL(); ?>" kind="subtitles" srclang="en" />
 </video>
