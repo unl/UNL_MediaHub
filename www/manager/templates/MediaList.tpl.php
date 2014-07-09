5ee6f1c2add74406492f6eb5df905d7ceb9c8095
Michael Fairchild <mfairchild365@gmail.com>
2014-07-09 16:47:39 -0500

    Update media manager media lists
    Mostly the same logic as the main media lists, but slightly different in key areas, such as how to check if you can edit, etc.  This is mainly due to differences is the controller...  I should explore reusing the code more here.

--- www/manager/templates/MediaList.tpl.php
+++ www/manager/templates/MediaList.tpl.php
@@ -18,27 +18,33 @@ if (count($context->items)) {
     echo '<ul class="medialist">';
     foreach ($context->items as $media) { ?>
         <li>
-            <a href="<?php echo UNL_MediaHub_Controller::getURL($media); ?>"><img class="thumbnail" src="<?php echo $media->getThumbnailURL(); ?>" alt="Thumbnail preview for <?php echo htmlentities($media->title, ENT_QUOTES); ?>" width="100" height="76" /></a>
-            <div class="actions">
-            <a href="<?php echo $addMediaURL; ?>&amp;id=<?php echo $media->id; ?>">Edit</a>
-            <?php
-            echo $savvy->render($media, 'Media/DeleteForm.tpl.php');
-            ?>
-            </div>
-            <div class="metaInfo">
-            <h4><a href="<?php echo UNL_MediaHub_Controller::getURL($media); ?>"><?php echo htmlspecialchars($media->title); ?></a></h4>
-            <?php
-            $element = $media->datecreated;
-                    echo '<h6 class="subhead">Added on '.date("F j, Y, g:i a", strtotime($element)).'</h6>';
-                    
-            $summary = $media->description;
-            if ($element = UNL_MediaHub_Feed_Media_NamespacedElements_itunes::mediaHasElement($media->id, 'summary')) {
-                $summary .= '<span class="itunes_summary">'.$element->value.'</span>';
-            }
-            $summary = strip_tags($summary, '<a><img>');
-            $summary = str_replace('Related Links', '', $summary);
-            ?>
-            <p><?php echo $summary; ?></p>
+            <div class="wdn-grid-set">
+                <div class="wdn-col-one-fourth center-text">
+                    <a href="<?php echo UNL_MediaHub_Controller::getURL($media); ?>"><img class="thumbnail" src="<?php echo $media->getThumbnailURL(); ?>" alt="Thumbnail preview for <?php echo htmlspecialchars($media->title, ENT_QUOTES); ?>" /></a>
+                    <div class="actions">
+                        <a href="<?php echo $addMediaURL; ?>&amp;id=<?php echo $media->id; ?>">Edit</a>
+                        <?php
+                        echo $savvy->render($media, 'Media/DeleteForm.tpl.php');
+                        ?>
+                    </div>
+                </div>
+                <div class="wdn-col-three-fourths metaInfo">
+                    <div class="wdn-head">
+                        <a href="<?php echo UNL_MediaHub_Controller::getURL($media); ?>"><?php echo htmlspecialchars($media->title); ?></a>
+                    </div>
+                    <?php
+                    $element = $media->datecreated;
+                    echo '<div class="wdn-subhead subhead">Added on '.date("F j, Y, g:i a", strtotime($element)).'</div>';
+
+                    $summary = $media->description;
+                    if ($element = UNL_MediaHub_Feed_Media_NamespacedElements_itunes::mediaHasElement($media->id, 'summary')) {
+                        $summary .= '<span class="itunes_summary">'.$element->value.'</span>';
+                    }
+                    $summary = strip_tags($summary, '<a><img>');
+                    $summary = str_replace('Related Links', '', $summary);
+                    ?>
+                    <p><?php echo $summary; ?></p>
+                </div>
             </div>
         </li>
     <?php  
