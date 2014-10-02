0766cdcbda7ac6355ad919e8e0e7fa244282ec3d
Michael Fairchild <mfairchild365@gmail.com>
2014-10-02 16:43:30 -0500

    Fix variable references

--- vagrant/dev/puphpet/files/exec-once/001-install.sh
+++ vagrant/dev/puphpet/files/exec-once/001-install.sh
@@ -9,17 +9,17 @@ git submodule init
 git submodule update
 
 #copy .htaccess
-if [ ! -f ${$BASEDIR}/www/.htaccess ]; then
+if [ ! -f ${BASEDIR}/www/.htaccess ]; then
     echo "Creating .htaccess"
-    cp ${$BASEDIR}/www/sample.htaccess ${$BASEDIR}/www/.htaccess
+    cp ${BASEDIR}/www/sample.htaccess ${BASEDIR}/www/.htaccess
 fi
 
 #copy config
-if [ ! -f ${$BASEDIR}/config.inc.php ]; then
+if [ ! -f ${BASEDIR}/config.inc.php ]; then
     echo "Creating config.inc.php"
-    cp ${$BASEDIR}/config.sample.php ${$BASEDIR}/config.inc.php
+    cp ${BASEDIR}/config.sample.php ${BASEDIR}/config.inc.php
 fi
 
-php ${$BASEDIR}/upgrade.php
+php ${BASEDIR}/upgrade.php
 
 echo "FINISHED installing mediahub"
