e867f37f5afc78f6663d33af44c58e09ad38553c
spennythug <spencermccoy@ucommklusman.unl.edu>
2014-10-07 11:27:39 -0500

    Front page styling
    Styling the front page

--- www/templates/html/DefaultHomepage.tpl.php
+++ www/templates/html/DefaultHomepage.tpl.php
@@ -3,9 +3,9 @@
             <div class="parallax-image band-020">
                 <div id="video-container">
                     <video id="video-player" preload="metadata" autoplay="autoplay" loop="" class="fillWidth">
-                        <source src="http://admissions.unl.edu/includes/videos/apply/apply-loop.mp4" type="video/mp4">
-                        <source src="http://admissions.unl.edu/includes/videos/apply/apply-loop.webm" type="video/webm">
-                        <source src="http://admissions.unl.edu/includes/videos/apply/apply-loop.ogg" type="video/ogg">
+                        <source src="http://admissions.unl.edu/includes/videos/why-unl/why-unl.mp4" type="video/mp4">
+                        <source src="http://admissions.unl.edu/includes/videos/why-unl/why-unl.webm" type="video/webm">
+                        <source src="http://admissions.unl.edu/includes/videos/why-unl/why-unl.ogg" type="video/ogg">
                         Your browser does not support the video tag. I suggest you upgrade your browser.
                     </video>
                 </div>
@@ -34,69 +34,53 @@ WDN.jQuery("#wdn_search_query").unbind("keyup");
 
 
 <div class="wdn-band">
-    <div class="wdn-grid-set"></div>
+    <div class="wdn-inner-wrapper"><div class="bp2-wdn-grid-set-thirds wdn-center">
+            
+        <div class="wdn-col">
+        <div class="mh-featured-icon centered"><div class="wdn-icon-wrench"></div></div>
+        <h3>MANAGE MEDIA</h3>
+        <p>Channels are groups of related media. Any media can be part of any channel. Create your own.</p>
+    </div>
+        <div class="wdn-col">
+         <div class="mh-featured-icon centered"><div class="wdn-icon-search"></div></div>
+         <h3>BROWSE VIDEOS</h3>
+        <p>Channels are groups of related media. Any media can be part of any channel. Create your own.</p>
+    </div>
+        <div class="wdn-col">
+         <div class="mh-featured-icon centered"><div class="wdn-icon-info"></div></div>
+         <h3>EXPLORE CHANNELS</h3>
+        <p>Channels are groups of related media. Any media can be part of any channel. Create your own.</p>
+        </div>
+
+        </div>
+        </div>
+</div>
+
+<div class="wdn-band">
+
+<div class="wdn-inner-wrapper"><div class="bp1-wdn-grid-set-thirds">
+	
+<div class="wdn-col">
+	
+<div class="mh-video-thumb wdn-center">
+<img src="http://localhost:8007/channels/620/image">
+<div class="mh-video-label">
+	<h6 class="wdn-brand">Video Title<span class="wdn-subhead">September 19, 2014, 8:33 am</span></h6>
+</div>
+</div>
+
+</div>
+
+</div></div>
+
 </div>
 
 
-<div class="wdn-grid-set">
 
-    <div class="bp1-wdn-col-three-fourths feature">
-        <div class="group">
-            <h3>Media</h3>
-            <ul class="switcher">
-                <li>
-                    <a href="#top_media" class="selected">Featured</a>
-                </li>
-                <li>
-                    <a href="#latest_media">Latest</a>
-                </li>
-            </ul>
-        </div>
-        <div class="wdn_tabs_content">
-            <div id="top_media">
-                <?php echo $savvy->render($context->top_media); ?>
-            </div>
-            <div id="latest_media">
-                <?php echo $savvy->render($context->latest_media); ?>
-            </div>
-        </div>
-        <div id="exploreMedia">
-            <h3>Explore MediaHub</h3>
-            <div class="box_display">
-                <div class="exp_channels">
-                    <h4>Channels</h4>
-                    <p>Channels are groups of related media. Any media can be part of any channel. Create your own channel of your favorite media!</p>
-                    <a href="channels/">Explore all Channels</a>
-                </div>
-                <div class="exp_tags">
-                    <h4>Tags</h4>
-                    <p>Tags will allow you to find related media, build dynamic collections and organize your media!</p>
-                    <p>Coming soon!</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="bp1-wdn-col-one-fourth">
-        <a href="<?php echo UNL_MediaHub_Controller::getURL(); ?>manager/" class="wdn-button wdn-button-triad">Add your media</a>
-        <?php
-        echo $savvy->render($context->featured_channels, 'CompactFeedList.tpl.php');
-        ?>
-        <a href="<?php echo UNL_MediaHub_Controller::getURL(); ?>channels/" class="wdn-button">See all channels</a>
-    </div>
-</div>
 <script type="text/javascript">
-WDN.jQuery('document').ready(function($){
-	$('#latest_media').hide();
-	$('ul.switcher a').click(function(e){
-		if (!($(this).hasClass('selected'))) {
-			$('ul.switcher a').each(function(){
-				$(this).toggleClass('selected');
-			});
-			$('.wdn_tabs_content ' + $(this).attr('href')).toggle();
-			$('.wdn_tabs_content > div').not($(this).attr('href')).hide();
-		}
-		e.preventDefault();
-		return false;
-	});
-});
+
+	var vid = document.getElementById("video-player"); // run main video at half speed
+	vid.playbackRate = 0.25;
+
+
 </script>
