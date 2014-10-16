3e84a279e4952dd7747498e56818eec1be9295ba
Michael Fairchild <mfairchild365@gmail.com>
2014-10-16 13:23:22 -0500

    Render teasers instead of hard coded thumbnails

--- www/templates/html/MediaList.tpl.php
+++ www/templates/html/MediaList.tpl.php
@@ -51,45 +51,19 @@ if (count($context->items)) {
     	}
 	    ?>
     </div>
-        <ul class="medialist">
-    
-        <?php
-        foreach ($context->items as $media) { ?>
-            <li>
-                <div class="wdn-grid-set">
-                    <div class="wdn-col-one-fourth center-text">
-                        <a href="<?php echo UNL_MediaHub_Controller::getURL($media); ?>"><img class="thumbnail" src="<?php echo $media->getThumbnailURL(); ?>" alt="Thumbnail preview for <?php echo htmlspecialchars($media->title, ENT_QUOTES); ?>" /></a>
-                        <?php if ($userCanEdit) { ?>
-                            <div class="actions">
-                                <a href="<?php echo $addMediaURL; ?>&amp;id=<?php echo $media->id; ?>">Edit</a>
-                                <?php
-                                echo $savvy->render($media, 'manager/templates/Media/DeleteForm.tpl.php');
-                                ?>
-                            </div>
-                        <?php }?>
-                    </div>
-                    <div class="wdn-col-three-fourths metaInfo">
-                        <div class="wdn-head">
-                            <a href="<?php echo UNL_MediaHub_Controller::getURL($media); ?>"><?php echo htmlspecialchars($media->title); ?></a>
-                        </div>
-                        <?php
-                        $element = $media->datecreated;
-                        echo '<div class="wdn-subhead subhead">Added on '.date("F j, Y, g:i a", strtotime($element)).'</div>';
 
-                        $summary = $media->description;
-                        if ($element = UNL_MediaHub_Feed_Media_NamespacedElements_itunes::mediaHasElement($media->id, 'summary')) {
-                            $summary .= '<span class="itunes_summary">'.$element->value.'</span>';
-                        }
-                        $summary = strip_tags($summary, '<a><img>');
-                        $summary = str_replace('Related Links', '', $summary);
-                        ?>
-                        <p><?php echo $summary; ?></p>
-                    </div>
-                </div>
-            </li>
-        <?php  
-        } ?>
-        </ul>
+    <div class="bp2-wdn-grid-set-thirds">
+        <?php
+        foreach ($context->items as $media):
+            ?>
+            <div class="wdn-col">
+                <?php echo $savvy->render($media, 'templates/html/Media/teaser.tpl.php'); ?>
+            </div>
+        <?php
+        endforeach;
+        ?>
+    </div>
+        
         <em>Displaying <?php echo $context->first; ?> through <?php echo $context->last; ?> out of <?php echo $context->total; ?></em>
         <?php echo $pager_links; ?>
 <?php
