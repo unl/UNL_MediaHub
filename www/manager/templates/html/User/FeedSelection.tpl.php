d72c0ccc840a3bb7d42940ba13ebb924a5b2cf81
Michael Fairchild <mfairchild365@gmail.com>
2014-10-30 09:11:26 -0500

    Use a fieldset to provide context

--- www/manager/templates/html/User/FeedSelection.tpl.php
+++ www/manager/templates/html/User/FeedSelection.tpl.php
@@ -1,12 +1,15 @@
-<label for="channels">Channels
-    <span class="required">*</span>
-</label>
-<div class="mh-channel-box">
-    <ul>
-        <?php
-        $list = UNL_MediaHub_Manager::getUser()->getFeeds(array('limit'=>300));
-        echo $savvy->render($list, 'Feed/Media/FeedList.tpl.php');
-        ?>
-        <li><label for="new_feed" class="element">New Channel</label><div class="element"><input id="new_feed" name="new_feed" type="text" /></div></li>
-    </ul>
-</div>
+<fieldset>
+    <legend>
+        Channels
+        <span class="required">*</span>
+    </legend>
+    <div class="mh-channel-box">
+        <ul>
+            <?php
+            $list = UNL_MediaHub_Manager::getUser()->getFeeds(array('limit'=>300));
+            echo $savvy->render($list, 'Feed/Media/FeedList.tpl.php');
+            ?>
+            <li><label for="new_feed" class="element">New Channel</label><div class="element"><input id="new_feed" name="new_feed" type="text" /></div></li>
+        </ul>
+    </div>
+</fieldset>
