c01bed0aaab3ddec899356357a34cb0853114f4b
Michael Fairchild <mfairchild365@gmail.com>
2014-11-05 15:04:29 -0600

    Fix method reference

--- src/UNL/MediaHub/Feed/Media/NamespacedElements/geo.php
+++ src/UNL/MediaHub/Feed/Media/NamespacedElements/geo.php
@@ -17,6 +17,6 @@ class UNL_MediaHub_Feed_Media_NamespacedElements_geo extends UNL_MediaHub_Feed_M
     
     public static function mediaHasElement($media_id, $element)
     {
-        return UNL_MediaHub_Feed_Media_NamespacedElements::mediaHasElement($media_id, $element, 'geo');
+        return UNL_MediaHub_Feed_Media_NamespacedElements::mediaHasElementNS($media_id, $element, 'geo');
     }
 }
