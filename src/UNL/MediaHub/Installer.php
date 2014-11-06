8837307dabbc1d9bddfaea42e56a8b6a9cb1d9f2
Michael Fairchild <mfairchild365@gmail.com>
2014-11-06 11:19:59 -0600

    Move install/upgrade logic to a reusable class
    This is so that the database structure can by auto-generated on a per-db-test basis.

---
+++ src/UNL/MediaHub/Installer.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The Installer is a class to help manage the installation and upgrade of the database
+ */
+class UNL_MediaHub_Installer
+{
+    /**
+     * @var UNL_MediaHub
+     */
+    protected $mediahub;
+    
+    public function __construct(UNL_MediaHub $mediahub)
+    {
+        $this->mediahub = $mediahub;
+    }
+    
+    public function install()
+    {
+        return $this->upgrade();
+    }
+    
+    public function upgrade()
+    {
+        $messages = array();
+
+        $messages[] = $this->exec_sql(file_get_contents(dirname(__FILE__).'/../../data/mediahub.sql'), 'Initializing database structure');
+        $messages[] = $this->exec_sql(file_get_contents(dirname(__FILE__).'/../../data/add_dateupdated.sql'), 'Add date updated', true);
+        $messages[] = $this->exec_sql(file_get_contents(dirname(__FILE__).'/../../data/add_feed_image.sql'), 'Adding feed image support', true);
+        $messages[] = $this->exec_sql(file_get_contents(dirname(__FILE__).'/../../data/add_featured_feed_fields.sql'), 'Adding featured feeds support', true);
+        $messages[] = $this->exec_sql(file_get_contents(dirname(__FILE__).'/../../data/add_media_privacy.sql'), 'Adding media privacy settings', true);
+        $messages[] = $this->exec_sql(file_get_contents(dirname(__FILE__).'/../../data/add_media_play_count.sql'), 'Adding media play count', true);
+        $messages[] = $this->exec_sql(file_get_contents(dirname(__FILE__).'/../../data/add_media_poster.sql'), 'Adding media poster', true);
+        $messages[] = $this->exec_sql(file_get_contents(dirname(__FILE__).'/../../data/add_media_uid.sql'), 'Adding uidcreated and uidupdated to media', true);
+        
+        return $messages;
+    }
+
+    protected function exec_sql($sql, $message, $fail_ok = false)
+    {
+        $db = $this->mediahub->getDB();
+
+        try {
+            $result = $db->execute($sql);
+        } catch (Exception $e) {
+            if (!$fail_ok) {
+                throw new UNL_MediaHub_RuntimeException('Query Failed: "' . $message . '" - ' . $db->errorInfo(), 500);
+            }
+        }
+        
+        return $message;
+    }
+}
--- upgrade.php
+++ upgrade.php
@@ -6,37 +6,12 @@ if (file_exists(dirname(__FILE__).'/config.inc.php')) {
 }
 
 echo 'Connecting to the database&hellip;';
-$mediahub = new UNL_MediaHub($dsn);
-$db = $mediahub->getDB();
-echo 'connected successfully!<br />';
-/**
- * 
- * Enter description here ...
- * @param Doctrine_Connection $db
- * @param $sql
- * @param $message
- * @param $fail_ok
- */
-function exec_sql($db, $sql, $message, $fail_ok = false)
-{
-	echo $message.'&hellip;';
-	try {
-	   $result = $db->execute($sql);
-	} catch (Exception $e) {
-		if (!$fail_ok) {
-			echo 'The query failed:'.$db->errorInfo();
-			exit();
-		}
-	}
-	echo 'finished.<br />';
+$mediahub  = new UNL_MediaHub($dsn);
+$installer = new UNL_MediaHub_Installer($mediahub);
+$messages  = $installer->install();
+
+foreach ($messages as $message) {
+    echo $message . PHP_EOL;
 }
-exec_sql($db, file_get_contents(dirname(__FILE__).'/data/mediahub.sql'), 'Initializing database structure');
-exec_sql($db, file_get_contents(dirname(__FILE__).'/data/add_dateupdated.sql'), 'Add date updated', true);
-exec_sql($db, file_get_contents(dirname(__FILE__).'/data/add_feed_image.sql'), 'Adding feed image support', true);
-exec_sql($db, file_get_contents(dirname(__FILE__).'/data/add_featured_feed_fields.sql'), 'Adding featured feeds support', true);
-exec_sql($db, file_get_contents(dirname(__FILE__).'/data/add_media_privacy.sql'), 'Adding media privacy settings', true);
-exec_sql($db, file_get_contents(dirname(__FILE__).'/data/add_media_play_count.sql'), 'Adding media play count', true);
-exec_sql($db, file_get_contents(dirname(__FILE__).'/data/add_media_poster.sql'), 'Adding media poster', true);
-exec_sql($db, file_get_contents(dirname(__FILE__).'/data/add_media_uid.sql'), 'Adding uidcreated and uidupdated to media', true);
 
 echo 'Upgrade complete!';
