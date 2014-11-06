8254874fcadae3babd1a74b51cdef9060576d6b9
Michael Fairchild <mfairchild365@gmail.com>
2014-11-06 16:41:47 -0600

    Add classes for DBTest helpers

---
+++ src/UNL/MediaHub/DBTests/BaseTestDataInstaller.php
@@ -0,0 +1,16 @@
+<?php
+class UNL_MediaHub_DBTests_BaseTestDataInstaller implements UNL_MediaHub_DBTests_MockTestDataInstallerInterface
+{
+    /**
+     * This function should execute commands to install mock data to the test database.
+     */
+    public function install() {
+        //Create users
+        
+        //Create some channels
+        
+        //Add some permissions
+        
+        //add some media
+    }
+}
