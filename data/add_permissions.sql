9a6d13bea5193e4ad8ddb78ac486395f79392059
Michael Fairchild <mfairchild365@gmail.com>
2014-11-14 15:41:20 -0600

    Add and use a permissions table

---
+++ data/add_permissions.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
