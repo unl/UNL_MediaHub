359b27f5cc070dc947e92330d7a9ef2e03962425
Michael Fairchild <mfairchild365@gmail.com>
2014-11-18 16:24:40 -0600

    Improve client validation of channel picker
    Now supports "must select either an existing channel or a new channel"

--- www/manager/templates/html/Feed/Media/FeedList.tpl.php
+++ www/manager/templates/html/Feed/Media/FeedList.tpl.php
@@ -5,5 +5,5 @@ foreach ($context->items as $item=>$feed) {
         || (isset($_GET['feed_id']) && $_GET['feed_id'] == $feed->id))) {
         $checked = 'checked="checked"';
     }
-    echo '<li><input id="feed_id_'.$feed->id.'" name="feed_id[]" type="checkbox" '.$checked.' class="validate-one-required-by-name" value="'.$feed->id.'" /> <label for="feed_id_'.$feed->id.'">'.$feed->title.'</label></li>';
+    echo '<li><input id="feed_id_'.$feed->id.'" name="feed_id[]" type="checkbox" '.$checked.' value="'.$feed->id.'" /> <label for="feed_id_'.$feed->id.'">'.$feed->title.'</label></li>';
 }
--- www/manager/templates/html/User/FeedSelection.tpl.php
+++ www/manager/templates/html/User/FeedSelection.tpl.php
@@ -3,12 +3,12 @@
         Channels<span class="required">*</span>
     </legend>
     <div class="mh-channel-box">
-        <ul>
+        <ul class="validation-container">
             <?php
             $list = UNL_MediaHub_Manager::getUser()->getFeeds(array('limit'=>300));
             echo $savvy->render($list, 'Feed/Media/FeedList.tpl.php');
             ?>
-            <li><label for="new_feed" class="element">New Channel</label><div class="element"><input id="new_feed" name="new_feed" type="text" /></div></li>
+            <li><label for="new_feed" class="element">New Channel</label><div class="element"><input id="new_feed" name="new_feed" type="text" class="validate-one-required"/></div></li>
         </ul>
     </div>
 </fieldset>
