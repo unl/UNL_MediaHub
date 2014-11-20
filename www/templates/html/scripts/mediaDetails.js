45d5b2fe268812fc5dacbdf1c0df43284c034a03
spennythug <spencermccoy@unl.edu>
2014-11-20 10:32:01 -0600

    Removed tinyMCE advanced resize because it was causing issues.

--- www/templates/html/scripts/mediaDetails.js
+++ www/templates/html/scripts/mediaDetails.js
@@ -236,7 +236,6 @@ WDN.jQuery(document).ready(function() {
             theme_advanced_toolbar_location : "top",
             theme_advanced_toolbar_align : "left",
             theme_advanced_statusbar_location : "bottom",
-            theme_advanced_resizing : true,
             theme_advanced_row_height : 33
         });
     });
