8a7aeb2f911cd4e64d4f4e6fc3a0952eb0f1be77
spennythug <spencermccoy@unl.edu>
2014-11-20 15:44:13 -0600

    Moving edit button on single channel view. made it red.

--- www/templates/html/FeedAndMedia.tpl.php
+++ www/templates/html/FeedAndMedia.tpl.php
@@ -6,7 +6,11 @@ $feed_url = htmlentities(UNL_MediaHub_Controller::getURL($context->feed), ENT_QU
 ?>
 <div class="wdn-band wdn-light-neutral-band mh-feed-info">
     <div class="wdn-inner-wrapper">
-        <h1><?php echo htmlentities($context->feed->title) ?></h1>
+        <h1 class="wdn-brand wdn-pull-left"><?php echo htmlentities($context->feed->title) ?></h1>
+        <?php if ($context->feed->userCanEdit($user)): ?>
+            <a href="<?php echo UNL_MediaHub_Manager::getURL()?>?view=feedmetadata&amp;id=<?php echo $context->feed->id ?>" class="wdn-button wdn-button-brand wdn-pull-right">Edit</a>
+        <?php endif ?>
+        <div class="clear"></div>
         <?php echo $savvy->render($context->feed, 'Feed/Creator.tpl.php') ?>
         <div class="wdn-grid-set">
             <div class="bp2-wdn-col-one-fourth wdn-pull-right">
@@ -20,9 +24,7 @@ $feed_url = htmlentities(UNL_MediaHub_Controller::getURL($context->feed), ENT_QU
             <div class="bp2-wdn-col-one-fourth wdn-pull-right mh-feed-stats">
                 <?php echo $savvy->render($context->feed, 'Feed/Stats.tpl.php') ?>
             </div>
-            <?php if ($context->feed->userCanEdit($user)): ?>
-                <a href="<?php echo UNL_MediaHub_Manager::getURL()?>?view=feedmetadata&amp;id=<?php echo $context->feed->id ?>" class="wdn-button wdn-pull-right">Edit</a>
-            <?php endif ?>
+
         </div>
     </div>
 </div>
