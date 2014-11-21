8b5b41c974ebe10bb1d704ccf4d77c24dd5e0b99
Michael Fairchild <mfairchild365@gmail.com>
2014-11-21 10:19:50 -0600

    Suppress errors from the peoplefinder service
    Users who no longer exist were throwing warnings

--- www/manager/templates/html/UserList.tpl.php
+++ www/manager/templates/html/UserList.tpl.php
@@ -3,7 +3,7 @@
 <ul id="userList">
 <?php
 foreach ($context->items as $user) {
-    echo '<li><img class="profile_pic medium" src="http://planetred.unl.edu/pg/icon/unl_'.$user->uid.'/medium/" />'.UNL_Services_Peoplefinder::getFullName($user->uid) .'<span class="uid">('.$user->uid.')</span>';
+    echo '<li><img class="profile_pic medium" src="http://planetred.unl.edu/pg/icon/unl_'.$user->uid.'/medium/" />'.@UNL_Services_Peoplefinder::getFullName($user->uid) .'<span class="uid">('.$user->uid.')</span>';
     echo $savvy->render($user, 'DeleteUserForm.tpl.php');
     echo '</li>';
 }
