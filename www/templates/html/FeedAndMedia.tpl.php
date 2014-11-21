7f1d61ba8cc4ff9d8dfc767a8e0e46d56c45f86b
spennythug <spencermccoy@unl.edu>
2014-11-21 16:08:48 -0600

    Added RSS link to channels. causes layout issues with edit button that will need resolved

--- www/templates/html/FeedAndMedia.tpl.php
+++ www/templates/html/FeedAndMedia.tpl.php
@@ -7,6 +7,7 @@ $feed_url = htmlentities(UNL_MediaHub_Controller::getURL($context->feed), ENT_QU
 <div class="wdn-band wdn-light-neutral-band mh-feed-info">
     <div class="wdn-inner-wrapper">
         <h1 class="wdn-brand wdn-pull-left"><?php echo htmlentities($context->feed->title) ?></h1>
+        <div class="wdn-pull-right mh-rss"><a href="?format=xml"><span class="wdn-icon-rss-squared"></a></span></div>
         <?php if ($context->feed->userCanEdit($user)): ?>
             <a href="<?php echo UNL_MediaHub_Manager::getURL()?>?view=feedmetadata&amp;id=<?php echo $context->feed->id ?>" class="wdn-button wdn-button-brand wdn-pull-right">Edit</a>
         <?php endif ?>
@@ -23,6 +24,7 @@ $feed_url = htmlentities(UNL_MediaHub_Controller::getURL($context->feed), ENT_QU
             </div>
             <div class="bp2-wdn-col-one-fourth wdn-pull-right mh-feed-stats">
                 <?php echo $savvy->render($context->feed, 'Feed/Stats.tpl.php') ?>
+
             </div>
 
         </div>
