5a6ac8889cd892988636c804b0c0833449ba45a1
Michael Fairchild <mfairchild365@gmail.com>
2014-11-24 14:26:37 -0600

    Throw permission and other exceptions before media is rendered

--- src/UNL/MediaHub/Feed/Media/Form.php
+++ src/UNL/MediaHub/Feed/Media/Form.php
@@ -21,7 +21,13 @@ class UNL_MediaHub_Feed_Media_Form
         $this->options = $options + $this->options;
         
         if (isset($this->options['id'])) {
-            $this->media = UNL_MediaHub_Media::getById($_GET['id']);
+            if (!$this->media = UNL_MediaHub_Media::getById($_GET['id'])) {
+                throw new Exception('Media could not be found.', 404);
+            }
+
+            if (!$this->media->userCanEdit(UNL_MediaHub_Manager::getUser())) {
+                throw new Exception('You do not have permission to edit this media.', 403);
+            }
 
             foreach (UNL_MediaHub_Controller::$usedMediaNameSpaces as $class) {
                 $this->media->loadReference($class);
