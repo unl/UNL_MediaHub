0635a745b561fba37fb972cb2981b26c512ebe2f
Michael Fairchild <mfairchild365@gmail.com>
2014-11-24 14:22:59 -0600

    Move javascript to confirm delete out of html

--- www/manager/templates/html/Media/DeleteForm.tpl.php
+++ www/manager/templates/html/Media/DeleteForm.tpl.php
@@ -1,5 +1,5 @@
-<form action="<?php echo UNL_MediaHub_Manager::getURL() ?>" method="post" id="deletemedia_<?php echo $context->id; ?>">
+<form action="<?php echo UNL_MediaHub_Manager::getURL() ?>" method="post" class="confirm-delete">
     <input type="hidden" id="__unlmy_posttarget" name="__unlmy_posttarget" value="delete_media" />
     <input type="hidden" name="media_id" value="<?php echo $context->id; ?>" />
-    <a href="#" class="delete" onclick="if (confirm('Are you sure? This will delete this media, and remove it from any associated channels.')) document.getElementById('deletemedia_<?php echo $context->id; ?>').submit();">Delete</a>
+    <input type="submit" class="delete" value="Delete">
 </form>
