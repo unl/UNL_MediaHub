924ab0294abd2f1cc95feac4be99d953b8e3abb0
spennythug <spencermccoy@unl.edu>
2014-11-25 14:54:45 -0600

    Making tools tips work on mobile

--- www/manager/templates/html/Feed/Media/fields/privacy.tpl.php
+++ www/manager/templates/html/Feed/Media/fields/privacy.tpl.php
@@ -1,21 +1,19 @@
-<label for="privacy" class="element">
-    Privacy
-    <div class="wdn-icon-info mh-tool-tip">
-        <div>
-            <ul>
-                <li>
-                    <span class="heading">Public</span> - Anyone can access the media.
-                </li>
-                <li>
-                    <span class="heading">Unlisted</span> - Media will not be included in public MediaHub listings.
-                </li>
-                <li>
-                    <span class="heading">Private</span> - Only members of channels that the media is included in can access it.
-                </li>
-            </ul>
-        </div>
+<label for="privacy" class="element">Privacy</label>
+<div class="wdn-icon-info mh-tooltip">
+    <div>
+        <ul>
+            <li>
+                <span class="heading">Public</span> - Anyone can access the media.
+            </li>
+            <li>
+                <span class="heading">Unlisted</span> - Media will not be included in public MediaHub listings.
+            </li>
+            <li>
+                <span class="heading">Private</span> - Only members of channels that the media is included in can access it.
+            </li>
+        </ul>
     </div>
-</label>
+</div>
 <select id="privacy" name="privacy">
     <?php
     foreach (UNL_MediaHub_Media::getPossiblePrivacyValues() as $value) {
