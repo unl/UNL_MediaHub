5e29d8be125c7caa6172adb6bb6946bcfb7e5af2
Michael Fairchild <mfairchild365@gmail.com>
2014-11-25 11:20:53 -0600

    Remove duplicate `<li>`
    No li element in scope but a li end tag seen.

--- www/manager/templates/html/Feed/Media/NamespacedElements/mediahub/creation_date.tpl.php
+++ www/manager/templates/html/Feed/Media/NamespacedElements/mediahub/creation_date.tpl.php
@@ -5,10 +5,9 @@
     }]);
 </script>
 
-<li>
-    <label for="media_creation_date" class="element">Media Creation Date<span class="helper">The date that the media was created.</span></label>
-    <div class="element">
-        <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[2][element]" type="hidden" value="media_creation_date"/>
-        <input id="media_creation_date" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[2][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'media_creation_date'); ?>"/>
-    </div>
-</li>
+
+<label for="media_creation_date" class="element">Media Creation Date<span class="helper">The date that the media was created.</span></label>
+<div class="element">
+    <input name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[2][element]" type="hidden" value="media_creation_date"/>
+    <input id="media_creation_date" name="UNL_MediaHub_Feed_Media_NamespacedElements_mediahub[2][value]" type="text" value="<?php echo getFieldValue($context, 'mediahub', 'media_creation_date'); ?>"/>
+</div>
