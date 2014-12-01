d7a9ca17386c0b90f80cdfbbd25e58c9cab9c5b1
Kevin Abel <kevin.abel.0@gmail.com>
2014-12-01 15:43:17 -0600

    Fix manager home link so you are able to return to main home

--- www/manager/templates/html/Manager.tpl.php
+++ www/manager/templates/html/Manager.tpl.php
@@ -5,7 +5,7 @@ if (isset($GLOBALS['UNLTEMPLATEDEPENDENTSPATH'])) {
     UNL_Templates::$options['templatedependentspath'] = $GLOBALS['UNLTEMPLATEDEPENDENTSPATH'];
 }
 $page->doctitle     = '<title>Manager | MediaHub | University of Nebraska-Lincoln</title>';
-$page->titlegraphic = 'UNL MediaHub Manager';
+$page->titlegraphic = 'UNL MediaHub';
 $page->pagetitle    = '';
 $page->addStyleSheet(UNL_MediaHub_Controller::getURL().'templates/html/css/all.css');
 $page->addStyleSheet(UNL_MediaHub_Manager::getURL().'templates/css/all_manager.css');
@@ -15,11 +15,20 @@ $page->leftRandomPromo = '';
 $page->breadcrumbs = '
 <ul>
     <li><a href="http://www.unl.edu/">UNL</a></li>
-    <li>MediaHub</li>
+    <li><a href="' . UNL_MediaHub_Controller::getURL() .'">MediaHub</a></li>
+    <li>Manage Media</li>
 </ul>';
 
 $page->navlinks = $savvy->render(null, 'Navigation.tpl.php');
 $savvy->addGlobal('page', $page);
 $page->maincontentarea =  $savvy->render($context->output);
-$page->footercontent = '&copy; '.date('Y').' University of Nebraska&ndash;Lincoln | Lincoln, NE 68588 | 402-472-7211 | <a href="http://www1.unl.edu/comments/" title="Click here to direct your comments and questions">comments?</a>';
+
+$page->contactinfo = '
+<p>University of Nebraska&ndash;Lincoln<br />
+1400 R Street<br />
+Lincoln, NE 68588<br />
+402-472-7211</p>';
+
+$page->footercontent = $page->footercontent = '© '.date('Y').' University of Nebraska–Lincoln · Lincoln, NE 68588 · 402-472-7211<br />
+    The University of Nebraska–Lincoln is an <a href="http://www.unl.edu/equity/">equal opportunity</a> educator and employer.';
 echo $page;
