b9ca8fa1c7caa3b316952baa23febcd7f2bb302b
spennythug <spencermccoy@unl.edu>
2014-12-02 09:28:00 -0600

    hierarchy and grid work on edit audio

--- www/manager/templates/html/Media/Preview/Audio.tpl.php
+++ www/manager/templates/html/Media/Preview/Audio.tpl.php
@@ -1,6 +1,7 @@
-<div id="audioPreview" class="two_col left">
-    <h1 style="padding: 15px 0 0 20px;">Preview your Audio</h1>
-</div>
+<div class="wdn-col-full">
+<p class="wdn-sans-serif">Preview your Audio</p>
+
 <div id="audioDisplay" class="two_col right">
     <?php echo $savvy->render($context, 'MediaPlayer.tpl.php'); ?>
+</div>	
 </div>
