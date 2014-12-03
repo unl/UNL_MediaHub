8a0dc8ca413b09a375870b34c73f8d98fffdc41e
spennythug <spencermccoy@unl.edu>
2014-12-02 18:36:13 -0600

    Using SVG for channel thumbnails with .png fallback.

--- www/templates/html/Feed/Image.tpl.php
+++ www/templates/html/Feed/Image.tpl.php
@@ -4,7 +4,8 @@ if (!empty($context->image_data) && !empty($context->image_type)) {
     header('Content-Length:'.$context->image_size);
     echo $context->image_data;
 } else {
-    header('Content-type:image/png');
-    echo file_get_contents(dirname(__FILE__).'/../../../manager/templates/css/images/channel-icon.png');
+//     header('Content-type:image/png');
+//     echo file_get_contents(dirname(__FILE__).'/../../../manager/templates/css/images/channel-icon.png');
+	return false;
 }
 
