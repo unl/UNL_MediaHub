64eea606f9c3d353aa08e2640d892dbe0c513db4
Michael Fairchild <mfairchild365@gmail.com>
2014-12-03 16:40:23 -0600

    Remove old privacy query parts

--- src/UNL/MediaHub/MediaList/Filter/ByFeed.php
+++ src/UNL/MediaHub/MediaList/Filter/ByFeed.php
@@ -7,23 +7,16 @@ class UNL_MediaHub_MediaList_Filter_ByFeed implements UNL_MediaHub_Filter
 
     /**
      * @param UNL_MediaHub_Feed $feed
-     * @param string $privacy  - One of PUBLIC, UNLISTED, PRIVATE, ALL
      */
-    function __construct(UNL_MediaHub_Feed $feed, $privacy = 'PUBLIC')
+    function __construct(UNL_MediaHub_Feed $feed)
     {
         $this->feed = $feed;
-        $this->privacy = $privacy;
     }
     
     function apply(Doctrine_Query &$query)
     {
         $sql = 'UNL_MediaHub_Feed_Media.feed_id = ? AND UNL_MediaHub_Feed_Media.media_id = m.id';
         $params = array($this->feed->id);
-        
-        if ($this->privacy != 'ALL') {
-            $sql .= ' AND m.privacy = ?';
-            $params[] = $this->privacy;
-        }
         
         $query->where($sql, $params);
     }
