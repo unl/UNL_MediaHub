6306146f5a144519d114a1178e8bb76b72668af7
spennythug <spencermccoy@unl.edu>
2014-12-03 10:05:27 -0600

    Fixing undefined var error and playing with thumbnail cropping for related channels

--- www/templates/html/CompactFeedList.tpl.php
+++ www/templates/html/CompactFeedList.tpl.php
@@ -6,15 +6,15 @@ if (count($context->items)) :
     <?php foreach ($context->items as $channel): ?>
     	<?php $feed_url = UNL_MediaHub_Controller::getURL($channel); ?>
 		<a href="<?php echo $feed_url ?>" title="<?php echo htmlentities($channel->description, ENT_QUOTES); ?>">
-		    <div class="mh-video-thumb mh-channel-thumb mh-featured-channel wdn-center">
-                <div class="mh-thumbnail-clip">
+		    <div class="mh-channel-thumb mh-featured-channel wdn-center">
+                <div>
                     <?php $channelImage = file_get_contents($feed_url."/image"); ?>
                     <?php if(!$channelImage): ?>
                         <object type="image/svg+xml" data="<?php echo $baseUrl; ?>/templates/html/css/images/channel-icon.svg"><img src="<?php echo $baseUrl; ?>/templates/html/css/images/channel-icon-white.png"></object>
                     <?php else: ?>
                     <img
                         src="<?php echo $feed_url; ?>/image"
-                        alt="<?php echo htmlentities($feed->title, ENT_QUOTES); ?> Image">
+                        alt="<?php echo htmlentities($channel->title, ENT_QUOTES); ?> Image">
                     <?php endif; ?>
                 </div>
             </div>
