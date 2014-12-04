5ca1dad53e9d728682f72a7415df7844a79a6708
Michael Fairchild <mfairchild365@gmail.com>
2014-12-04 08:56:00 -0600

    Optimize the privacy filter

--- src/UNL/MediaHub/MediaList/Filter/Privacy.php
+++ src/UNL/MediaHub/MediaList/Filter/Privacy.php
@@ -3,22 +3,20 @@ class UNL_MediaHub_MediaList_Filter_Privacy implements UNL_MediaHub_Filter
 {
     function apply(Doctrine_Query &$query)
     {
+        $where = 'm.privacy = "PUBLIC"';
+        
         $user = UNL_MediaHub_Controller::getUser();
         
         if ($user) {
-            $feeds = $user->getFeeds();
-            $feeds->run();
-
-            $feeds_array = array();
-            foreach ($feeds->items as $feed) {
-                $feeds_array[] = $feed->id;
-            }
+            $feeds = $user->getFeedIDs();
             
-            $query->andWhere('m.privacy = \'PUBLIC\' OR m.UNL_MediaHub_Feed_Media.feed_id IN (' . implode(',', $feeds_array) .')');
-        } else {
-            $query->andWhere('m.privacy = \'PUBLIC\'');
+            if (!empty($feeds)) {
+                //There is a chance that a user will not have any feeds, so account for that.
+                $where = 'm.privacy = "PUBLIC" OR m.UNL_MediaHub_Feed_Media.feed_id IN (' . implode(',', $feeds) .')';
+            }
         }
         
+        $query->andWhere($where);
     }
 
     function getLabel()
