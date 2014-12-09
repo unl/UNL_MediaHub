0899795b96ec1e2d6243a965079b1e05392d2c27
Michael Fairchild <mfairchild365@gmail.com>
2014-12-09 16:40:26 -0600

    Remove unused mobile detection

--- src/UNL/MediaHub/Controller.php
+++ src/UNL/MediaHub/Controller.php
@@ -10,9 +10,9 @@ class UNL_MediaHub_Controller
      *
      * @var array
      */
-    public $options = array('model'  => false,
-                            'format' => 'html',
-                            'mobile' => false,
+    public $options = array(
+        'model'  => false,
+        'format' => 'html',
     );
 
     /**
@@ -98,11 +98,6 @@ class UNL_MediaHub_Controller
 
         // Initialize default options
         $this->options = $options + $this->options;
-
-        if ($this->options['format'] == 'html'
-            && $this->options['mobile'] != 'no') {
-            $this->options['mobile'] = self::isMobileClient();
-        }
         
         if ($this->options['model'] == 'media_embed') {
             $this->options['format'] = 'js';
@@ -131,36 +126,6 @@ class UNL_MediaHub_Controller
         }
 
         return $namespaces;
-    }
-
-    public static function isMobileClient($options = array())
-    {
-        if (!isset($_SERVER['HTTP_ACCEPT'], $_SERVER['HTTP_USER_AGENT'])) {
-            // We have no vars to check
-            return false;
-        }
-
-        if (isset($_COOKIE['wdn_mobile'])
-            && $_COOKIE['wdn_mobile'] == 'no') {
-            // The user has a cookie set, requesting no mobile views
-            return false;
-        }
-
-        if ( // Check the http_accept and user agent and see
-            preg_match('/text\/vnd\.wap\.wml|application\/vnd\.wap\.xhtml\+xml/i', $_SERVER['HTTP_ACCEPT'])
-                ||
-            (preg_match('/'.
-               'sony|symbian|nokia|samsung|mobile|windows ce|epoc|opera mini|' .
-               'nitro|j2me|midp-|cldc-|netfront|mot|up\.browser|up\.link|audiovox|' .
-               'blackberry|ericsson,|panasonic|philips|sanyo|sharp|sie-|' .
-               'portalmmm|blazer|avantgo|danger|palm|series60|palmsource|pocketpc|' .
-               'smartphone|rover|ipaq|au-mic|alcatel|ericy|vodafone\/|wap1\.|wap2\.|iPhone|Android' .
-               '/i', $_SERVER['HTTP_USER_AGENT'])
-           ) && !preg_match('/ipad/i', $_SERVER['HTTP_USER_AGENT'])) {
-            return true;
-        }
-
-        return false;
     }
 
     /**
