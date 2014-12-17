6ae127f412e8672f83fbb0783c6955359d45d0b1
Michael Fairchild <mfairchild365@gmail.com>
2014-12-17 16:48:48 -0600

    Index the datecreated field

--- data/add_media_views.sql
+++ data/add_media_views.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS `media_views` (
   `datecreated` timestamp NOT NULL,
   `ip_address` varchar(32) NULL,
   PRIMARY KEY (`id`),
-  INDEX `media_views_media_id` (`media_id`)
+  INDEX `media_views_media_id` (`media_id`),
+  INDEX `media_views_datecreated` (`datecreated`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 ALTER TABLE `media` ADD `popular_play_count` INT NOT NULL DEFAULT 0 AFTER `play_count`;
