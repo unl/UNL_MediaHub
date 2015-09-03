4ab33284b06d7e55e62e5da39ff0994d36310575
Michael Fairchild <mfairchild365@gmail.com>
2015-09-03 16:41:50 -0500

    Add the date on insert

--- src/UNL/MediaHub/MediaTextTrackFile.php
+++ src/UNL/MediaHub/MediaTextTrackFile.php
@@ -2,5 +2,15 @@
 
 class UNL_MediaHub_MediaTextTrackFile extends UNL_MediaHub_Models_BaseMediaTextTrackFile
 {
-
+    /**
+     * called before an item is inserted to the database
+     *
+     * @param $event
+     *
+     * @return void
+     */
+    public function preInsert($event)
+    {
+        $this->datecreated = date('Y-m-d H:i:s');
+    }
 }
