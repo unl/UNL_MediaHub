2d9841e0ec9e5206e0a5b351ad936ec882c7f5a0
Michael Fairchild <mfairchild365@gmail.com>
2015-09-24 11:49:01 -0500

    Improve caption notices

--- src/UNL/MediaHub/Notice.php
+++ src/UNL/MediaHub/Notice.php
@@ -3,7 +3,7 @@
  * Class which handles post data and media uploads
  * @author bbieber
  */
-class UNL_MediaHub_Manager_Notice
+class UNL_MediaHub_Notice
 {
     const TYPE_NOTICE = '';
     const TYPE_SUCCESS = 'affirm';
@@ -14,10 +14,17 @@ class UNL_MediaHub_Manager_Notice
     public $title;
     public $body;
     
+    public $links = array();
+    
     public function __construct($title, $body, $type = '')
     {
         $this->title = $title;
         $this->body = $body;
         $this->type = $type;
+    }
+    
+    public function addLink($url, $text)
+    {
+        $this->links[$url] = $text;
     }
 }
