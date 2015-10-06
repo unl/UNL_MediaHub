44f9379f52b2b7b47a22dac0fd755d8fbcd2cb81
Michael Fairchild <mfairchild365@gmail.com>
2015-10-06 09:12:07 -0500

    Adjust confirmation email to reflect the caption requirement

--- scripts/rev_cron.php
+++ scripts/rev_cron.php
@@ -164,7 +164,9 @@ foreach ($orders->items as $order) {
 
                     // Give it a body
                     $html = '<p>Your caption order is complete for <a href="'.$media->getURL().'">'.$media->title.'</a>.</p>';
-                    $html .= '<p>Now that your media is captioned, it is published with your chosen privacy settings.</p>';
+                    if (UNL_MediaHub_Controller::$caption_requirement_date) {
+                        $html .= '<p>Now that your media is captioned, it is published with your chosen privacy settings.</p>';
+                    }
                     $html .= '<p>You will receive a bill for the oder before ' . date('F j, Y', UNL_MediaHub_RevAPI::getEstimatedBillingDate()) . ' .</p>';
                     $html .= '<p>Thank you for using the service, and please let us know if you have any questions.</p>';
                     
