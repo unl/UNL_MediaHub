8c6bdca096c4d1dc950a3312662bc26d4a867e98
Michael Fairchild <mfairchild365@gmail.com>
2015-10-08 14:47:24 -0500

    Bring the DB up to snuff
    Engine and charset updates

--- data/add_media_text_tracks.sql
+++ data/add_media_text_tracks.sql
@@ -1,6 +1,25 @@
-ALTER TABLE `media` ENGINE = INNODB;
-ALTER TABLE `users` ENGINE = INNODB;
+/** Bring old tables up to snuff **/
+ALTER TABLE users CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE feed_has_media CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE feed_has_nselement CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE feeds CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE media CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE media_has_nselement CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE subscriptions CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE user_has_permission CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE permissions CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
+ALTER TABLE users ENGINE = INNODB;
+ALTER TABLE feed_has_media ENGINE = INNODB;
+ALTER TABLE feed_has_nselement ENGINE = INNODB;
+ALTER TABLE feeds ENGINE = INNODB;
+ALTER TABLE media ENGINE = INNODB;
+ALTER TABLE media_has_nselement ENGINE = INNODB;
+ALTER TABLE subscriptions ENGINE = INNODB;
+ALTER TABLE user_has_permission ENGINE = INNODB;
+ALTER TABLE permissions ENGINE = INNODB;
+
+/** Add new tables/structure **/
 CREATE TABLE IF NOT EXISTS `media_text_tracks` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `media_id` int(10) unsigned NOT NULL,
@@ -10,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `media_text_tracks` (
   PRIMARY KEY (`id`),
   FOREIGN KEY (`media_id`) REFERENCES media(id),
   INDEX `media_text_tracks_datecreated` (`datecreated`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `media_text_tracks_files` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -23,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `media_text_tracks_files` (
   PRIMARY KEY (`id`),
   FOREIGN KEY (`media_text_tracks_id`) REFERENCES media_text_tracks(id),
   INDEX `media_text_tracks_datecreated` (`datecreated`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 ALTER TABLE `media` ADD `media_text_tracks_id` int(10) unsigned NULL;
 ALTER TABLE `media`
@@ -49,6 +68,6 @@ CREATE TABLE IF NOT EXISTS `rev_orders` (
   FOREIGN KEY (`media_id`) REFERENCES media(id),
   INDEX `rev_orders_datecreated` (`datecreated`),
   INDEX `rev_orders_cost_object` (`costobjectnumber`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 
