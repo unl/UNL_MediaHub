e5656f0dd3aed9ab5a6a08f597583488916ebec5
Michael Fairchild <mfairchild365@gmail.com>
2015-10-09 11:54:21 -0500

    Use datetimes instead of timestamps

---
+++ data/media_text_tracks_dates.sql
@@ -0,0 +1,4 @@
+ALTER TABLE media_text_tracks MODIFY datecreated DATETIME;
+ALTER TABLE media_text_tracks_files MODIFY datecreated DATETIME;
+ALTER TABLE rev_orders MODIFY datecreated DATETIME;
+ALTER TABLE rev_orders MODIFY dateupdated DATETIME;
--- src/UNL/MediaHub/Installer.php
+++ src/UNL/MediaHub/Installer.php
@@ -34,6 +34,7 @@ class UNL_MediaHub_Installer
         $messages[] = $this->exec_sql(file_get_contents(dirname(__FILE__).'/../../../data/add_permissions.sql'), 'Adding permissions table', true);
         $messages[] = $this->exec_sql(file_get_contents(dirname(__FILE__).'/../../../data/add_media_views.sql'), 'Adding the media views table', true);
         $messages[] = $this->exec_sql(file_get_contents(dirname(__FILE__).'/../../../data/add_media_text_tracks.sql'), 'Adding media text tracks table', true);
+        $messages[] = $this->exec_sql(file_get_contents(dirname(__FILE__).'/../../../data/media_text_tracks_dates.sql'), 'Fixing date fields', true);
         
         return $messages;
     }
