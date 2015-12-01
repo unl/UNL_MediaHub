6b2d801d483be2adde1255452624dbc099d9c4f3
Michael Fairchild <mfairchild365@gmail.com>
2015-12-01 15:20:59 -0600

    Use less to manage the player CSS file

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Task configuration.
+        less: {
+            development: {
+                files: {
+                    './www/templates/html/css/player.css': './www/templates/html/less/player.less'
+                }
+            }
+        },
+        watch: {
+            files: ['./www/templates/html/less/*'],
+            tasks: ['less'],
+            options: {
+                event: ['changed', 'added', 'deleted']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['watch']);
+};
