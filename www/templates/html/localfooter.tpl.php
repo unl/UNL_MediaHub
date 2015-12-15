283341f7f639b2449494a115786341f0bd70c98c
Michael Fairchild <mfairchild365@gmail.com>
2015-12-15 14:47:23 -0600

    Improve the local footer

---
+++ www/templates/html/localfooter.tpl.php
@@ -0,0 +1,29 @@
+
+<div class="wdn-grid-set wdn-footer-links-local">
+    <div class="bp960-wdn-col-two-thirds">
+        <div class="wdn-footer-module">
+            <span role="heading" class="wdn-footer-heading">About UNL MediaHub<span class="wdn-text-hidden"> Contact Information</span></span>
+            <p>
+                This application is developed and maintained by <a href="http://iim.unl.edu/">Internet and Interactive Media</a> (IIM), which is a partnership with University Communications and Information Technology Services.
+            </p>
+        </div>
+    </div>
+    <div class="bp960-wdn-col-one-third">
+        <div class="wdn-footer-module">
+            <span role="heading" class="wdn-footer-heading">Related Links</span>
+            <ul class="wdn-related-links-v1">
+                <li><a href="<?php echo UNL_MediaHub_Controller::getURL() ?>developers">Developer Documentation</a></li>
+                <li><a href="http://itunes.unl.edu/">UNL On iTunes U</a></li>
+                <li><a href="http://iim.unl.edu/">Internet and Interactive Media</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-22295578-1']);
+    _gaq.push(['_setDomainName', '.unl.edu']);
+    _gaq.push(['_setAllowLinker', true]);
+    _gaq.push(['_trackPageview']);
+</script>
