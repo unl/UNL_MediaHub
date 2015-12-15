eb3c30d0d432934492d14ee47e03c8f250b3e85d
Michael Fairchild <mfairchild365@gmail.com>
2015-12-15 14:25:38 -0600

    Update to 4.1

--- www/templates/html/Controller.tpl.php
+++ www/templates/html/Controller.tpl.php
@@ -1,21 +1,15 @@
 <?php
-require_once 'UNL/Templates.php';
+use UNL\Templates\Templates;
 
-UNL_Templates::$options['version'] = 4.0;
+$page = Templates::factory('Fixed', Templates::VERSION_4_1);
 
-$template = 'Fixed';
 $baseUrl = UNL_MediaHub_Controller::getURL();
 
-$page = UNL_Templates::factory($template);
-
-if (isset($GLOBALS['UNLTEMPLATEDEPENDENTSPATH'])) {
-    UNL_Templates::$options['templatedependentspath'] = $GLOBALS['UNLTEMPLATEDEPENDENTSPATH'];
-}
 $page->doctitle     = '<title>MediaHub | University of Nebraska-Lincoln</title>';
 $page->titlegraphic = 'UNL MediaHub';
 $page->pagetitle    = '';
 $page->addStyleSheet($baseUrl . 'templates/html/css/all.css?v=' . UNL_MediaHub_Controller::VERSION);
-$page->breadcrumbs = '<ul> <li><a href="http://www.unl.edu/">UNL</a></li> <li>MediaHub</li></ul>';
+$page->breadcrumbs = '<ul> <li><a href="http://www.unl.edu/">UNL</a></li> <li><a href="' . $baseUrl . '">MediaHub</a></li></ul>';
 $page->head .= '<script>WDN.setPluginParam("idm", "logout", "' . $baseUrl . '?logout");</script>';
 $page->addScript(UNL_MediaHub_Controller::getURL().'templates/html/scripts/mediatools.js?v=' . UNL_MediaHub_Controller::VERSION);
 if (!$context->output instanceof UNL_MediaHub_FeedAndMedia) {
@@ -48,8 +42,7 @@ $page->contactinfo = '
 Lincoln, NE 68588<br />
 402-472-7211</p>';
 
-$page->footercontent = '© '.date('Y').' University of Nebraska–Lincoln · Lincoln, NE 68588 · 402-472-7211<br />
-    The University of Nebraska–Lincoln is an <a href="http://www.unl.edu/equity/">equal opportunity</a> educator and employer.
+$page->contactinfo .= '
 <script type="text/javascript">
     var _gaq = _gaq || [];
     _gaq.push([\'_setAccount\', \'UA-22295578-1\']);
