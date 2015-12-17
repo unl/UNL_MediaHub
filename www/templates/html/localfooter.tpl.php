90360f896d27cc2bf6d5007d34c67788e81ede12
Michael Fairchild <mfairchild365@gmail.com>
2015-12-17 14:37:43 -0600

    Use shared iim app footer

--- www/templates/html/localfooter.tpl.php
+++ www/templates/html/localfooter.tpl.php
@@ -3,9 +3,13 @@
     <div class="bp960-wdn-col-two-thirds">
         <div class="wdn-footer-module">
             <span role="heading" class="wdn-footer-heading">About UNL MediaHub</span>
-            <p>
-                This application is developed and maintained by <a href="http://iim.unl.edu/">Internet and Interactive Media</a> (IIM), which is a partnership with University Communications and Information Technology Services.
-            </p>
+            <?php
+            if ($file = @file_get_contents(UNL_MediaHub::getRootDir() . '/tmp/iim-app-footer.html')) {
+                echo $file;
+            } else {
+                echo file_get_contents('http://iim.unl.edu/iim-app-footer?format=partial');
+            }
+            ?>
         </div>
     </div>
     <div class="bp960-wdn-col-one-third">
