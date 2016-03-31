535b014c01f27c3447a79d4478b1b7d9d317b4d9
Michael Fairchild <mfairchild365@gmail.com>
2016-03-31 14:01:45 -0500

    Remove unnecessary logic

--- www/templates/mosaic-xml/Controller.tpl.php
+++ www/templates/mosaic-xml/Controller.tpl.php
@@ -1,16 +1,6 @@
-<?php
-if ($context->output instanceof UNL_MediaHub_MediaList
-    || (is_array($context->output) && (
-        $context->output[0] instanceof UNL_MediaHub_MediaList
-        || $context->output[0] instanceof UNL_MediaHub_Media
-        || $context->output[0] instanceof UNL_MediaHub_DefaultHomepage
-        || $context->output[0] instanceof UNL_MediaHub_FeedAndMedia))):
-    echo '<?xml version="1.0" encoding="UTF-8" standalone="no"?>'; ?>
-    <videos>
-        <?php
-        echo $savvy->render($context->output);
-        ?>
-    </videos>
-<?php else: ?>
-    <?php echo $savvy->render($context->output); ?>
-<?php endif; ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8" standalone="no"?>'; ?>
+<videos>
+    <?php
+    echo $savvy->render($context->output);
+    ?>
+</videos>
