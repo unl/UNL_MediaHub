177a551209cc4b2ebe6cca05ccb7d504e86e32a8
Michael Fairchild <mfairchild365@gmail.com>
2016-04-01 13:26:02 -0500

    No need to echo

--- www/templates/mosaic-xml/Controller.tpl.php
+++ www/templates/mosaic-xml/Controller.tpl.php
@@ -1,4 +1,4 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8" standalone="no"?>'; ?>
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <videos>
     <?php
     echo $savvy->render($context->output);
