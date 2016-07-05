829f18ccd5c020d9935fbe3dcbe154ba73bc55a7
Michael Fairchild <mfairchild365@gmail.com>
2016-07-05 11:40:47 -0500

    Reduce scope and reuse objects where possible

--- www/templates/html/scripts/mediaDetails.js
+++ www/templates/html/scripts/mediaDetails.js
@@ -1,7 +1,16 @@
-var mediaDetails;
 
 require(['jquery'], function($){
-    mediaDetails = function() {
+    var mediaDetails = function() {
+        var $posterPicker = $('#poster_picker');
+        var $posterPickerDisabled = $('#poster_picker_disabled');
+        var $mediaPoster = $('#media_poster');
+        var $mediaForm = $('#media_form');
+        var $itunesHeader = $('#itunes_header');
+        var $mrssHeader = $('#mrss_header');
+        var $imageOverlay = $('#imageOverlay');
+        var $thumbnail = $('#thumbnail');
+        var $feedList = $('#feedlist');
+        
         return {
             getImageURL: function() {
                 return front_end_baseurl + 'media/' + $(mejs.players.mep_0.media).attr('data-mediahub-id') + '/image';
@@ -16,13 +25,13 @@ require(['jquery'], function($){
             },
 
             updateThumbnail : function(currentTime) {
-                $('#imageOverlay').show();
+                $imageOverlay.show();
 
                 var src = mediaDetails.getImageURL() + '?time='+mediaDetails.formatTime(currentTime)+'&rebuild';
 
                 $.ajax(src).always(function() {
-                    $('#thumbnail').attr('src', src.replace('&rebuild', ''));
-                    $('#imageOverlay').hide();
+                    $thumbnail.attr('src', src.replace('&rebuild', ''));
+                    $imageOverlay.hide();
                 });
             },
 
@@ -40,131 +49,117 @@ require(['jquery'], function($){
             scalePlayer: function() {
                 // Now scale down the player
                 var thumbnail = new Image();
-                thumbnail.src = $('#thumbnail').attr('src')+'?time='+mediaDetails.formatTime(0);
+                thumbnail.src = $thumbnail.attr('src')+'?time='+mediaDetails.formatTime(0);
                 thumbnail.onload = function(){
                     var calcHeight = (this.height * 460)/this.width;
                     $('#videoDisplay object').attr('style', 'width:460px;height:'+calcHeight);
-                    $('#thumbnail').attr('src', thumbnail.src);
+                    $thumbnail.attr('src', thumbnail.src);
                 };
                 thumbnail.onerror = '';
             },
 
             hidePosterPicker: function() {
-                $('#poster_picker').hide();
-                $('#poster_picker_disabled').show();
+                $posterPicker.hide();
+                $posterPickerDisabled.show();
             },
 
             showPosterPicker: function() {
-                $('#poster_picker').show();
-                $('#poster_picker_disabled').hide();
+                $posterPicker.show();
+                $posterPickerDisabled.hide();
+            },
+            
+            initialize: function() {
+                $feedList.hide();
+                $('#formDetails, #formDetails form, #formDetails fieldset, #continue3').not('#addMedia').css({'display' : 'block'});
+                $('.share-video-link').css('display', 'none');
+
+                if (mediaType == 'video') {
+                    mediaDetails.scalePlayer();
+                }
+
+                if ($mediaPoster.val() !== '') {
+                    $posterPicker.hide();
+                } else {
+                    $posterPickerDisabled.hide();
+                }
+
+                $mediaPoster.on('keyup', function() {
+                    if (this.value == '') {
+                        mediaDetails.showPosterPicker();
+                    } else {
+                        $thumbnail.attr('src', this.value);
+                        mediaDetails.hidePosterPicker();
+                    }
+                });
+
+                $('#enable_poster_picker').click(function() {
+                    $mediaPoster.val('');
+                    mediaDetails.showPosterPicker();
+                });
+
+                $('#setImage').on('click', function(){
+                    var currentTime;
+
+                    currentTime = mejs.players.mep_0.getCurrentTime();
+
+                    mediaDetails.updateThumbnail(currentTime);
+
+                    return false;
+                });
+
+                //deal with the outpost extra information
+                $itunesHeader.find("ol").hide();
+                $mrssHeader.find("ol").hide();
+
+                $itunesHeader.find("legend").click(function() {
+                    $itunesHeader.find("ol").toggle(400);
+                    return false;
+                });
+                $mrssHeader.find("legend").click(function() {
+                    $mrssHeader.find("ol").toggle(400);
+                    return false;
+                });
+
+                WDN.initializePlugin('form_validation', [function() {
+                    $.validation.addMethod('geo_long', 'This must be a valid longitude.', {min:-180, max:180});
+                    $.validation.addMethod('geo_lat', 'This must be a valid latitude.', {min:-90, max:90});
+                    $mediaForm.validation({
+                        containerClassName: 'validation-container',
+                        immediate: true
+                    });
+
+                    $mediaForm.bind('validate-form', function(event, result) {
+                        if (result) {
+                            //prevent duplicate submissions
+                            $('#continue3').attr('disabled', 'disabled');
+                        }
+                    });
+                }]);
+
+                //Collapisible forms.
+                $('.collapsible > legend').prepend("<span class='toggle'>+</span>");
+                $('.collapsible > ol').hide();
+                $('.collapsible > legend').click(function(){
+                    if ($(this).next('ol').is(":visible")) {
+                        $(this).next('ol').hide(200);
+                        $(this).find('.toggle').html('+');
+                    } else {
+                        $(this).next('ol').show(200);
+                        $(this).find('.toggle').html('-');
+                    }
+                });
+
+                //Update duration
+                $('.find-duration').click(function() {
+                    mediaDetails.updateDuration();
+                    //Prevent default action
+                    return false;
+                });
             }
         };
     }();
 
     $(document).ready(function() {
-
-        $('#feedlist').hide();
-        $('#formDetails, #formDetails form, #formDetails fieldset, #continue3').not('#addMedia').css({'display' : 'block'});
-        $('.share-video-link').css('display', 'none');
-        
-        if (mediaType == 'video') {
-            mediaDetails.scalePlayer();
-        }
-
-        if ($('#media_poster').val() !== '') {
-            $('#poster_picker').hide();
-        } else {
-            $('#poster_picker_disabled').hide();
-        }
-
-        $('#media_poster').on('keyup', function() {
-            if (this.value == '') {
-                mediaDetails.showPosterPicker();
-            } else {
-                $('#thumbnail').attr('src', this.value);
-                mediaDetails.hidePosterPicker();
-            }
-        });
-
-        $('#enable_poster_picker').click(function() {
-            $('#media_poster').val('');
-            mediaDetails.showPosterPicker();
-        });
-
-        $("#mediaSubmit").click(function (event) { //called when a user adds video
-            // Hide the url field, user is uploading a file
-            $('#media_url').closest('li').hide();
-
-            $('#fileUpload').hide();
-
-            $("#addMedia, #feedlist").slideUp(400, function () {
-                $("#headline_main").slideDown(400, function () {
-                    $("#media_form").show().css({"width": "930px"}).parent("#formDetails").removeClass("two_col right");
-                    $("#existing_media, #enhanced_header, #feedSelect, #maincontent form.zenform #continue3").slideDown(400);
-
-                    $(this).css('display', 'inline-block');
-                });
-            });
-
-        });
-
-        $('a#setImage').on('click', function(){
-            var currentTime;
-
-            currentTime = mejs.players.mep_0.getCurrentTime();
-
-            mediaDetails.updateThumbnail(currentTime);
-
-            return false;
-        });
-
-        //deal with the outpost extra information
-        $("#itunes_header ol").hide();
-        $("#mrss_header ol").hide();
-
-        $("#itunes_header legend").click(function() {
-            $("#itunes_header ol").toggle(400);
-            return false;
-        });
-        $("#mrss_header legend").click(function() {
-            $("#mrss_header ol").toggle(400);
-            return false;
-        });
-
-        WDN.initializePlugin('form_validation', [function() {
-            $.validation.addMethod('geo_long', 'This must be a valid longitude.', {min:-180, max:180});
-            $.validation.addMethod('geo_lat', 'This must be a valid latitude.', {min:-90, max:90});
-            $('#media_form').validation({
-                containerClassName: 'validation-container',
-                immediate: true
-            });
-
-            $('#media_form').bind('validate-form', function(event, result) {
-                if (result) {
-                    //prevent duplicate submissions
-                    $('#continue3').attr('disabled', 'disabled');
-                }
-            });
-        }]);
-
-        //Collapisible forms.
-        $('.collapsible > legend').prepend("<span class='toggle'>+</span>");
-        $('.collapsible > ol').hide();
-        $('.collapsible > legend').click(function(){
-            if ($(this).next('ol').is(":visible")) {
-                $(this).next('ol').hide(200);
-                $(this).find('.toggle').html('+');
-            } else {
-                $(this).next('ol').show(200);
-                $(this).find('.toggle').html('-');
-            }
-        });
-        
-        //Update duration
-        $('.find-duration').click(function() {
-            mediaDetails.updateDuration();
-            //Prevent default action
-            return false;
-        });
+        mediaDetails.initialize();
     });
 });
