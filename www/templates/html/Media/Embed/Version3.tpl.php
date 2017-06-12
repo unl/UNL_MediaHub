2c6f2d2c8edc2abe54a6961a80853800c86a5a13
Michael Fairchild <mfairchild365@gmail.com>
2017-06-12 14:58:55 -0500

    Better describe the iframe
    The title by itself doesn't explain the purpose of the iframe (which is to play a video/audio)

--- www/templates/html/Media/Embed/Version3.tpl.php
+++ www/templates/html/Media/Embed/Version3.tpl.php
@@ -1,3 +1,9 @@
 <div class="wdn-responsive-embed wdn-aspect16x9">
-    <iframe src="<?php echo $controller->getURL($context->media)?>?format=iframe&autoplay=0" title="<?php echo htmlentities($context->media->title) ?>" allowfullscreen></iframe>
+    <?php
+    $prefix = 'Video Player: ';
+    if (!$context->media->isVideo()) {
+        $prefix = 'Audio Player: ';
+    }
+    ?>
+    <iframe src="<?php echo $controller->getURL($context->media)?>?format=iframe&autoplay=0" title="<?php echo $prefix ?> <?php echo htmlentities($context->media->title) ?>" allowfullscreen></iframe>
 </div>
