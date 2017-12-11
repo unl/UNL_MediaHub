c21f2957cce37e4559dd3583aeb0184691ef01e5
Michael Fairchild <mfairchild365@gmail.com>
2017-12-11 11:18:58 -0600

    Cache bust new video files

--- www/templates/iframe/Controller.tpl.php
+++ www/templates/iframe/Controller.tpl.php
@@ -11,20 +11,18 @@ if (isset($context->output[0]) && $context->output[0] instanceof UNL_MediaHub_Me
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Test</title>
     <script src="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/jquery.js?v=<?php echo UNL_MediaHub_Controller::getVersion() ?>"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r76/three.js"></script>
-    <link href="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/video-js.css" rel="stylesheet">
-    <script src="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/video.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r76/three.js?v=<?php echo UNL_MediaHub_Controller::getVersion() ?>"></script>
+    <link href="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/video-js.css?v=<?php echo UNL_MediaHub_Controller::getVersion() ?>" rel="stylesheet">
+    <script src="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/video.js?v=<?php echo UNL_MediaHub_Controller::getVersion() ?>"></script>
     <?php if ($is_audio): ?>
-        <script src="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/wavesurfer.min.js"></script>
-        <script src="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/videojs.wavesurfer.min.js"></script>
+        <script src="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/wavesurfer.min.js?v=<?php echo UNL_MediaHub_Controller::getVersion() ?>"></script>
+        <script src="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/videojs.wavesurfer.min.js?v=<?php echo UNL_MediaHub_Controller::getVersion() ?>"></script>
+        <link href="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/videojs.wavesurfer.min.css?v=<?php echo UNL_MediaHub_Controller::getVersion() ?>" rel="stylesheet">
+        <link href="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/css/font-awesome.min.css?v=<?php echo UNL_MediaHub_Controller::getVersion() ?>" rel="stylesheet">
     <?php endif; ?>
-    <?php if ($is_audio): ?>
-        <link href="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/videojs.wavesurfer.min.css" rel="stylesheet">
-        <link href="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/css/font-awesome.min.css" rel="stylesheet">
-    <?php endif; ?>
-    <script src="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/videojs-panorama.v5.js"></script>
-    <link href="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/videojs-panorama.min.css" rel="stylesheet">
-    <script src="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/videojs-mediahub.js"></script>
+    <script src="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/videojs-panorama.v5.js?v=<?php echo UNL_MediaHub_Controller::getVersion() ?>"></script>
+    <link href="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/videojs-panorama.min.css?v=<?php echo UNL_MediaHub_Controller::getVersion() ?>" rel="stylesheet">
+    <script src="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/videojs-mediahub.js?v=<?php echo UNL_MediaHub_Controller::getVersion() ?>"></script>
     <!-- <script src="<?php echo UNL_MediaHub_Controller::getURL(); ?>templates/iframe/js/vendor/videojs-captions-toggle.js"></script> -->
     <link rel="stylesheet" href="<?php echo UNL_MediaHub_Controller::toAgnosticURL(UNL_MediaHub_Controller::$url); ?>templates/iframe/css/iframe.css?v=<?php echo UNL_MediaHub_Controller::getVersion() ?>" />
     
