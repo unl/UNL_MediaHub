53300103f31a7b8628db95d22ee0e2c95d5ded1c
Michael Fairchild <mfairchild365@gmail.com>
2017-12-14 11:04:06 -0600

    Fix a bug preventing keyboard-only use of captions

--- www/templates/iframe/js/vendor/video.js
+++ www/templates/iframe/js/vendor/video.js
@@ -16116,7 +16116,7 @@ var MenuButton = function (_Component) {
 
       // set the focus into the submenu, except on iOS where it is resulting in
       // undesired scrolling behavior when the player is in an iframe
-      if (!IS_IOS && !isInFrame()) {
+      if (true) { //see https://github.com/videojs/video.js/issues/4821
         this.menu.focus();
       }
     }
