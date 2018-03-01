55449ee01bc5f7956cc253c36163ba18decfe5f5
Michael Fairchild <mfairchild365@gmail.com>
2018-03-01 14:02:49 -0600

    Generate a new thumbnail upon transcoding

--- scripts/transcode.php
+++ scripts/transcode.php
@@ -153,6 +153,18 @@ function downloadOutput($output_bucket, $key, UNL_MediaHub_Media $media)
     //Update the URL of the media to the media720.mp4 file
     $media->url = UNL_MediaHub_Controller::getURL() . 'uploads/'.$media->UUID.'/media720.mp4';
     $media->save();
+
+    $poster_file = UNL_MediaHub::getRootDir() . '/www/uploads/thumbnails/'.$media->id.'/original.jpg';
+    if (file_exists($poster_file)) {
+        //Delete the poster file so that a new one is generated
+        unlink($poster_file);
+    }
+
+    $poster_file = UNL_MediaHub::getRootDir() . '/www/uploads/thumbnails/'.$media->id.'/use-default';
+    if (file_exists($poster_file)) {
+        //the default was selected, delete it so that a new one is generated
+        unlink($poster_file);
+    }
 }
 
 function deleteInputFile($input_bucket, $key)
