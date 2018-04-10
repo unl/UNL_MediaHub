4fac828d93c7a276ce11e1d800086fcaa6c3d222
Michael Fairchild <mfairchild365@gmail.com>
2018-04-10 16:13:47 -0500

    Fix heading levels in the local footer

--- www/templates/html/localfooter.tpl.php
+++ www/templates/html/localfooter.tpl.php
@@ -2,13 +2,13 @@
 <div class="wdn-grid-set wdn-footer-links-local">
     <div class="bp960-wdn-col-two-thirds">
         <div class="wdn-footer-module">
-            <span role="heading" class="wdn-footer-heading">About UNL MediaHub</span>
+            <span role="heading" aria-level="2" class="wdn-footer-heading">About UNL MediaHub</span>
             <p>This application is a product of the <a href="https://iim.unl.edu/">Internet and Interactive Media group at Nebraska</a>. IIM is a partnership of <a href="https://ucomm.unl.edu/">University Communication</a> and <a href="https://its.unl.edu/">Information Technology Services</a>.</p>
         </div>
     </div>
     <div class="bp960-wdn-col-one-third">
         <div class="wdn-footer-module">
-            <span role="heading" class="wdn-footer-heading">Related Links</span>
+            <span role="heading" aria-level="2" class="wdn-footer-heading">Related Links</span>
             <ul class="wdn-related-links-v1">
                 <li><a href="<?php echo UNL_MediaHub_Controller::getURL() ?>developers">Developer Documentation</a></li>
                 <li><a href="<?php echo UNL_MediaHub_Controller::getURL() ?>help/media-prep">Preparing Your Media</a></li>
