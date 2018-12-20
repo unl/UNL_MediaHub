b804fddcdd6bfd6ca481a1254415ca515a16a540
Jeffrey Sturek <jsturek8@unl.edu>
2018-12-20 14:40:47 -0600

    Convert #maincontent to #dcf-main

--- www/manager/templates/html/Manager/PostHandler/UploadException.tpl.php
+++ www/manager/templates/html/Manager/PostHandler/UploadException.tpl.php
@@ -2,7 +2,7 @@
 $html = $savvy->render($context, 'Exception.tpl.php');
 ?>
 <script type="text/javascript">
-parent.WDN.jQuery('#maincontent').prepend(<?php echo json_encode($html); ?>);
+parent.WDN.jQuery('#dcf-main').prepend(<?php echo json_encode($html); ?>);
 parent.WDN.jQuery('.meter').hide();
 parent.WDN.jQuery('#fileUpload').show();
 </script>
