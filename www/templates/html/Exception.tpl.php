2e5d9484d472f2c29b72e3de0ef99b97581bc3d0
Jeff Sturek <jsturek8@unl.edu>
2020-04-16 12:53:04 -0500

    update notice loader

--- www/templates/html/Exception.tpl.php
+++ www/templates/html/Exception.tpl.php
@@ -31,8 +31,8 @@ switch ($code) {
         });
     });
 </script>
-<div class="dcf-mt-8 dcf-mb-8 wdn_notice alert">
-    <div class="message" style="color: #fff; padding: 6px;">
+<div class="dcf-mt-8 dcf-mb-8 wdn_notice alert" style="padding: 6px;">
+    <div class="message" style="color: #fff;">
         <h1 class="title"><?php echo $title; ?></h1>
         <p><?php echo UNL_MediaHub::escape($context->getMessage()); ?></p>
     </div>
