7ba41acc448faf5bbc945135f012b34c8a4e55c7
Jeff Sturek <jsturek8@unl.edu>
2020-07-20 13:20:45 -0500

    Add video and audio title and hide on mouseover

--- www/templates/html/MediaPlayer/v3/Audio.tpl.php
+++ www/templates/html/MediaPlayer/v3/Audio.tpl.php
@@ -1,4 +1,4 @@
-<audio class="mh_player video-js vjs-default-skin" preload="auto" data-url="<?php echo $controller->getURL($context); ?>" data-mediahub-id="<?php echo (int)$context->id ?>" poster="<?php echo UNL_MediaHub_Controller::toAgnosticURL($context->getThumbnailURL()); ?>" src="<?php echo $context->getMediaURL()?>">
+<audio class="mh_player video-js vjs-default-skin" preload="auto" data-url="<?php echo $controller->getURL($context); ?>" data-mediahub-id="<?php echo (int)$context->id ?>" poster="<?php echo UNL_MediaHub_Controller::toAgnosticURL($context->getThumbnailURL()); ?>" src="<?php echo $context->getMediaURL()?>" title="<?php echo UNL_MediaHub::escape($context->title); ?>" onmouseover="this.title='';">
     <?php foreach ($context->getTextTrackURLs() as $lang=>$track):?>
         <track id="mediahub-track-<?php echo UNL_MediaHub::escape($lang) ?>" src="<?php echo UNL_MediaHub::escape(UNL_MediaHub_Controller::toAgnosticURL($track)) ?>" kind="captions" srclang="<?php echo UNL_MediaHub::escape($lang) ?>" />
     <?php endforeach ?>
