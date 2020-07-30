b47b5f98bb8778a4ce584e7363bb10bc2fc006b0
Jeff Sturek <jsturek8@unl.edu>
2020-07-30 14:11:06 -0500

    start

--- www/templates/html/Media/Embed/Version3.tpl.php
+++ www/templates/html/Media/Embed/Version3.tpl.php
@@ -1,9 +1,11 @@
-<div style="padding-top: 56.25%; overflow: hidden; position:relative;">
-    <?php
-    $prefix = 'Video Player: ';
-    if (!$context->media->isVideo()) {
-        $prefix = 'Audio Player: ';
-    }
-    ?>
+<?php
+$prefix = 'Video Player: ';
+$ratioPercentage = '56.25%';
+if (!$context->media->isVideo()) {
+    $prefix = 'Audio Player: ';
+    $ratioPercentage = '25%';
+}
+?>
+<div style="padding-top: <?php echo $ratioPercentage ?>; overflow: hidden; position:relative;">
     <iframe style="bottom: 0; left: 0; position: absolute; right: 0; top: 0; border: 0; height: 100%; width: 100%;" src="<?php echo $controller->getURL($context->media)?>?format=iframe&autoplay=0" title="<?php echo $prefix ?> <?php echo UNL_MediaHub::escape($context->media->title) ?>" allowfullscreen></iframe>
 </div>
