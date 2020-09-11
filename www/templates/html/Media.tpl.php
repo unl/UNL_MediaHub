6399ade63342bd89973e75f43b0392c12a9100ae
Jeff Sturek <jsturek8@unl.edu>
2020-09-11 12:32:37 -0500

    Player style changes

--- www/templates/html/Media.tpl.php
+++ www/templates/html/Media.tpl.php
@@ -46,20 +46,18 @@ if ($type == 'video') {
 $controller->setReplacementData('head', $meta);
 $getTracks = $context->getTextTrackURLs();
 
-$div1Class = 'dcf-pt-4 dcf-pb-4 dcf-jc-center';
-$div2Class = 'dcf-ratio dcf-ratio-16x9';
+$divClass = 'dcf-ratio dcf-ratio-16x9 dcf-flex-grow-1';
 $iframeClass = 'dcf-ratio-child';
 if ($type === 'audio') {
-    $div1Class = 'dcf-pt-4 dcf-pb-4 dcf-d-flex dcf-jc-center';
-    $div2Class = 'dcf-h-10 dcf-w-max-xl dcf-flex-grow-1 dcf-overflow-hidden dcf-relative';
+    $divClass = 'dcf-h-10 dcf-w-max-xl dcf-flex-grow-1 dcf-overflow-hidden dcf-relative';
     $iframeClass = 'dcf-obj-fit-contain dcf-obj-top dcf-b-0';
 }
 
 ?>
 
 <div class="dcf-bleed mh-video-band">
-    <div class="dcf-wrapper <?php echo $div1Class; ?>">
-        <div class="<?php echo $div2Class; ?>">
+    <div class="dcf-wrapper dcf-pt-4 dcf-pb-4 dcf-d-flex dcf-jc-center">
+        <div class="<?php echo $divClass; ?>">
             <iframe class="<?php echo $iframeClass; ?> dcf-obj-fit-contain dcf-obj-top dcf-b-0" height="667" src="<?php echo $controller->getURL($context)?>?format=iframe&autoplay=0&preload=auto" allowfullscreen title="play media"></iframe>
         </div>
     </div>
