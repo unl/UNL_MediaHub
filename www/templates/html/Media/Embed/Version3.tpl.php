0296ca007ac6e04c55cc1d6087879d58dda003a0
Jeff Sturek <jsturek8@unl.edu>
2020-09-11 16:04:07 -0500

    update embed style

--- www/templates/html/Media/Embed/Version3.tpl.php
+++ www/templates/html/Media/Embed/Version3.tpl.php
@@ -1,11 +1,11 @@
 <?php
 $prefix = 'Video Player: ';
-$divStyle = 'padding-top: 56.25%';
+$divStyle = 'padding-top: 56.25%;';
 if (!$context->media->isVideo()) {
     $prefix = 'Audio Player: ';
-    $divStyle = 'height: 5.62em;';
+    $divStyle = 'height: 5.62em; max-width: 56.12rem;';
 }
 ?>
-<div style="<?php echo $divStyle ?> overflow: hidden; position:relative;">
+<div style="<?php echo $divStyle ?> overflow: hidden; position:relative; -webkit-box-flex: 1; flex-grow: 1;">
     <iframe style="bottom: 0; left: 0; position: absolute; right: 0; top: 0; border: 0; height: 100%; width: 100%;" src="<?php echo $controller->getURL($context->media)?>?format=iframe&autoplay=0" title="<?php echo $prefix ?> <?php echo UNL_MediaHub::escape($context->media->title) ?>" allowfullscreen></iframe>
 </div>
