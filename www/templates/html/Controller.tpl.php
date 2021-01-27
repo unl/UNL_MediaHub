5a7fef44b1a80ba05cae182f23eb29ef832a888a
Jeff Sturek <jsturek8@unl.edu>
2021-01-27 16:18:02 -0600

    Support WDN version 5.3

--- www/templates/html/Controller.tpl.php
+++ www/templates/html/Controller.tpl.php
@@ -1,10 +1,10 @@
 <?php
 use UNL\Templates\Templates;
 
-$page = Templates::factory('AppLocal', Templates::VERSION_5_2);
+$page = Templates::factory('AppLocal', Templates::VERSION_5_3);
 
 $wdn_include_path = UNL_MediaHub::getRootDir() . '/www';
-if (file_exists($wdn_include_path . '/wdn/templates_5.2')) {
+if (file_exists($wdn_include_path . '/wdn/templates_5.3')) {
     $page->setLocalIncludePath($wdn_include_path);
 }
 
@@ -20,7 +20,7 @@ if ($title = $context->getReplacementData('pagetitle')) {
 }
 
 // Add WDN Deprecated Styles
-$page->head .= '<link rel="preload" href="/wdn/templates_5.2/css/deprecated.css" as="style" onload="this.onload=null;this.rel=\'stylesheet\'"> <noscript><link rel="stylesheet" href="/wdn/templates_5.2/css/deprecated.css"></noscript>';
+$page->head .= '<link rel="preload" href="/wdn/templates_5.3/css/deprecated.css" as="style" onload="this.onload=null;this.rel=\'stylesheet\'"> <noscript><link rel="stylesheet" href="/wdn/templates_5.3/css/deprecated.css"></noscript>';
 
 //Header
 $page->addStyleSheet($baseUrl . 'templates/html/css/all.css?v=' . UNL_MediaHub_Controller::getVersion());
