4b042a50afe436fb302432b894d90741473e8192
Jeff Sturek <jsturek@gmail.com>
2021-03-04 10:31:15 -0600

    tabs

--- www/templates/html/MediaPlayer/v3/Audio.tpl.php
+++ www/templates/html/MediaPlayer/v3/Audio.tpl.php
@@ -1,6 +1,6 @@
 <audio class="mh_player video-js vjs-default-skin" preload="auto" data-url="<?php echo $controller->getURL($context); ?>" data-mediahub-id="<?php echo (int)$context->id ?>" poster="<?php echo UNL_MediaHub_Controller::toAgnosticURL($context->getThumbnailURL()); ?>" src="<?php echo UNL_MediaHub_Controller::toAgnosticURL($context->getMediaURL()); ?>" title="<?php echo UNL_MediaHub::escape($context->title); ?>">
-  <source src="<?php echo $context->getMediaURL(); ?>" type='audio/mpeg'>
-  <?php foreach ($context->getTextTrackURLs() as $lang=>$track):?>
-      <track id="mediahub-track-<?php echo UNL_MediaHub::escape($lang) ?>" src="<?php echo UNL_MediaHub::escape(UNL_MediaHub_Controller::toAgnosticURL($track)) ?>" kind="captions" srclang="<?php echo UNL_MediaHub::escape($lang) ?>" />
-  <?php endforeach ?>
+    <source src="<?php echo $context->getMediaURL(); ?>" type='audio/mpeg'>
+    <?php foreach ($context->getTextTrackURLs() as $lang=>$track):?>
+        <track id="mediahub-track-<?php echo UNL_MediaHub::escape($lang) ?>" src="<?php echo UNL_MediaHub::escape(UNL_MediaHub_Controller::toAgnosticURL($track)) ?>" kind="captions" srclang="<?php echo UNL_MediaHub::escape($lang) ?>" />
+    <?php endforeach ?>
 </audio>
