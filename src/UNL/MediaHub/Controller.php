28cee4ed5c8468e524e495feed0472fdfbdb6df8
Thomas Neumann <thomasneumann21800@gmail.com>
2022-08-12 15:11:53 -0500

    changed $this to self

--- src/UNL/MediaHub/Controller.php
+++ src/UNL/MediaHub/Controller.php
@@ -710,7 +710,7 @@ class UNL_MediaHub_Controller
      * @return string the model
      */
     public static function getModel() {
-        return $this->options['model'];
+        return self::options['model'];
     }
 }
 
