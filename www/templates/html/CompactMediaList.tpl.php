d5596bb4214f5a35feff1dad7c67b51a3962a496
Thomas Neumann <thomasneumann21800@gmail.com>
2023-08-04 12:25:27 -0500

    converted all popup and collapsible fieldsets to DCF and fixed thumbnail image sizes

--- www/templates/html/CompactMediaList.tpl.php
+++ www/templates/html/CompactMediaList.tpl.php
@@ -6,7 +6,9 @@ $limit = 3;
 	<?php foreach ($context->items as $media): ?>
 	<li>
 		<a href="<?php echo $controller::getURL($media) ?>">
-			<img src="<?php echo $media->getThumbnailURL() ?>" alt="<?php echo UNL_MediaHub::escape($media->title) ?>" />
+			<div class="dcf-ratio dcf-ratio-16x9 mh-thumbnail-clip">
+				<img class="dcf-ratio-child dcf-obj-fit-cover" src="<?php echo $media->getThumbnailURL() ?>" alt="<?php echo UNL_MediaHub::escape($media->title) ?>">
+			</div>
 		</a>
 	</li>
 	<?php if (++$i == $limit) break; ?> 
