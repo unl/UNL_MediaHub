a4d157d4b69f2915c4a8da8efa757a8444d8c2bc
Thomas Neumann <thomasneumann21800@gmail.com>
2023-08-18 15:33:15 -0500

    Updated popup html

--- www/templates/html/MediaPlayer/Transcript.tpl.php
+++ www/templates/html/MediaPlayer/Transcript.tpl.php
@@ -20,13 +20,12 @@
         <div class="title unl-font-sans">
             <?php echo \UNL\Templates\Icons::get(\UNL\Templates\Icons::ICON_SEARCH, '{"size": 5}'); ?>
             <h2 class="unl-font-sans">Searchable Transcript</h2>
-            <div class="dcf-popup dcf-d-inline" id="title-details" data-hover="true" data-point="true">
+            <div class="dcf-popup dcf-d-inline" id="transcript-details" data-point="true">
                 <button class="dcf-btn dcf-btn-tertiary dcf-btn-popup dcf-p-0" type="button">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         class="dcf-d-block dcf-h-3 dcf-w-3 dcf-fill-current"
-                        viewBox="0 0 24 24"
-                    >
+                        viewBox="0 0 24 24" >
                         <path d="M11.5,1C5.159,1,0,6.159,0,12.5C0,18.841,5.159,24,11.5,24
                             S23,18.841,23,12.5C23,6.159,17.841,1,11.5,1z M11.5,23 C5.71,23,1,18.29,1,12.5
                             C1,6.71,5.71,2,11.5,2S22,6.71,22,12.5C22,18.29,17.29,23,11.5,23z"></path>
@@ -45,10 +44,8 @@
                         unl-cream
                         unl-bg-blue
                         dcf-p-1
-                        dcf-rounded
-                    "
-                    style="min-width: 25ch;"
-                >
+                        dcf-rounded"
+                    style="min-width: 25ch;" >
                     <p class="dcf-m-0 dcf-regular">
                         <ul>
                             <li>Use the text input to search the transcript.</li>
