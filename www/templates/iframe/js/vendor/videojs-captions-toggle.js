aa1b66af50156f07892950b05927d56af58ecc77
Thomas Neumann <thomasneumann21800@gmail.com>
2023-08-24 13:08:07 -0500

    Updated video js to version 8

--- www/templates/iframe/js/vendor/videojs-captions-toggle.js
+++ www/templates/iframe/js/vendor/videojs-captions-toggle.js
@@ -8,45 +8,50 @@ function toggleSingleCaptionTrack(options) {
         var activeTextTrackCount = 0;
         var activeTextTrack;
         
-        var ToggleCaptionsButton = videojs.extend(Button, {
-          constructor: function(player, options) {
-              Button.call(this, player, options);
-              Button.apply(this, arguments);
-              
-              var styleEl = document.createElement('style'),
-                  styleSheet;
-              document.head.appendChild(styleEl);
-              styleSheet = styleEl.sheet;
-              styleSheet.insertRule(".vjs-subs-caps-button.toggle.active .vjs-icon-placeholder:before { color: "+options.activeColor+" }", 0);
-          },
+        // var ToggleCaptionsButton = videojs.extend(Button, {
+          
+        // });
+
+        class ToggleCaptionsButton extends Button {
+            constructor(player, options) {
+                Button.call(this, player, options);
+                Button.apply(this, arguments);
+                
+                var styleEl = document.createElement('style'),
+                    styleSheet;
+                document.head.appendChild(styleEl);
+                styleSheet = styleEl.sheet;
+                styleSheet.insertRule(".vjs-subs-caps-button.toggle.active .vjs-icon-placeholder:before { color: "+options.activeColor+" }", 0);
+            }
             /**
-           * Put the current `MenuButton` into a pressed state.
-           */
-          pressButton: function() {
-              if (mq.matches) {
-                  //We are in desktop. Make sure the caption settings button is enabled and perform default functionality
-                  this.menu.children()[0].enable();
-                  Button.prototype.pressButton.call(this);
-              } else {
-                  //We are on mobile (or at least at a mobile width)
-                  if (activeTextTrackCount === 1 && activeTextTrack) {
-                      //Only one caption track was found. Just toggle it
-                      if(activeTextTrack.mode === "showing") {
-                          activeTextTrack.mode = "disabled";
-                      } else {
-                          activeTextTrack.mode = "showing";
-                      }
-                      CheckCaptionsShowing(activeTextTrack, MyToggleCaptionsButton);
-                  } else {
-                      //More than one caption track was found, so we need to display a list
-                      //Disable the caption settings menu item because it doesn't work on mobile
-                      this.menu.children()[0].disable();
-                      //Focus the second option
-                      this.menu.focus(1);
-                  }
-              }
-          }
-        });
+             * Put the current `MenuButton` into a pressed state.
+             */
+            pressButton() {
+                if (mq.matches) {
+                    //We are in desktop. Make sure the caption settings button is enabled and perform default functionality
+                    this.menu.children()[0].enable();
+                    Button.prototype.pressButton.call(this);
+                } else {
+                    //We are on mobile (or at least at a mobile width)
+                    if (activeTextTrackCount === 1 && activeTextTrack) {
+                        //Only one caption track was found. Just toggle it
+                        if(activeTextTrack.mode === "showing") {
+                            activeTextTrack.mode = "disabled";
+                        } else {
+                            activeTextTrack.mode = "showing";
+                        }
+                        CheckCaptionsShowing(activeTextTrack, MyToggleCaptionsButton);
+                    } else {
+                        //More than one caption track was found, so we need to display a list
+                        //Disable the caption settings menu item because it doesn't work on mobile
+                        this.menu.children()[0].disable();
+                        //Focus the second option
+                        this.menu.focus(1);
+                    }
+                }
+            }
+        }
+
         this.controlBar.removeChild("subsCapsButton");
         videojs.registerComponent('ToggleCaptionsButton', ToggleCaptionsButton);
         var ButtonChildPosition = this.controlBar.children().indexOf(this.controlBar.getChild("remainingTimeDisplay"))+1; // place after remainingTimeDisplay
