7baf5e969a7609bbd69d200f613923b30af4d588
Thomas Neumann <thomasneumann21800@gmail.com>
2023-09-29 14:44:48 -0500

    Added upload caption form

--- www/manager/templates/html/Media/EditCaptions.tpl.php
+++ www/manager/templates/html/Media/EditCaptions.tpl.php
@@ -136,9 +136,79 @@
             </div>
         </div>
     </div>
-</div> 
+</div>
 
 <div class="dcf-bleed unl-bg-lightest-gray dcf-pt-6 dcf-pb-6">
+    <div class="dcf-wrapper">
+        <h2>Upload your own .vtt or .srt file</h2>
+        <div class="dcf-grid dcf-col-gap-vw">
+            <div class="dcf-col-100% dcf-col-50%-start@sm">
+                <p>
+                    When crafting captions for your video, it's crucial to ensure clarity,
+                    accuracy, and accessibility. Begin by transcribing spoken content
+                    faithfully, including relevant sounds and music cues.
+                    Use concise and easily readable sentences to maintain viewer
+                    engagement. Incorporate punctuation thoughtfully to convey
+                    tone and pacing accurately. Avoid overcrowding the screen with
+                    text and ensure proper timing to sync captions with audio.
+                    Common mistakes to avoid include inaccurate transcriptions
+                    and insufficient proofreading.
+                </p>
+            </div>
+            <div class="dcf-col-100% dcf-col-50%-end@sm">
+                <form class="dcf-form dcf-d-inline" method="post" enctype="multipart/form-data">
+                    <input type="hidden" name="__unlmy_posttarget" value="upload_caption_file" />
+                    <input type="hidden" name="media_id" value="<?php echo (int)$context->media->id ?>" />
+                    <input type="hidden" name="<?php echo $controller->getCSRFHelper()->getTokenNameKey() ?>" value="<?php echo $controller->getCSRFHelper()->getTokenName() ?>" />
+                    <input type="hidden" name="<?php echo $controller->getCSRFHelper()->getTokenValueKey() ?>" value="<?php echo $controller->getCSRFHelper()->getTokenValue() ?>">
+                    <fieldset>
+                        <legend>Caption File Upload</legend>
+                        <div class="dcf-form-group">
+                            <label for="captions-language-select"> Language <small class="dcf-required">Required</small></label>
+                            <select id="captions-language-select" name="language" required>
+                                <?php $languages = array("Abkhazian","Afar","Afrikaans","Akan","Albanian","Amharic","Arabic","Aragonese","Armenian","Assamese","Avaric","Avestan","Aymara","Azerbaijani","Bambara","Bashkir","Basque","Belarusian","Bengali","Bislama","Bosnian","Breton","Bulgarian","Burmese","Catalan, Valencian","Chamorro","Chechen","Chichewa, Chewa, Nyanja","Chinese","Church Slavonic, Old Slavonic, Old Church Slavonic","Chuvash","Cornish","Corsican","Cree","Croatian","Czech","Danish","Divehi, Dhivehi, Maldivian","Dutch, Flemish","Dzongkha","English","Esperanto","Estonian","Ewe","Faroese","Fijian","Finnish","French","Western Frisian","Fulah","Gaelic, Scottish Gaelic","Galician","Ganda","Georgian","German","Greek, Modern (1453–)","Kalaallisut, Greenlandic","Guarani","Gujarati","Haitian, Haitian Creole","Hausa","Hebrew","Herero","Hindi","Hiri Motu","Hungarian","Icelandic","Ido","Igbo","Indonesian","Interlingua (International Auxiliary Language Association)","Interlingue, Occidental","Inuktitut","Inupiaq","Irish","Italian","Japanese","Javanese","Kannada","Kanuri","Kashmiri","Kazakh","Central Khmer","Kikuyu, Gikuyu","Kinyarwanda","Kirghiz, Kyrgyz","Komi","Kongo","Korean","Kuanyama, Kwanyama","Kurdish","Lao","Latin","Latvian","Limburgan, Limburger, Limburgish","Lingala","Lithuanian","Luba-Katanga","Luxembourgish, Letzeburgesch","Macedonian","Malagasy","Malay","Malayalam","Maltese","Manx","Maori","Marathi","Marshallese","Mongolian","Nauru","Navajo, Navaho","North Ndebele","South Ndebele","Ndonga","Nepali","Norwegian","Norwegian Bokmål","Norwegian Nynorsk","Sichuan Yi, Nuosu","Occitan","Ojibwa","Oriya","Oromo","Ossetian, Ossetic","Pali","Pashto, Pushto","Persian","Polish","Portuguese","Punjabi, Panjabi","Quechua","Romanian, Moldavian, Moldovan","Romansh","Rundi","Russian","Northern Sami","Samoan","Sango","Sanskrit","Sardinian","Serbian","Shona","Sindhi","Sinhala, Sinhalese","Slovak","Slovenian","Somali","Southern Sotho","Spanish, Castilian","Sundanese","Swahili","Swati","Swedish","Tagalog","Tahitian","Tajik","Tamil","Tatar","Telugu","Thai","Tibetan","Tigrinya","Tonga (Tonga Islands)","Tsonga","Tswana","Turkish","Turkmen","Twi","Uighur, Uyghur","Ukrainian","Urdu","Uzbek","Venda","Vietnamese","Volapük","Walloon","Welsh","Wolof","Xhosa","Yiddish","Yoruba","Zhuang, Chuang","Zulu"); ?>
+                                <?php $languages_codes = array("ab","aa","af","ak","sq","am","ar","an","hy","as","av","ae","ay","az","bm","ba","eu","be","bn","bi","bs","br","bg","my","ca","ch","ce","ny","zh","cu","cv","kw","co","cr","hr","cs","da","dv","nl","dz","en","eo","et","ee","fo","fj","fi","fr","fy","ff","gd","gl","lg","ka","de","el","kl","gn","gu","ht","ha","he","hz","hi","ho","hu","is","io","ig","id","ia","ie","iu","ik","ga","it","ja","jv","kn","kr","ks","kk","km","ki","rw","ky","kv","kg","ko","kj","ku","lo","la","lv","li","ln","lt","lu","lb","mk","mg","ms","ml","mt","gv","mi","mr","mh","mn","na","nv","nd","nr","ng","ne","no","nb","nn","ii","oc","oj","or","om","os","pi","ps","fa","pl","pt","pa","qu","ro","rm","rn","ru","se","sm","sg","sa","sc","sr","sn","sd","si","sk","sl","so","st","es","su","sw","ss","sv","tl","ty","tg","ta","tt","te","th","bo","ti","to","ts","tn","tr","tk","tw","ug","uk","ur","uz","ve","vi","vo","wa","cy","wo","xh","yi","yo","za","zu"); ?>
+                                <?php foreach ($languages as $index => $language): ?>
+                                    <?php $selected = $language === 'English' ? 'selected="selected"' : ''; ?>
+                                    <option value="<?php echo $languages_codes[$index]; ?>" <?php echo $selected; ?>>
+                                        <?php echo $language; ?>
+                                    </option>
+                                <?php endforeach;?>
+                            </select>
+                        </div>
+                        <div class="dcf-form-group">
+                            <label for="captions-country-select"> Country (Optional)</label>
+                            <select id="captions-country-select" name="country">
+                                <option value="" selected="selected">N/A</option>
+                                <?php $countries = array("Afghanistan","Åland Islands","Albania","Algeria","American Samoa","Andorra","Angola","Anguilla","Antarctica","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia (Plurinational State of)","Bonaire, Sint Eustatius and Saba","Bosnia and Herzegovina","Botswana","Bouvet Island","Brazil","British Indian Ocean Territory","Brunei Darussalam","Bulgaria","Burkina Faso","Burundi","Cabo Verde","Cambodia","Cameroon","Canada","Cayman Islands","Central African Republic","Chad","Chile","China","Christmas Island","Cocos (Keeling) Islands","Colombia","Comoros","Congo","Congo, Democratic Republic of the","Cook Islands","Costa Rica","Côte d'Ivoire","Croatia","Cuba","Curaçao","Cyprus","Czechia","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Eswatini","Ethiopia","Falkland Islands (Malvinas)","Faroe Islands","Fiji","Finland","France","French Guiana","French Polynesia","French Southern Territories","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guadeloupe","Guam","Guatemala","Guernsey","Guinea","Guinea-Bissau","Guyana","Haiti","Heard Island and McDonald Islands","Holy See","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran (Islamic Republic of)","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Korea (Democratic People's Republic of)","Korea, Republic of","Kuwait","Kyrgyzstan","Lao People's Democratic Republic","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macao","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Martinique","Mauritania","Mauritius","Mayotte","Mexico","Micronesia (Federated States of)","Moldova, Republic of","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands, Kingdom of the","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Niue","Norfolk Island","North Macedonia","Northern Mariana Islands","Norway","Oman","Pakistan","Palau","Palestine, State of","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Pitcairn","Poland","Portugal","Puerto Rico","Qatar","Réunion","Romania","Russian Federation","Rwanda","Saint Barthélemy","Saint Helena, Ascension and Tristan da Cunha","Saint Kitts and Nevis","Saint Lucia","Saint Martin (French part)","Saint Pierre and Miquelon","Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Sint Maarten (Dutch part)","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Georgia and the South Sandwich Islands","South Sudan","Spain","Sri Lanka","Sudan","Suriname","Svalbard and Jan Mayen","Sweden","Switzerland","Syrian Arab Republic","Taiwan, Province of China","Tajikistan","Tanzania, United Republic of","Thailand","Timor-Leste","Togo","Tokelau","Tonga","Trinidad and Tobago","Tunisia","Türkiye","Turkmenistan","Turks and Caicos Islands","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom of Great Britain and Northern Ireland","United States Minor Outlying Islands","United States of America","Uruguay","Uzbekistan","Vanuatu","Venezuela (Bolivarian Republic of)","Viet Nam","Virgin Islands (British)","Virgin Islands (U.S.)","Wallis and Futuna","Western Sahara","Yemen","Zambia","Zimbabwe"); ?>
+                                <?php $country_codes = array("AF","AX","AL","DZ","AS","AD","AO","AI","AQ","AG","AR","AM","AW","AU","AT","AZ","BS","BH","BD","BB","BY","BE","BZ","BJ","BM","BT","BO","BQ","BA","BW","BV","BR","IO","BN","BG","BF","BI","CV","KH","CM","CA","KY","CF","TD","CL","CN","CX","CC","CO","KM","CG","CD","CK","CR","CI","HR","CU","CW","CY","CZ","DK","DJ","DM","DO","EC","EG","SV","GQ","ER","EE","SZ","ET","FK","FO","FJ","FI","FR","GF","PF","TF","GA","GM","GE","DE","GH","GI","GR","GL","GD","GP","GU","GT","GG","GN","GW","GY","HT","HM","VA","HN","HK","HU","IS","IN","ID","IR","IQ","IE","IM","IL","IT","JM","JP","JE","JO","KZ","KE","KI","KP","KR","KW","KG","LA","LV","LB","LS","LR","LY","LI","LT","LU","MO","MG","MW","MY","MV","ML","MT","MH","MQ","MR","MU","YT","MX","FM","MD","MC","MN","ME","MS","MA","MZ","MM","NA","NR","NP","NL","NC","NZ","NI","NE","NG","NU","NF","MK","MP","NO","OM","PK","PW","PS","PA","PG","PY","PE","PH","PN","PL","PT","PR","QA","RE","RO","RU","RW","BL","SH","KN","LC","MF","PM","VC","WS","SM","ST","SA","SN","RS","SC","SL","SG","SX","SK","SI","SB","SO","ZA","GS","SS","ES","LK","SD","SR","SJ","SE","CH","SY","TW","TJ","TZ","TH","TL","TG","TK","TO","TT","TN","TR","TM","TC","TV","UG","UA","AE","GB","UM","US","UY","UZ","VU","VE","VN","VG","VI","WF","EH","YE","ZM","ZW"); ?>
+                                <?php foreach ($countries as $index => $country): ?>
+                                    <option value="<?php echo $country_codes[$index]; ?>">
+                                        <?php echo $country; ?>
+                                    </option>
+                                <?php endforeach;?>
+                            </select>
+                        </div>
+                        <div class="dcf-form-group">
+                            <label for="caption-file">Caption File <small class="dcf-required">Required</small></label>
+                            <input type="file" id="caption-file" name="caption_file" accept=".vtt,.srt" aria-describedby="caption-file-help" required>
+                            <p class="dcf-form-help dcf-mb-0" id="caption-file-help">Must be of type .vtt or .srt</p>
+                        </div>
+                        <div class="dcf-form-group">
+                            <label for="caption-comment">Comment</label>
+                            <input type="text" id="caption-comment" name="caption_comment">
+                        </div>
+                        <div class="dcf-form-group">
+                            <input class="dcf-btn dcf-btn-primary dcf-mt-1" type="submit" value="Upload">
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</div> 
+
+<div class="dcf-bleed dcf-pt-6 dcf-pb-6">
     <div class="dcf-wrapper">
         <h2>Order history and status</h2>
         <p>View the current status of your orders</p>
