04a8ddc53102acee6812d3c7a6a17ac9ddc8fdd6
Thomas Neumann <thomasneumann21800@gmail.com>
2024-06-18 09:36:12 -0500

    Added error message for if we can't contact the captioning server

--- www/manager/templates/html/Feed/Media/transcriber_maintenance_notice.tpl.php
+++ www/manager/templates/html/Feed/Media/transcriber_maintenance_notice.tpl.php
@@ -3,7 +3,7 @@
         <h2>Auto Captions Maintenance Notice</h2>
         <div>
             We will be preforming maintenance on
-            the auto captioning service on <span id="auto-captioning-maintenance-date-range"></span>. <br>
+            the auto captioning service on <span id="auto-captioning-maintenance-date-range"></span>.
             Captions may not be generated at that
             time but they will be generated once
             the maintenance has finished.
@@ -11,10 +11,21 @@
     </div>
 </div>
 
+<div id="auto-captioning-error" class="dcf-d-none">
+    <div id="auto-captioning-error-notice" class="dcf-notice dcf-notice-danger" data-no-close-button="true" hidden>
+        <h2>Auto Captions Error</h2>
+        <div>
+            There is an error trying to contact the auto captioning server. If this error persists please contact an administrator.
+        </div>
+    </div>
+</div>
+
 <script>
     const api_status_url = '<?php echo UNL_MediaHub_TranscriptionManager::getURL(); ?>';
     const auto_captioning_maintenance = document.getElementById('auto-captioning-maintenance');
+    const auto_captioning_error = document.getElementById('auto-captioning-error');
     const auto_captioning_maintenance_notice = document.getElementById('auto-captioning-maintenance-notice');
+    const auto_captioning_error_notice = document.getElementById('auto-captioning-error-notice');
 
     async function check_captioning_status() {
         const auto_captioning_maintenance_date_range = document.getElementById('auto-captioning-maintenance-date-range');
@@ -27,8 +38,14 @@
             }
             const parsed_data = await api_status_response.json();
 
-            if ( !('status' in parsed_data) || parsed_data['status'] !== 'OK') {
+            if ( !('status' in parsed_data) ) {
                 console.error('Bad result from ' + api_status_url);
+                return;
+            }
+
+            if (parsed_data['status'] !== 'OK') {
+                // Show the error message
+                auto_captioning_error.classList.remove('dcf-d-none');
                 return;
             }
 
@@ -46,7 +63,10 @@
 
     window.addEventListener('inlineJSReady', function() {
         let timer = setInterval(() => {
-            if (auto_captioning_maintenance_notice.classList.contains('dcf-notice-initialized')) {
+            if (
+                auto_captioning_maintenance_notice.classList.contains('dcf-notice-initialized')
+                && auto_captioning_error_notice.classList.contains('dcf-notice-initialized')
+            ) {
                 clearInterval(timer);
                 check_captioning_status();
             }
