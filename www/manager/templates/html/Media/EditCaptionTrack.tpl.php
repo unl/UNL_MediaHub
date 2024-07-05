5cd61ca186fc4ca002bde41097f4de6a78914904
Thomas Neumann <thomasneumann21800@gmail.com>
2024-07-05 11:34:38 -0500

    Added media embed/link to edit captions page

--- www/manager/templates/html/Media/EditCaptionTrack.tpl.php
+++ www/manager/templates/html/Media/EditCaptionTrack.tpl.php
@@ -57,9 +57,57 @@
                         <div class="dcf-mt-4" role="group" aria-label="Edit Caption Actions">
                             <input type="hidden" form="captions_edit" name="__unlmy_posttarget" value="update_text_track_file" />
                             <input class="dcf-btn dcf-btn-primary" form="captions_edit" name="submit" type="submit" id="save-captions-btn" value="Save">
-                            <a href="<?php echo UNL_MediaHub_Controller::getURL() . 'manager/?view=editcaptions&id=' . (int)$context->media->id?>" class="dcf-btn dcf-btn-secondary" onclick="return confirm('Are you sure you want to cancel?');">Cancel</a>
+                            <a href="<?php echo UNL_MediaHub_Controller::getURL() . 'manager/?view=editcaptions&id=' . (int)$context->media->id; ?>" class="dcf-btn dcf-btn-secondary" onclick="return confirm('Are you sure you want to cancel?');">Cancel</a>
                         </div>
                     <?php endif; ?>
+                </div>
+                <div class="dcf-mt-6 dcf-mb-6">
+                    <?php
+                        $padding = array(
+                            '1x1' => '100%',
+                            '3x4' => '133.33%',
+                            '4x3' => '75%',
+                            '9x16' => '177.77%',
+                            '16x9' => '56.25%',
+                        );
+
+                        $prefix = 'Video Player: ';
+                        $divStyle = 'padding-top: ' . $padding['16x9'] . ';';
+                        if (!$context->media->isVideo()) {
+                            $prefix = 'Audio Player: ';
+                            $divStyle = 'height: 5.62em; max-width: 56.12rem;';
+                            $aspect_ratio = '16x9';
+                        } else {
+                            $aspect_ratio_raw = $context->media->getAspectRatio();
+                            $aspect_ratio = '16x9';
+                            switch ($aspect_ratio_raw) {
+                                case UNL_MediaHub_Media::ASPECT_1x1:
+                                $aspect_ratio = '1x1';
+                                break;
+                                case UNL_MediaHub_Media::ASPECT_3x4:
+                                $aspect_ratio = '3x4';
+                                break;
+                                case UNL_MediaHub_Media::ASPECT_4x3:
+                                $aspect_ratio = '4x3';
+                                break;
+                                case UNL_MediaHub_Media::ASPECT_9x16:
+                                $aspect_ratio = '9x16';
+                                break;
+                                default:
+                                $aspect_ratio = '16x9';
+                            }
+                            $divStyle = 'padding-top: ' . $padding[$aspect_ratio] . ';';
+                        }
+                    ?>
+                    <div style="<?php echo $divStyle; ?> overflow: hidden; position:relative; -webkit-box-flex: 1; flex-grow: 1;">
+                        <iframe
+                            style="bottom: 0; left: 0; position: absolute; right: 0; top: 0; border: 0; height: 100%; width: 100%;"
+                            src="<?php echo UNL_MediaHub_Controller::getURL($context->media); ?>?format=iframe&autoplay=0"
+                            title="<?php echo $prefix; ?> <?php echo UNL_MediaHub::escape($context->media->title); ?>"
+                            allowfullscreen
+                        ></iframe>
+                    </div>
+                    <a class="dcf-btn dcf-btn-secondary dcf-mt-1" href="<?php echo UNL_MediaHub_Controller::getURL($context->media)?>" target="_blank">View Media</a>
                 </div>
             </div>
             <?php
